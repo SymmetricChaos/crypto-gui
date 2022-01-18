715228d3550bc2ba2b13df61b0a9df81d60cd429
SymmetricChaos <ajfraebel@gmail.com>
2022-01-17 19:09:06 -0500

    genericized math functions

--- src/math.rs
+++ src/math.rs
@@ -1,44 +1,37 @@
 use rand::prelude::{ThreadRng, SliceRandom};
-use num::Integer;
+use num::{Integer, ToPrimitive};
 
-fn egcd<N: Integer + Copy>(a: N, b: N) -> (N,N,N) {
-    if a.is_zero() {
-        (b,N::zero(),N::one())
-    } else {
-        let (g, y, x) = egcd(b.mod_floor(&a), a);
-        (g,x-(b/a)*y,y)
-    }
-}
 
-pub fn mul_inv<N: Integer + Copy>(num: N, modulus: N) -> Option<N> {
-    let (g, x, _) = egcd(num, modulus);
-    if !g.is_one() {
+pub fn mul_inv<N: Integer + Copy >(num: N, modulus: N) -> Option<N> {
+    let egcd = num.extended_gcd(&modulus);
+    if !egcd.gcd.is_one() {
         None 
     } else {
-        Some( x.mod_floor(&modulus) )
+        Some( egcd.x.mod_floor(&modulus) )
     }
 }
 
 // We're not going to deal with big numbers so this thse crude factorizations are plenty
-pub fn factors(n: usize) -> Vec<usize> {
+pub fn factors<N: Integer + Copy + ToPrimitive>(n: N) -> Vec<N> {
     let mut out = Vec::new();
-    for f in 1..n/2 {
-        if n%f == 0 {
+    for f in num::range(N::one(), n) {
+        if n.is_multiple_of(&f) {
             out.push(n);
-            out.push(n/f);
+            out.push(n.div(f));
         }
     }
     out.sort();
     out
 }
 
-pub fn prime_factorization(n: usize) -> Vec<usize> {
+pub fn prime_factorization<N: Integer + Copy>(n: N) -> Vec<N> {
+    if n.is_zero() { return Vec::new() }
     let mut out = Vec::new();
     let mut n = n;
-    let mut f = 1;
-    while n != 1 {
-        f += 1;
-        while n % f == 0 {
+    let mut f = N::one();
+    while !n.is_one() {
+        f = f + N::one();
+        while n.is_multiple_of(&f) {
             n = n/f;
             out.push(f);
         }
@@ -47,7 +40,7 @@ pub fn prime_factorization(n: usize) -> Vec<usize> {
     out
 }
 
-pub fn prime_factors(n: usize) -> Vec<usize> {
+pub fn prime_factors<N: Integer + Copy>(n: N) -> Vec<N> {
     let mut out = prime_factorization(n);
     out.dedup();
     out
