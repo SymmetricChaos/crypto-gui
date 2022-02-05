99c52e829fe920bbc173e48503c27c0deda8039b
SymmetricChaos <ajfraebel@gmail.com>
2022-02-04 19:59:14 -0500

    settings

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,5 @@
 use crate::cipher_id::CipherID;
-use eframe::{egui::{CtxRef, SidePanel, CentralPanel, ScrollArea}, epi};
+use eframe::{egui::{CtxRef, SidePanel, CentralPanel, ScrollArea, TopBottomPanel, Window, Button}, epi};
 
 use crate::cipher_panel::{ControlPanel, DisplayPanel};
 
@@ -11,7 +11,7 @@ pub struct ClassicCrypto {
     output: String,
     errors: String,
     active_cipher: CipherID,
-
+    show_settings: bool,
 }
 
 impl Default for ClassicCrypto {
@@ -23,6 +23,7 @@ impl Default for ClassicCrypto {
             output: String::new(),
             errors: String::new(),
             active_cipher: CipherID::default(),
+            show_settings: false,
         }
     }
 }
@@ -35,18 +36,33 @@ impl epi::App for ClassicCrypto {
 
     fn setup(
         &mut self,
-        _ctx: &CtxRef,
+        ctx: &CtxRef,
         _frame: &epi::Frame,
         _storage: Option<&dyn epi::Storage>,
     ) {
+        let x = ctx.fonts().pixels_per_point();
     }
 
     fn update(&mut self, ctx: &CtxRef, frame: &epi::Frame) {
         frame.set_window_size((1000.0,550.0).into());
         ctx.set_pixels_per_point(1.2);
+        
+        TopBottomPanel::top("top_panel").show(ctx, |ui| {
+            if ui.add(Button::new("Settings").small() ).clicked() {
+                self.show_settings = !self.show_settings;
+            }
+        });
+
+        Window::new("🔧 Settings")
+            .open(&mut self.show_settings)
+            .vscroll(true)
+            .show(ctx, |ui| {
+                ctx.settings_ui(ui);
+        });
 
         SidePanel::right("display_panel").max_width(300.0).show(ctx, |ui| {
-            self.display.ui(ui, &mut self.input, &mut self.output, &mut self.errors)
+            self.display.ui(ui, &mut self.input, &mut self.output, &mut self.errors);
+            
         });
 
         CentralPanel::default().show(ctx, |ui| {
