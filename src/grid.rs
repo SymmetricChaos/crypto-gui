b83f053b9e4298765ba43e7b768a3f2aa2e87355
SymmetricChaos <ajfraebel@gmail.com>
2022-02-11 07:20:12 -0500

    Update grid.rs

--- src/grid.rs
+++ src/grid.rs
@@ -8,7 +8,7 @@ pub fn str_to_grid_symbols(text: &str, empty_char: char, blocked_char: char) -> 
         } else if c == blocked_char {
             v.push(Symbol::Blocked)
         } else {
-            v.push(Symbol::Symbol(c))
+            v.push(Symbol::Character(c))
         }
     }
     v
@@ -16,7 +16,7 @@ pub fn str_to_grid_symbols(text: &str, empty_char: char, blocked_char: char) -> 
  
 #[derive(Copy, Clone, PartialEq, Eq, Debug)]
 pub enum Symbol {
-    Symbol(char),
+    Character(char),
     Empty,
     Blocked
 }
@@ -45,15 +45,16 @@ impl Grid {
  
     pub fn new_col_major(text: &str, num_rows: usize, num_cols: usize, empty_char: char, blocked_char: char) -> Self {
         let mut symbols = str_to_grid_symbols(text, empty_char, blocked_char);
+        let grid_size = num_rows * num_cols;
         // Drop excess symbols or pad with Empty as needed
-        symbols.truncate(num_rows * num_cols);
-        symbols.resize(num_rows * num_cols, Symbol::Empty);
+        symbols.truncate(grid_size);
+        symbols.resize(grid_size, Symbol::Empty);
  
-        let mut grid = Vec::with_capacity(num_rows*num_cols);
+        let mut grid = Vec::with_capacity(grid_size);
  
         for col in 0..num_cols {
             for row in 0..num_rows {
-                grid[col+(row*num_cols)] = symbols.remove(0);
+                grid[col+(grid_size)] = symbols.remove(0);
             }
         }
  
@@ -68,7 +69,7 @@ impl Grid {
         self.num_rows
     }
  
-    pub fn num_row(&self) -> usize {
+    pub fn num_rows(&self) -> usize {
         self.num_rows
     }
  
@@ -76,26 +77,37 @@ impl Grid {
         self.num_cols
     }
  
-    fn get_index(&self, pos: (usize,usize)) -> Option<usize> {
-        if pos.0 < self.num_rows && pos.1 < self.num_cols {
-            Some(pos.0 * self.row_len() + pos.1)
+    pub fn grid_size(&self) -> usize {
+        self.num_rows * self.num_cols
+    }
+ 
+    pub fn coord_from_index(&self, idx: usize) -> Option<(usize,usize)> {
+        if idx >= self.grid.len() {
+            return None
+        }
+        Some((idx / self.num_cols, idx % self.num_cols))
+    }
+ 
+    pub fn index_from_coord(&self, coord: (usize,usize)) -> Option<usize> {
+        if coord.0 < self.num_rows && coord.1 < self.num_cols {
+            Some(coord.0 * self.row_len() + coord.1)
         } else {
             None
         }
     }
  
-    pub fn get(&self, pos: (usize, usize)) -> Option<&Symbol> {
-        let idx = self.get_index(pos)?;
-        self.grid.get(idx)
+    pub fn get(&self, coord: (usize, usize)) -> Option<&Symbol> {
+        let coord = self.index_from_coord(coord)?;
+        self.grid.get(coord)
     }
  
-    pub fn get_mut(&mut self, pos: (usize, usize)) -> Option<&mut Symbol> {
-        let idx = self.get_index(pos)?;
+    pub fn get_mut(&mut self, coord: (usize, usize)) -> Option<&mut Symbol> {
+        let idx = self.index_from_coord(coord)?;
         self.grid.get_mut(idx)
     }
  
     pub fn row(&self, row_index: usize) -> impl Iterator<Item = &Symbol> {
-        let start = self.get_index((row_index, 0))
+        let start = self.index_from_coord((row_index, 0))
             .expect("Row index was out of bounds");
         let end = start + self.row_len();
         self.grid[start..end].iter()
@@ -119,6 +131,48 @@ impl Grid {
         }
         symbols.into_iter()
     }
+ 
+    // Replace the cell with the Symbol::Empty and return what was overwritten, panics if out of bounds
+    pub fn empty_cell(&mut self, coord: (usize, usize)) -> Option<Symbol> {
+        let contents = *self.get(coord)?;
+        self[coord] = Symbol::Empty;
+        Some(contents)
+    }
+ 
+    // Replace the cell with the Symbol::Blocked and return what was overwritten
+    pub fn block_cell(&mut self, coord: (usize, usize)) -> Option<Symbol> {
+        let contents = *self.get(coord)?;
+        self[coord] = Symbol::Blocked;
+        Some(contents)
+    }
+ 
+    // Replace the cell with the Symbol and return what was overwritten
+    pub fn replace(&mut self, coord: (usize, usize), new_sym: Symbol) -> Option<Symbol> {
+        let contents = *self.get(coord)?;
+        self[coord] = new_sym;
+        Some(contents)
+    }
+ 
+    // Replace only if the position is Symbol::Empty
+    pub fn replace_if_empty(&mut self, coord: (usize, usize), new_sym: Symbol) -> Option<Symbol> {
+        let contents = *self.get(coord)?;
+        if contents == Symbol::Empty {
+            self[coord] = new_sym;
+            return Some(contents)
+        }
+        None
+    }
+ 
+    // Replace if the position is anything other than Symbol::Blocked
+    pub fn replace_if_not_blocked(&mut self, coord: (usize, usize), new_sym: Symbol) -> Option<Symbol> {
+        let contents = *self.get(coord)?;
+        if contents != Symbol::Blocked {
+            self[coord] = new_sym;
+            return Some(contents)
+        }
+        None
+    }
+ 
  
     // pub fn rotate(&mut self) {
     //     let mut new_grid = Vec::<Symbol>::with_capacity(self.grid.len());
