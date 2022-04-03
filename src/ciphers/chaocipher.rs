67aa249a1b6ac2d931e0bf8187c2f8abf8e8bfc6
SymmetricChaos <ajfraebel@gmail.com>
2022-04-03 14:53:55 -0400

    removed state from chaocipher

--- src/ciphers/chaocipher.rs
+++ src/ciphers/chaocipher.rs
@@ -1,42 +1,47 @@
-use std::cell::RefCell;
-
 use crate::errors::CipherError;
 use crate::text_types::{VecString, PresetAlphabet};
 
 use super::Cipher;
  
 pub struct Chaocipher {
-    left: RefCell<VecString>,
-    right: RefCell<VecString>,
+    left: VecString,
+    right: VecString,
+    //prev_state: (VecString,VecString),
 }
 
 impl Chaocipher {
-    fn left_permute(&self, n: usize) {
-        self.left.borrow_mut().rotate_left(n);
-        let t = self.left.borrow_mut().remove(1).unwrap();
-        self.left.borrow_mut().insert(13, t);
+    fn left_permute(left: &mut VecString, n: usize) {
+        left.rotate_left(n);
+        let t = left.remove(1).unwrap();
+        left.insert(13, t);
     }
 
-    fn right_permute(&self, n: usize) {
-        self.right.borrow_mut().rotate_left(n+1);
-        let t = self.right.borrow_mut().remove(2).unwrap();
-        self.right.borrow_mut().insert(13, t);
+    fn right_permute(right: &mut VecString, n: usize) {
+        right.rotate_left(n+1);
+        let t = right.remove(2).unwrap();
+        right.insert(13, t);
     }
 
     pub fn set_left(&mut self, s: &str) {
-        self.left = RefCell::new(VecString::from(s))
+        self.left = VecString::from(s)
     }
 
     pub fn set_right(&mut self, s: &str) {
-        self.right = RefCell::new(VecString::from(s))
+        self.right = VecString::from(s)
     }
+
+    // pub fn previous_state(&mut self) {
+    //     self.left.replace(self.prev_state.0.clone());
+    //     self.right.replace(self.prev_state.1.clone());
+    // }
 }
 
 impl Default for Chaocipher {
     fn default() -> Self {
         Chaocipher { 
-            left: RefCell::new(VecString::from(PresetAlphabet::BasicLatin)), 
-            right: RefCell::new(VecString::from("AZDNBUHYFWJLVGRCQMPSOEXTKI")),  
+            left: VecString::from(PresetAlphabet::BasicLatin), 
+            right: VecString::from("AZDNBUHYFWJLVGRCQMPSOEXTKI"),
+            //prev_state: (VecString::from(PresetAlphabet::BasicLatin), VecString::from("AZDNBUHYFWJLVGRCQMPSOEXTKI"))
         }
     }
 }
@@ -44,35 +49,62 @@ impl Default for Chaocipher {
 impl Cipher for Chaocipher {
 
     fn encrypt(&self, text: &str) -> Result<String, CipherError> {
+        let mut left = self.left.clone();
+        let mut right = self.right.clone();
+
         let symbols = text.chars();
         let mut out = String::new();
         for c in symbols {
-            let n = self.right.borrow().pos(c).unwrap();
-            out.push(self.left.borrow()[n]);
-            self.left_permute(n);
-            self.right_permute(n);
+            let n = right.pos(c).unwrap();
+            out.push(left[n]);
+            Chaocipher::left_permute(&mut left,n);
+            Chaocipher::right_permute(&mut right,n);
         }
         Ok(out)
     }
 
     fn decrypt(&self, text: &str) -> Result<String, CipherError> {
+        let mut left = self.left.clone();
+        let mut right = self.right.clone();
+
         let symbols = text.chars();
         let mut out = String::new();
         for c in symbols {
-            let n = self.left.borrow().pos(c).unwrap();
-            out.push(self.right.borrow()[n]);
-            self.left_permute(n);
-            self.right_permute(n);
+            let n = left.pos(c).unwrap();
+            out.push(right[n]);
+            Chaocipher::left_permute(&mut left,n);
+            Chaocipher::right_permute(&mut right,n);
         }
         Ok(out)
  
     }
 
-    fn randomize(&mut self, rng: &mut rand::prelude::StdRng) {
+    fn randomize(&mut self, _rng: &mut rand::prelude::StdRng) {
         todo!()
     }
 
     fn reset(&mut self) {
         *self = Self::default();
+    }
+}
+
+
+#[cfg(test)]
+mod chaocipher_tests {
+    use super::*;
+
+    const PLAINTEXT: &'static str = "THEQUICKBROWNFOXJUMPSOVERTHELAZYDOG";
+    const CIPHERTEXT: &'static str = "";
+
+    #[test]
+    fn encrypt_test() {
+        let cipher = Chaocipher::default();
+        assert_eq!(cipher.encrypt(PLAINTEXT).unwrap(), CIPHERTEXT);
+    }
+
+    #[test]
+    fn decrypt_test() {
+        let cipher = Chaocipher::default();
+        assert_eq!(cipher.decrypt(CIPHERTEXT).unwrap(), PLAINTEXT);
     }
 }
