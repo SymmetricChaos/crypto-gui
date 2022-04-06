f568c6e633333ff80afc802176408d135cd874e4
SymmetricChaos <ajfraebel@gmail.com>
2022-04-06 07:25:10 -0400

    Create spelling_alphabet.rs

---
+++ src/codes/spelling_alphabet.rs
@@ -0,0 +1,78 @@
+use lazy_static::lazy_static;
+use std::{collections::HashMap, fmt};
+
+fn make_maps(alphabet: &'static str, codes: &[&'static str]) -> (HashMap<char,&'static str>, HashMap<&'static str, char>) {
+        let mut map = HashMap::new();
+        let mut map_inv = HashMap::new();
+        for (l,w) in alphabet.chars().zip( codes.iter()) {
+            map.insert(l, *w);
+            map_inv.insert(*w,l);
+        }
+        (map, map_inv)
+}
+
+
+lazy_static! {
+    // Yes, ALFA and JULIETT are meant to be spelled that way
+    // Yes, the spelling of the numerals is correct even though the pronunciation is different
+    pub static ref NATO: (HashMap<char,&'static str>, HashMap<&'static str, char>) = make_maps(
+                                                "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", 
+                                                &["ALFA", "BRAVO", "CHARLIE", "DELTA", "ECHO", "FOXTROT",
+                                                "GOLF", "HOTEL", "INDIA", "JULIETT", "KILO", "LIMA",
+                                                "MIKE", "NOVEMBER", "OSCAR", "PAPA", "QUEBEC", "ROMEO",
+                                                "SIERRA", "TANGO", "UNIFORM", "VICTOR", "WHISKEY",
+                                                "XRAY", "YANKEE", "ZULU", "ZERO", "ONE", "TWO", "THREE", "FOUR",
+                                                "FIVE", "SIX", "SEVEN", "EIGHT", "NINE"]);
+                                                
+    pub static ref CCB: (HashMap<char,&'static str>, HashMap<&'static str, char>) = make_maps(
+                                              "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789", 
+                                              &["ABLE", "BAKER", "CHARLIE", "DOG", "EASY", "FOX",
+                                              "GEORGE", "HOW", "ITEM", "JIG", "KING", "LOVE",
+                                              "MIKE", "NAN", "OBOE", "PETER", "QUEEN", "ROGER",
+                                              "SUGAR", "TARE", "UNCLE", "VICTOR", "WILLIAM",
+                                              "XRAY", "YOKE", "ZEBRA", "ZERO", "ONE", "TWO", "THREE", "FOUR",
+                                              "FIVE", "SIX", "SEVEN", "EIGHT", "NINE"]);
+}
+
+pub enum SpellingAlphabetMode {
+    Nato,
+    Ccb,
+}
+
+impl SpellingAlphabetMode {
+    pub fn encode(c: char) -> Option<&str> {
+        match self {
+            Nato => NATO.0.get(&c),
+            Ccb => CCB.0.get(&c),
+        }
+    }
+    
+    pub fn decode(s: &str) -> Option<char> {
+        match self {
+            Nato => NATO.1.get(s),
+            Ccb => CCB.1.get(s),
+        }
+    }
+}
+
+pub struct SpellingAlphabet {
+    mode: SpellingAlphabetMode
+}
+
+impl Default for SpellingAlphabet {
+    fn default() -> Self {
+        Self { mode: Default::default() }
+    }
+}
+
+
+
+impl Code for SpellingAlphabet {
+    fn encode(&self, text: &str) -> Result<CodeError,String> {
+        Ok( text.chars().map(|c| self.mode.encode(c).ok_or(CodeError::input("invalid character"))? ).join(' ').collect() )
+    }
+
+    fn decode(&self, text: &str) -> Result<CodeError,String> {
+        Ok(text.split(" ").map(|s| self.mode.decode(s).ok_or(CodeError::input("invalid character"))? ).collect() )
+    }
+}
