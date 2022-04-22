762cac54ece96153d6a99e365789d85f45254d41
SymmetricChaos <ajfraebel@gmail.com>
2022-04-22 07:35:23 -0400

    update to alphabet
    need to test the speed of this before patting myself on the back
    also need to replace VecString with this in most places
    also maybe PresetAlphabets should be integrated with this using lazy_static

--- src/alphabet.rs
+++ src/alphabet.rs
@@ -1,41 +1,117 @@
-use std::{collections::BTreeMap, fmt};
+use std::collections::BTreeMap;
+use std::fmt;
+use std::ops::Index;
 use itertools::Itertools;
 
+// An Alphabet is a small ordered collection of unique chars
+// We want to be able to quickly know the index of any char and the char at
+// any index. A BTreeMap should work for this, with u8 keys to save space.
+// This is probably a needless optimization but it will save writing lots of
+// iters and lambdas.
+// No way to modify an Alphabet once created is provided.
 #[derive(Clone,Debug)]
 pub struct Alphabet {
-    positions: BTreeMap<char,usize>
+    set: BTreeMap<char,u8>
 }
 
 impl Alphabet {
 
-    pub fn empty() -> Self {
-        Self{ positions: BTreeMap::new() }
-    }
-
-    pub fn from(alphabet: &str) -> Self {
-        let characters = alphabet.chars().unique();
-        let mut positions = BTreeMap::new();
-        for (pos,c) in characters.enumerate() {
-            positions.insert(c, pos);
-        }
-        Self{ positions }
-    }
-
+    // Get the character at some position
     pub fn get_char(&self, n: usize) -> Option<char> {
-        self.positions.keys().nth(n).map(|c| *c)
+        self.set.keys().nth(n).map(|c| *c)
     }
-
+    
+    // Get the position of some character
     pub fn get_pos(&self, c: char) -> Option<usize> {
-        self.positions.get(&c).map(|n| *n)
+        self.set.get(&c).map(|n| *n as usize)
+    }
+    
+    // Get the character at some position with a (positive or negative) offset
+    pub fn get_char_offset(&self, n: usize, offset: i32) -> Option<char> {
+        let idx = ((n + self.len()) as i32 + offset) as usize % self.len();
+        self.set.keys().nth(idx).map(|c| *c)
+    }
+    
+    // Get the position of some character with a (positive or negative) offset
+    pub fn get_pos_offset(&self, c: char, offset: i32) -> Option<usize> {
+        let shift = (self.len() as i32 - offset) as usize % self.len();
+        self.set.get(&c).map(|n| (*n as usize + shift) % self.len() )
     }
 
     pub fn contains(&self, c: char) -> bool {
-        self.positions.contains_key(&c)
+        self.set.contains_key(&c)
+    }
+    
+    pub fn chars(&self) -> impl Iterator<Item=char> +'_ {
+        self.set.keys().map(|c| *c)
+    }
+    
+    pub fn len(&self) -> usize {
+        self.set.len()
     }
 }
 
 impl fmt::Display for Alphabet {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", self.positions.keys().collect::<String>())
+        write!(f, "{}", self.set.keys().collect::<String>())
+    }
+}
+
+impl From<String> for Alphabet {
+    fn from(str: String) -> Self {
+        let mut set = BTreeMap::new();
+        for (pos,c) in str.chars().unique().enumerate() {
+            set.insert(c, pos as u8);
+        }
+        Self{ set }
+    }
+}
+
+impl From<&String> for Alphabet {
+    fn from(str: &String) -> Self {
+        let mut set = BTreeMap::new();
+        for (pos,c) in str.chars().unique().enumerate() {
+            set.insert(c, pos as u8);
+        }
+        Self{ set }
+    }
+}
+
+impl From<&str> for Alphabet {
+    fn from(str: &str) -> Self {
+        let mut set = BTreeMap::new();
+        for (pos,c) in str.chars().unique().enumerate() {
+            set.insert(c, pos as u8);
+        }
+        Self{ set }
+    }
+}
+
+#[cfg(test)]
+mod alphabet_tests {
+    use super::*;
+ 
+    #[test]
+    fn nth_offset()  {
+        let alphabet = Alphabet::from("ABCD");
+        assert_eq!(alphabet.get_char_offset(1,1).unwrap(),'C');
+    }
+ 
+    #[test]
+    fn pos_offset() {
+        let alphabet = Alphabet::from("ABCD");
+        assert_eq!(alphabet.get_pos_offset('C',1).unwrap(),1);
+    }
+ 
+    #[test]
+    fn nth_offset_neg()  {
+        let alphabet = Alphabet::from("ABCD");
+        assert_eq!(alphabet.get_char_offset(3,-1).unwrap(),'C');
+    }
+ 
+    #[test]
+    fn pos_offset_neg() {
+        let alphabet = Alphabet::from("ABCD");
+        assert_eq!(alphabet.get_pos_offset('C',-1).unwrap(),3);
     }
 }
