8e49c845a77918c5bd0abd676690e53bb55e4a0a
SymmetricChaos <ajfraebel@gmail.com>
2022-05-31 07:42:26 -0400

    trying out grapheme clusters

--- src/text_aux/mod.rs
+++ src/text_aux/mod.rs
@@ -1,20 +1,20 @@
-// pub mod alphabet;
-// pub use alphabet::VecString;
-
 pub mod vecstring;
 pub use vecstring::VecString;
+
+// pub mod vecstring_ex;
+// pub use vecstring_ex::VecStringExtended;
 
 pub mod preset_alphabet;
 pub use preset_alphabet::PresetAlphabet;
 
 pub mod text_functions;
 pub use text_functions::{
-    dedup_alphabet, // should be unnecessary due to VecString
+    dedup_alphabet,
     keyed_alphabet,
     prep_text, // should be made more flexible
     random_char_vec,
     random_sample_replace,
     rank_str,
     shuffled_str,
-    validate_alphabet, // this needs to be changed to work with VecString
+    validate_alphabet,
 };
