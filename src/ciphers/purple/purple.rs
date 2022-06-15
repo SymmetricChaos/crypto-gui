3eedce85f0af883a1f90539b497e096f9e60383d
SymmetricChaos <ajfraebel@gmail.com>
2022-06-15 19:32:37 -0400

    moved purple and added wiring

---
+++ src/ciphers/purple/purple.rs
@@ -0,0 +1,42 @@
+// a reference implementation: https://github.com/gremmie/purple/tree/master/purple
+pub struct Switches {
+    sixes: usize,
+    twenties_1: usize,
+    twenties_2: usize,
+    twenties_3: usize,
+}
+
+impl Switches {
+    pub fn step(&mut self, c: char) -> char {
+        todo!("")
+    }
+
+    pub fn pass_signal(&mut self, c: char) -> char {
+        todo!("")
+    }
+}
+
+
+// pub struct Purple {
+//     switches: Switches, // this will be cloned during execution and then mutated
+//     input_plugboard: Plugboard,
+//     output_plugboard: Plugboard,
+// }
+
+// impl Purple {
+//     const SIXES: &'static str = "AEIOUY";
+//     const TWENTIES: &'static str = "BCDFGHJKLMNPQRSTVWXZ";
+    
+    
+// }
+
+// impl Cipher for Purple {
+//     fn encrypt(&self, text: &str) -> Result<String,CipherError> {
+//         todo!("
+//         for each symbol of text:
+//             pass the signal through the input_plugboard
+//             pass the signal through the sixes or twenties
+//             pass the signal through the output plugboard
+//         ")
+//     }
+// }
