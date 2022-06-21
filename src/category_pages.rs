3a240de654bfd4d9b880f1193c75f3a13c15c60d
SymmetricChaos <ajfraebel@gmail.com>
2022-06-21 09:31:15 -0400

    Update category_pages.rs

--- src/category_pages.rs
+++ src/category_pages.rs
@@ -18,8 +18,8 @@ impl CipherCategory {
             CipherCategory::Polyalphabetic => "Polyalphabetic ciphers are a broad category of substitution ciphers in which the substitution rule changes with every symbol enciphered. Rotor machines could also be classified here but have their own section. Simple polyaphabetic ciphers just repeat their key and can be attacked by identifying the period and then solving them by parts.\n\nThe first polyaphabetic ciphers received an incorrect reputation for being unbreakable due to their ability to defeat all forms of attack developed for simple substitution. Much later, in 1945, Claude Shannon proved that if the key were truly random and was never reused a polyalphabetic cipher was in fact immune to all forms of cryptanalysis. However huge amounts of high quality random data are difficult to produce and even more difficult to communicate making this system, called a one-time pad usable only in rare scenarios.",
             CipherCategory::RotorMachine => "Rotor Machines were the premier information security technology in the middle of the 20th century including the famous Enigma Machine. They are all electromechanical devices that rely on rotors to change their internal wiring each time a letter is typed. In the simplest form, such as the Hebern Machine, this is simply a Vigenere cipher with an extremely long period. The regular movement of the rotors and the fact that most never move at all is a serious design flaw that exposes such a device to attacks similar to those used against the Vigenere cipher. The machines built for military used additions that confounded anyone relying on this such as the Enigma's plugboard and the SIGABA's stepping maze. Though rendered technically obsolete by in 1970s there were some rotor machines still in use into the late 1980s.",
             CipherCategory::Transposition => "Transposition ciphers disguise text by changing the positions of the letters in it. Unlike other kinds of ciphers this means that a shorter plaintext is less secure than a long one. Although transposition theoretically has a very large key-space, growing exponentially as the size of the text increases, in practice it is very difficult to communicate an arbitrary permutation of some text as a key so the key-space is fairly small. The most powerful use of transposition is to combine it with some other kind of cipher as the transposition will destroy patterns in the plaintext the attacker might otherwise take advantage of.",
-            CipherCategory::Playfair => "NEED DESCRIPTION",
-            CipherCategory::Tactical => "Tactical Ciphers are designed for use on an active battlefield where there is a need to communicate tactical information quickly but without revealing it to the enemy. The Playfair cipher was also intended as a tactical cipher but has its own section due to the existence of a family of related ciphers. In general tactical ciphers gain security from three places: First tactical information is not relevant for long and thus by the time cryptanalysis can be applied should not help the enemy. Second messages are kept short, limiting the amount of text available to an attacker. Third the key is changed frequently, in both examples a new message key is picked for every transmission and a new page is switched to every few hours.",
+            CipherCategory::Playfair => "The Playfair cipher was developed by Charles Wheatstone but is named for its promoter, Lord Playfair. All members of the Playfair family operate on pairs of symbols rather than encrypting one symbol at a time. This complicated frequency analysis that allowed easy attacks on simpler substitution ciphers but clear weaknesses lead to the creation of the other members of the family.",
+            CipherCategory::Tactical => "Tactical Ciphers are designed for use on an active battlefield where there is a need to communicate tactical information quickly but without revealing it to the enemy. The Playfair cipher was also intended as a tactical cipher but has its own section due to the existence of a family of related ciphers. In general tactical ciphers gain security from three places: First tactical information is not relevant for long and thus by the time cryptanalysis can be applied decryption should not help the enemy. Second messages are kept short, limiting the amount of text available to an attacker. Third the key is changed frequently, in both examples a new message key is picked for every transmission and a new page is switched to every few hours.",
             CipherCategory::Polybius => "The Polybius Square is a very slight modification of a simple substitution cipher which encodes each symbol as a pair of symbols. The idea can be extended to a Polybius cube, provided among the examples, or even generalized to arbitrary Polybius hypercubes. On their own none of these provide any increase in security over ordinary substitution. Instead the other members of this family of ciphers use the Polybius square followed by a transposition in order to produce much strong composite ciphers. The information from a single symbol becomes diffused across multiple symbols of the ciphertext.",
         }
     }
@@ -44,7 +44,11 @@ impl CipherCategory {
                 CipherID::Chaocipher,
                 CipherID::Hutton,
             ],
-            CipherCategory::RotorMachine => &[CipherID::M209, CipherID::Enigma, CipherID::Sigaba],
+            CipherCategory::RotorMachine => &[
+                CipherID::M209, 
+                CipherID::Enigma, 
+                CipherID::Sigaba
+            ],
             CipherCategory::Transposition => &[
                 CipherID::Columnar,
                 CipherID::Grille,
@@ -58,7 +62,11 @@ impl CipherCategory {
                 CipherID::TwoSquare,
                 CipherID::FourSquare,
             ],
-            CipherCategory::Tactical => &[CipherID::Batco, CipherID::Dryad, CipherID::Rs44],
+            CipherCategory::Tactical => &[
+                CipherID::Batco, 
+                CipherID::Dryad, 
+                CipherID::Rs44
+            ],
             CipherCategory::Polybius => &[
                 CipherID::Polybius,
                 CipherID::PolybiusCube,
