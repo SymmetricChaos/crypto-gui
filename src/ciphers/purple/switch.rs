831fe6de6007dd04f7154f1cab72f236dcd7d9a3
SymmetricChaos <ajfraebel@gmail.com>
2022-07-13 15:48:39 -0400

    default speeds

--- src/ciphers/purple/switch.rs
+++ src/ciphers/purple/switch.rs
@@ -1,12 +1,12 @@
 use super::wiring::*;
-
+ 
 #[derive(PartialEq, Clone, Copy)]
 pub enum SwitchSpeed {
     Slow,
     Middle,
     Fast
 }
-
+ 
 #[derive(Clone, Copy)]
 pub struct Switch<const N: usize> {
     pub position: usize,
@@ -14,34 +14,34 @@ pub struct Switch<const N: usize> {
     wiring_enc: &'static [[usize; N]; 25],
     wiring_dec: &'static [[usize; N]; 25],
 }
-
+ 
 impl<const N: usize> Switch<N> {
-
-    pub fn new(wiring_enc: &'static [[usize; N]; 25], wiring_dec: &'static [[usize; N]; 25]) -> Switch<N> {
-        Self { position: 0, speed: SwitchSpeed::Slow, wiring_enc, wiring_dec }
+ 
+    pub fn new(speed: SwitchSpeed, wiring_enc: &'static [[usize; N]; 25], wiring_dec: &'static [[usize; N]; 25]) -> Switch<N> {
+        Self { position: 0, speed, wiring_enc, wiring_dec }
     }
-
+ 
     pub fn sixes() -> Switch<6_usize> {
-        Switch::new(&SIXES_ENC, &SIXES_DEC)
+        Switch::new(SwitchSpeed::Fast, &SIXES_ENC, &SIXES_DEC)
     }
-
+ 
     pub fn twenties() -> [Switch<20_usize>; 3] {
-        
-        let t1 = Switch::new(&TWENTIES_1_ENC, &TWENTIES_1_DEC);
-        let t2 = Switch::new(&TWENTIES_2_ENC, &TWENTIES_2_DEC);
-        let t3 = Switch::new(&TWENTIES_3_ENC, &TWENTIES_3_DEC);
-
+ 
+        let t1 = Switch::new(SwitchSpeed::Slow, &TWENTIES_1_ENC, &TWENTIES_1_DEC);
+        let t2 = Switch::new(SwitchSpeed::Middle, &TWENTIES_2_ENC, &TWENTIES_2_DEC);
+        let t3 = Switch::new(SwitchSpeed::Fast, &TWENTIES_3_ENC, &TWENTIES_3_DEC);
+ 
         [t1, t2, t3]
     }
-
+ 
     pub fn step(&mut self) {
         self.position = (self.position + 1) % 25
     }
-
+ 
     pub fn encrypt(&self, n: usize) -> usize {
         self.wiring_enc[self.position][n]
     }
-
+ 
     pub fn decrypt(&self, n: usize) -> usize {
         self.wiring_dec[self.position][n]
     }
