e989ab4cea57d73e40ec6d51af552fb8ac800765
SymmetricChaos <ajfraebel@gmail.com>
2022-07-21 17:39:33 -0400

    purple now encrypts and decrypts correcttly

--- src/ciphers/purple/purple.rs
+++ src/ciphers/purple/purple.rs
@@ -37,9 +37,9 @@ impl Switches {
         if spos == 23 && mpos == 24 {
             self.get_switch(SwitchSpeed::Slow).step();
         } else if spos == 24 {
-            self.get_switch(SwitchSpeed::Fast).step();
-        } else {
             self.get_switch(SwitchSpeed::Middle).step();
+        } else {
+            self.get_switch(SwitchSpeed::Fast).step();
         }
     }
 
@@ -47,9 +47,9 @@ impl Switches {
         if n < 6 {
             self.sixes.encrypt(n)
         } else {
-            let n = self.twenties[2].encrypt(n - 6);
+            let n = self.twenties[0].encrypt(n - 6);
             let n = self.twenties[1].encrypt(n);
-            self.twenties[0].encrypt(n)+6
+            self.twenties[2].encrypt(n)+6
         }
     }
 
@@ -57,9 +57,9 @@ impl Switches {
         if n < 6 {
             self.sixes.decrypt(n)
         } else {
-            let n = self.twenties[0].decrypt(n - 6);
+            let n = self.twenties[2].decrypt(n - 6);
             let n = self.twenties[1].decrypt(n);
-            self.twenties[2].decrypt(n)+6
+            self.twenties[0].decrypt(n)+6
         }
     }
 
@@ -95,34 +95,12 @@ lazy_static! {
     pub static ref PURPLE_ALPHABET: VecString = VecString::from("AEIOUYBCDFGHJKLMNPQRSTVWXZ");
 }
 
-// impl Purple {
-//     fn text_to_nums(text: &str) -> Result<Vec<usize>, CipherError> {
-//         let mut out = Vec::with_capacity(text.len());
-//         for c in text.chars() {
-//             let n = PURPLE_ALPHABET
-//                 .get_pos(c)
-//                 .ok_or(CipherError::input("invalid character"))?;
-//             out.push(n);
-//         }
-//         Ok(out)
-//     }
-
-//     fn nums_to_text(nums: Vec<usize>) -> Result<String, CipherError> {
-//         let mut out = String::with_capacity(nums.len());
-//         for n in nums {
-//             let c = PURPLE_ALPHABET
-//                 .get_char(n)
-//                 .ok_or(CipherError::input("invalid character"))?;
-//             out.push(c);
-//         }
-//         Ok(out)
-//     }
-// }
-
 impl Cipher for Purple {
     fn encrypt(&self, text: &str) -> Result<String, CipherError> {
         // convert kana to romaji if needed
-        let text = to_romaji_ks(text);
+        //let text = to_romaji_ks(text);
+
+
 
         // Clone switches then encrypt letters one by one, stepping each time
         let mut switches = self.switches.clone();
@@ -130,7 +108,7 @@ impl Cipher for Purple {
         for c in text.chars() {
             let n = self.plugboard.get(&c).ok_or(CipherError::input("invalid character"))?;
             let encrypted = switches.encrypt_num(*n);
-            out.push(PURPLE_ALPHABET.get_char_at(encrypted).ok_or(CipherError::input("invalid character"))?);
+            out.push(*self.plugboard_inv.get(&encrypted).ok_or(CipherError::input("invalid character"))?);
             switches.step();
         }
 
@@ -145,9 +123,9 @@ impl Cipher for Purple {
         let mut switches = self.switches.clone();
         let mut out = String::with_capacity(text.len());
         for c in text.chars() {
-            let n = PURPLE_ALPHABET.get_pos(c).ok_or(CipherError::input("invalid character"))?;
-            let decrypted = switches.decrypt_num(n);
-            out.push(*self.plugboard_inv.get(&decrypted).ok_or(CipherError::input("invalid character"))?);
+            let n = self.plugboard.get(&c).ok_or(CipherError::input("invalid character"))?;
+            let encrypted = switches.decrypt_num(*n);
+            out.push(*self.plugboard_inv.get(&encrypted).ok_or(CipherError::input("invalid character"))?);
             switches.step();
         }
 
@@ -168,41 +146,20 @@ mod purple_tests {
 
     use super::*;
 
-    // const PLAINTEXT: &'static str = "ZTXODNWKCCMAVNZXYWEETUQTCIMNVEUVIWBLUAXRRTLVA";
-    // const CIPHERTEXT: &'static str = "FOVTATAKIDASINIMUIMINOMOXIWOIRUBESIFYXXFCKZZR";
-
-    // Checked by hand for Purple::default()
-    // N will go through the Sixes
-    const PLAINTEXT_V: &'static str = "N";
-    const CIPHERTEXT_V: &'static str = "Y";
-
-    // Checked by hand for Purple::default(), but this contradicts the tests for the reference, decryption also fails
-    // X will go through the Twenties
-    const PLAINTEXT_C: &'static str = "X";
-    const CIPHERTEXT_C: &'static str = "F";
+    const PLAINTEXT: &'static str = "FOVTATAKIDASINIMUIMINOMOXIWOIRUBESIFYXXFCKZZR";
+    const CIPHERTEXT: &'static str = "ZTXODNWKCCMAVNZXYWEETUQTCIMNVEUVIWBLUAXRRTLVA";
 
     #[test]
-    fn encrypt_vowel() {
+    fn encrypt() {
         let cipher = Purple::default();
-        assert_eq!(cipher.encrypt(PLAINTEXT_V).unwrap(), CIPHERTEXT_V);
+        assert_eq!(cipher.encrypt(PLAINTEXT).unwrap(), CIPHERTEXT);
     }
 
     #[test]
-    fn decrypt_vowel() {
+    fn decrypt() {
         let cipher = Purple::default();
-        assert_eq!(cipher.decrypt(CIPHERTEXT_V).unwrap(), PLAINTEXT_V);
+        assert_eq!(cipher.decrypt(CIPHERTEXT).unwrap(), PLAINTEXT);
     }
 
-    #[test]
-    fn encrypt_consonant() {
-        let cipher = Purple::default();
-        assert_eq!(cipher.encrypt(PLAINTEXT_C).unwrap(), CIPHERTEXT_C);
-    }
-
-    #[test]
-    fn derypt_consonant() {
-        let cipher = Purple::default();
-        assert_eq!(cipher.decrypt(CIPHERTEXT_C).unwrap(), PLAINTEXT_C);
-    }
 
 }
