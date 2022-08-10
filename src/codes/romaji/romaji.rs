a03a7e931be78d364e6810fce336df4385e2d496
SymmetricChaos <ajfraebel@gmail.com>
2022-08-10 07:27:06 -0400

    reference instead of clone

--- src/codes/romaji/romaji.rs
+++ src/codes/romaji/romaji.rs
@@ -1,6 +1,6 @@
 use crate::tokenizer::{Node, TokenError};
 
-pub fn to_romaji(orig: &str, tree: Node) -> Result<String, TokenError> {
+pub fn to_romaji(orig: &str, tree: &Node) -> Result<String, TokenError> {
     let tokens = tree.extract_tokens(orig)?;
 
     Ok(tokens.iter().cloned().collect())
@@ -18,7 +18,7 @@ mod romaji_test {
     fn hepbern_text() {
         let latin_hs = "CHI SHI TSU JI JI ZU JA JA KONNI KON'O";
         assert_eq!(
-            to_romaji(HIRAGANA, HEPBERN_SHIKI.clone()).unwrap(),
+            to_romaji(HIRAGANA, &HEPBERN_SHIKI).unwrap(),
             latin_hs
         );
     }
@@ -26,12 +26,12 @@ mod romaji_test {
     #[test]
     fn nihon_text() {
         let latin_ns = "TI SI TU DI ZI DU DYA ZYA KONNI KON'O";
-        assert_eq!(to_romaji(HIRAGANA, NIHON_SHIKI.clone()).unwrap(), latin_ns);
+        assert_eq!(to_romaji(HIRAGANA, &NIHON_SHIKI).unwrap(), latin_ns);
     }
 
     #[test]
     fn kunrei_text() {
         let latin_ks = "TI SI TU ZI ZI ZU ZYA ZYA KONNI KON'O";
-        assert_eq!(to_romaji(HIRAGANA, KUNREI_SHIKI.clone()).unwrap(), latin_ks);
+        assert_eq!(to_romaji(HIRAGANA, &KUNREI_SHIKI).unwrap(), latin_ks);
     }
 }
