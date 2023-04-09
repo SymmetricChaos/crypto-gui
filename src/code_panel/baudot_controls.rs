8a9d6611c464379a8ede86be4ac7d79041dfa92d
SymmetricChaos <ajfraebel@gmail.com>
2023-04-09 08:41:06 -0400

    Update baudot_controls.rs

--- src/code_panel/baudot_controls.rs
+++ src/code_panel/baudot_controls.rs
@@ -1,10 +1,14 @@
 use super::{generic_components::fill_code_columns, View, ViewableCode};
-use crate::codes::Baudot;
+use crate::codes::{baudot::BaudotVersion, Baudot};
 
 impl ViewableCode for Baudot {}
 
 impl View for Baudot {
     fn ui(&mut self, ui: &mut eframe::egui::Ui, _errors: &mut String) {
+        ui.selectable_value(&mut self.version, BaudotVersion::Ita1, "ITA1 (Baudot)");
+        ui.selectable_value(&mut self.version, BaudotVersion::Ita2, "ITA2 (Murray)");
+        ui.selectable_value(&mut self.version, BaudotVersion::UsTty, "US TTY");
+        ui.add_space(16.0);
         fill_code_columns(32, 4, ui, self.codes_chars());
     }
 }
