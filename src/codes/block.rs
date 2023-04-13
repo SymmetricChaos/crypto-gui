c8d7725f924a7db57cc717aaa0b21d5ee8af65aa
SymmetricChaos <ajfraebel@gmail.com>
2023-04-13 11:09:24 -0400

    better reporting and error handling

--- src/codes/block.rs
+++ src/codes/block.rs
@@ -27,7 +27,9 @@ fn to_str_radix(n: usize, radix: usize, width: usize, symbols: &VecString) -> St
 
 pub struct BlockCode {
     pub width: usize,
+    pub alphabet_string: String,
     pub alphabet: VecString,
+    pub symbol_string: String,
     pub symbols: VecString,
 }
 
@@ -35,23 +37,35 @@ impl Default for BlockCode {
     fn default() -> Self {
         BlockCode {
             width: 5,
+            alphabet_string: String::from(PresetAlphabet::BasicLatin),
             alphabet: VecString::from(PresetAlphabet::BasicLatin),
+            symbol_string: String::from("01"),
             symbols: VecString::from("01"),
         }
     }
 }
 
 impl BlockCode {
+    pub fn set_symbols(&mut self) {
+        self.symbols = VecString::unique_from(&self.symbol_string);
+    }
+
+    pub fn set_alphabet(&mut self) {
+        self.alphabet = VecString::unique_from(&self.alphabet_string);
+    }
+
     fn num_to_string(&self, n: usize) -> String {
         to_str_radix(n, self.symbols.len(), self.width, &self.symbols)
     }
 
-    pub fn decrease_width(&mut self) {
-        self.width = self.width.sub(1).clamp(2, 8)
+    pub fn decrease_width(&mut self) -> Result<(), Error> {
+        self.width = self.width.sub(1).clamp(2, 8);
+        self.check_code_width()
     }
 
-    pub fn increase_width(&mut self) {
-        self.width = self.width.add(1).clamp(2, 8)
+    pub fn increase_width(&mut self) -> Result<(), Error> {
+        self.width = self.width.add(1).clamp(2, 8);
+        self.check_code_width()
     }
 
     pub fn chars_codes(&self) -> Box<dyn Iterator<Item = (char, String)> + '_> {
@@ -59,14 +73,31 @@ impl BlockCode {
             self.alphabet
                 .chars()
                 .enumerate()
-                .map(|(n, c)| (c, self.num_to_string(n))),
+                .map(|(n, c)| (c, self.num_to_string(n)))
+                .take(self.max_codes()),
         )
     }
 
-    pub fn valid_code_width(&self) -> bool {
-        let n_symbols = self.alphabet.chars().count();
-        let min_width = (n_symbols as f32).log(self.symbols.len() as f32).ceil() as usize;
-        min_width < self.width
+    fn max_codes(&self) -> usize {
+        self.symbols.len().pow(self.width as u32)
+    }
+
+    fn min_code_width(&self) -> usize {
+        (self.alphabet.len() as f32)
+            .log(self.symbols.len() as f32)
+            .ceil() as usize
+    }
+
+    pub fn check_code_width(&self) -> Result<(), Error> {
+        let min_width = self.min_code_width();
+        if min_width < self.symbols.len() {
+            Err(Error::State(format!(
+                "a width of {} is needed to represent the entire alphabet",
+                min_width
+            )))
+        } else {
+            Ok(())
+        }
     }
 }
 
@@ -78,6 +109,9 @@ impl Code for BlockCode {
                 .alphabet
                 .get_pos(c)
                 .ok_or_else(|| Error::invalid_input_char(c))?;
+            if n > self.max_codes() {
+                return Err(Error::invalid_input_char(c));
+            }
             out.push(self.num_to_string(n));
         }
         Ok(out.join(""))
@@ -96,7 +130,14 @@ impl Code for BlockCode {
             val += n * self.symbols.len().pow(p as u32);
 
             if p == 0 {
-                out.push(self.alphabet[val]);
+                if val > self.max_codes() {
+                    return Err(Error::input("unable to decode"));
+                }
+                out.push(
+                    self.alphabet
+                        .get_char_at(val)
+                        .ok_or(Error::input("unable to decode"))?,
+                );
                 val = 0;
             }
         }
