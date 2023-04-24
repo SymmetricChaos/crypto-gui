a31661ac2f56654ec24ee5b4e149374a6521fa0b
SymmetricChaos <ajfraebel@gmail.com>
2023-04-24 08:59:00 -0400

    file dialogues for some binary to text re-enabled

---
+++ src/code_panel/ascii85_controls.rs
@@ -0,0 +1,42 @@
+use super::{
+    generic_components::{binary_to_text_input_mode, fill_code_columns},
+    View, ViewableCode,
+};
+use crate::codes::binary_to_text::ascii85::{Ascii85, Ascii85Variant};
+use rfd::FileDialog;
+
+impl ViewableCode for Ascii85 {}
+
+impl View for Ascii85 {
+    fn ui(&mut self, ui: &mut eframe::egui::Ui, _errors: &mut String) {
+        ui.selectable_value(&mut self.variant, Ascii85Variant::Btoa, "btoa");
+        ui.selectable_value(&mut self.variant, Ascii85Variant::Ipv6, "IPv6");
+        ui.selectable_value(&mut self.variant, Ascii85Variant::ZeroQm, "Z85 (ZeroQM)");
+        ui.add_space(10.0);
+        match self.variant {
+            Ascii85Variant::Btoa => ui.label("The original Ascii85 encoding created for the btoa (binary-to-ASCII) utility simply uses the printing ASCII characters from '!' to 'u' in order. For efficiency with real world inputs it has two special encodings: 0x00000000 (the all zero word) is encoded as just 'z' while 0x20202020 (the sequence of four ASCII spaces) is encoded as just 'y'."),
+            Ascii85Variant::Ipv6 => ui.label("While it can be used for arbitary data this variant was created to allow encoding for IPv6 addresses. For this use the 85 character set is the most efficient possible. The selection of characters is different in order to avoid conflicts."),
+            Ascii85Variant::ZeroQm => ui.label("Z85 variant deliberately excludes the the ASCII quote characters to make the encoding safe to used in quoted strings and thus much easier to include in source code. While no special encoding is used for the all zero word (0x00000000) the placement of zero as the first digit means that runs of zero bytes appear as runs of zeroes in encoded text."),
+        };
+        ui.add_space(10.0);
+        binary_to_text_input_mode(ui, &mut self.mode);
+        ui.add_space(10.0);
+        ui.label("You can upload a file and encode its binary data as text. Decoding files is not supported as it is impossible to know the contents.");
+        if ui.button("Upload File").clicked() {
+            self.file = FileDialog::new().pick_file();
+        }
+        if self.file.is_some() {
+            let file_name = self.file.as_ref().unwrap().file_name().unwrap().to_str();
+            ui.add_space(10.0);
+            ui.label(format!("{}", file_name.unwrap()));
+            if ui.button("Download Encoded File").clicked() {
+                let target_file = FileDialog::new().add_filter("", &[".txt"]).save_file();
+                if let Some(file) = target_file {
+                    std::fs::write(file, self.encode_file().unwrap()).unwrap()
+                }
+            }
+        }
+        ui.add_space(32.0);
+        fill_code_columns(17, 5, ui, Box::new(self.chars_codes()));
+    }
+}
--- src/code_panel/base64_controls.rs
+++ src/code_panel/base64_controls.rs
@@ -1,18 +1,14 @@
-use super::{generic_components::fill_code_columns, View, ViewableCode};
-use crate::codes::{
-    binary_to_text::{base64::B64Variant, BinaryToTextMode},
-    Base64,
+use super::{
+    generic_components::{binary_to_text_input_mode, fill_code_columns},
+    View, ViewableCode,
 };
-//use rfd::FileDialog;
+use crate::codes::{binary_to_text::base64::B64Variant, Base64};
+use rfd::FileDialog;
 
 impl ViewableCode for Base64 {}
 
 impl View for Base64 {
     fn ui(&mut self, ui: &mut eframe::egui::Ui, _errors: &mut String) {
-        // ui.label("You can upload a file and encode its binary data in Base64. This website does not allow decoding of arbitrary inputs because it is impossible to know their contents before decoding, which is potentially dangerous.");
-        // if ui.button("Select a File").clicked() {
-        //     self.file = FileDialog::new().pick_file();
-        // }
         ui.selectable_value(&mut self.variant, B64Variant::Standard, "Standard");
         ui.selectable_value(
             &mut self.variant,
@@ -27,25 +23,29 @@ impl View for Base64 {
             B64Variant::UrlSafe => ui.label("URL and Filename Safe variant is defioned in RFC 4684 section 5 to be used in situations where the + and / characters might have special use defined for them. They are replaced by - and _."),
         };
         ui.add_space(10.0);
-        ui.label("Encoding Mode");
-        ui.selectable_value(&mut self.mode, BinaryToTextMode::Hex, "Hex")
-            .on_hover_text("interpret input as hexcode");
-        ui.selectable_value(&mut self.mode, BinaryToTextMode::Utf8, "Text")
-            .on_hover_text("convert text to raw bytes");
+        ui.add_space(10.0);
+        binary_to_text_input_mode(ui, &mut self.mode);
+        ui.add_space(10.0);
         ui.add_space(10.0);
         ui.label("When padding is enabled the padding symbol `=` is added to the end until the length is a multiple of three. Padding is ignored when decoding.");
         ui.checkbox(&mut self.use_padding, "Use Padding");
         ui.add_space(10.0);
+        ui.label("You can upload a file and encode its binary data as text. Decoding files is not supported as it is impossible to know the contents.");
+        if ui.button("Upload File").clicked() {
+            self.file = FileDialog::new().pick_file();
+        }
+        if self.file.is_some() {
+            let file_name = self.file.as_ref().unwrap().file_name().unwrap().to_str();
+            ui.add_space(10.0);
+            ui.label(format!("{}", file_name.unwrap()));
+            if ui.button("Download Encoded File").clicked() {
+                let target_file = FileDialog::new().add_filter("", &[".txt"]).save_file();
+                if let Some(file) = target_file {
+                    std::fs::write(file, self.encode_file().unwrap()).unwrap()
+                }
+            }
+        }
+        ui.add_space(32.0);
         fill_code_columns(16, 4, ui, Box::new(self.chars_codes()));
-        ui.add_space(10.0);
-        // if self.file.is_some() {
-        //     let file_name = self.file.as_ref().unwrap().file_name().unwrap().to_str();
-        //     ui.add_space(10.0);
-        //     ui.label(format!("File: {}", file_name.unwrap()));
-        //     ui.label("\nBase 64 Encoding\n");
-        //     if ui.button("Copy to Clipboard").clicked() {
-        //         ui.output_mut(|o| o.copied_text = self.encode_file().unwrap());
-        //     }
-        // }
     }
 }
