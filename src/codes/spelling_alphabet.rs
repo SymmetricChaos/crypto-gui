f15802b8a7b93a6ab089208196edbfc455aac3f5
SymmetricChaos <ajfraebel@gmail.com>
2023-05-08 07:51:15 -0400

    update spelling alphabets

--- src/codes/spelling_alphabet.rs
+++ src/codes/spelling_alphabet.rs
@@ -11,29 +11,27 @@ lazy_static! {
     // Yes, ALFA and JULIETT are meant to be spelled that way
     // Yes, the spelling of the numerals is correct even though the pronunciation is different
     pub static ref NATO: BiMap<char,&'static str> = bimap_from_iter(
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".chars().zip([
+        PresetAlphabet::BasicLatin.chars().zip([
             "ALFA", "BRAVO", "CHARLIE", "DELTA", "ECHO", "FOXTROT",
             "GOLF", "HOTEL", "INDIA", "JULIETT", "KILO", "LIMA",
             "MIKE", "NOVEMBER", "OSCAR", "PAPA", "QUEBEC", "ROMEO",
             "SIERRA", "TANGO", "UNIFORM", "VICTOR", "WHISKEY",
-            "XRAY", "YANKEE", "ZULU", "ZERO", "ONE", "TWO", "THREE", "FOUR",
-            "FIVE", "SIX", "SEVEN", "EIGHT", "NINE"].into_iter())
+            "XRAY", "YANKEE", "ZULU"].into_iter())
     );
 
 
     pub static ref CCB: BiMap<char,&'static str> = bimap_from_iter(
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".chars().zip([
+        PresetAlphabet::BasicLatin.chars().zip([
             "ABLE", "BAKER", "CHARLIE", "DOG", "EASY", "FOX",
             "GEORGE", "HOW", "ITEM", "JIG", "KING", "LOVE",
             "MIKE", "NAN", "OBOE", "PETER", "QUEEN", "ROGER",
             "SUGAR", "TARE", "UNCLE", "VICTOR", "WILLIAM",
-            "XRAY", "YOKE", "ZEBRA", "ZERO", "ONE", "TWO", "THREE", "FOUR",
-            "FIVE", "SIX", "SEVEN", "EIGHT", "NINE"].into_iter())
+            "XRAY", "YOKE", "ZEBRA"].into_iter())
 
     );
 
     pub static ref WESTERN_UNION_1912: BiMap<char,&'static str> = bimap_from_iter(
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ".chars().zip([
+        PresetAlphabet::BasicLatin.chars().zip([
             "ADAMS", "BOSTON", "CHICAGO", "DENVER", "EASY", "FRANK",
             "GEORGE", "HENRY", "IDA", "JERSEY", "KING", "LINCOLN",
             "MARY", "NEWARK", "OCEAN", "PETER", "QUEEN", "ROGER",
@@ -42,7 +40,7 @@ lazy_static! {
     );
 
     pub static ref WESTERN_UNION_1942: BiMap<char,&'static str> = bimap_from_iter(
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ".chars().zip([
+        PresetAlphabet::BasicLatin.chars().zip([
             "ADAMS", "BOSTON", "CHICAGO", "DENVER", "EASY", "FRANK",
             "GEORGE", "HENRY", "IDA", "JOHN", "KING", "LINCOLN",
             "MARY", "NEWYORK", "OCEAN", "PETER", "QUEEN", "ROGER",
@@ -51,7 +49,7 @@ lazy_static! {
     );
 
     pub static ref US_NAVY_1908: BiMap<char,&'static str> = bimap_from_iter(
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ".chars().zip([
+        PresetAlphabet::BasicLatin.chars().zip([
             "ACTOR", "BAKER", "CANTEEN", "DIVER", "EAGLE", "FISHER",
             "GANGWAY", "HALLIARD", "INSECT", "JOKCEY", "KNAPSACK", "LUGGER",
             "MUSKET", "NEPTUNE", "OYSTER", "PISTOL", "QUADRANT", "REEFER",
@@ -60,8 +58,8 @@ lazy_static! {
     );
 
 
-    pub static ref US_NAVY_1908_ALT: BiMap<char,&'static str> = bimap_from_iter(
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ".chars().zip([
+    pub static ref US_NAVY_1908_SHORT: BiMap<char,&'static str> = bimap_from_iter(
+        PresetAlphabet::BasicLatin.chars().zip([
             "ASH", "BACK", "CHAIN", "DOG", "EGG", "FOX",
             "GIG", "HORSE", "ICE", "JAKE", "KING", "LASH",
             "MULE", "NET", "OAK", "PAGE", "QUAIL", "RAFT",
@@ -70,12 +68,21 @@ lazy_static! {
     );
 
     pub static ref US_MILITARY_1941: BiMap<char,&'static str> = bimap_from_iter(
-        "ABCDEFGHIJKLMNOPQRSTUVWXYZ".chars().zip([
+        PresetAlphabet::BasicLatin.chars().zip([
             "ABLE", "BAKER", "CHARLIE", "DOG", "EASY", "FOX",
             "GEORGE", "HOW", "ITEM", "JIG", "KING", "LOVE",
             "MIKE", "NAN", "OBOE", "PETER", "QUEEN", "ROGER",
             "SAIL", "TARE", "UNCLE", "VICTOR", "WILLIAM",
             "XRAY", "YOKE", "ZEBRA"].into_iter())
+    );
+
+    pub static ref UK_ARMY_1904: BiMap<char,&'static str> = bimap_from_iter(
+        PresetAlphabet::BasicLatin.chars().zip([
+            "ACK", "BEER", "CORK", "DON", "EDDY", "FREDDY",
+            "GEORGE", "HARRY", "INK", "JUB", "KING", "LONDON",
+            "EMMA", "NUTS", "ORANGE", "PIP", "QUAD", "ROBERT",
+            "ESSES", "TOC", "UNCLE", "VIC", "WILLIAM",
+            "XERXES", "YELLOW", "ZEBRA"].into_iter())
     );
 }
 
@@ -88,17 +95,15 @@ pub enum SpellingAlphabetMode {
     Usn1908,
     Usn1908Alt,
     Us1941,
+    Uka1904,
     FirstLetter,
 }
 
 impl SpellingAlphabetMode {
     pub fn alphabet(&self) -> &str {
         match self {
-            Self::Nato | Self::Ccb => PresetAlphabet::BasicLatinWithDigits.slice(),
-            Self::Wu1912 | Self::Wu1942 | Self::Us1941 | Self::Usn1908 | Self::Usn1908Alt => {
-                PresetAlphabet::BasicLatin.slice()
-            }
             Self::FirstLetter => "",
+            _ => PresetAlphabet::BasicLatin.slice(),
         }
     }
 
@@ -110,7 +115,8 @@ impl SpellingAlphabetMode {
             Self::Wu1942 => WESTERN_UNION_1942.get_by_left(&c),
             Self::Us1941 => US_MILITARY_1941.get_by_left(&c),
             Self::Usn1908 => US_NAVY_1908.get_by_left(&c),
-            Self::Usn1908Alt => US_NAVY_1908_ALT.get_by_left(&c),
+            Self::Usn1908Alt => US_NAVY_1908_SHORT.get_by_left(&c),
+            Self::Uka1904 => UK_ARMY_1904.get_by_left(&c),
             Self::FirstLetter => None,
         }
     }
@@ -123,7 +129,8 @@ impl SpellingAlphabetMode {
             Self::Wu1942 => WESTERN_UNION_1942.get_by_right(s),
             Self::Us1941 => US_MILITARY_1941.get_by_right(s),
             Self::Usn1908 => US_NAVY_1908.get_by_right(s),
-            Self::Usn1908Alt => US_NAVY_1908_ALT.get_by_right(s),
+            Self::Usn1908Alt => US_NAVY_1908_SHORT.get_by_right(s),
+            Self::Uka1904 => UK_ARMY_1904.get_by_right(s),
             Self::FirstLetter => None,
         }
     }
