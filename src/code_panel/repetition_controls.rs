81a92c3b7594bcc3173e510df8dc82e86011b503
SymmetricChaos <ajfraebel@gmail.com>
2023-05-12 08:51:16 -0400

    Update repetition_controls.rs

--- src/code_panel/repetition_controls.rs
+++ src/code_panel/repetition_controls.rs
@@ -9,5 +9,17 @@ impl View for Repetition {
     fn ui(&mut self, ui: &mut eframe::egui::Ui, _errors: &mut String) {
         ui.label("Block Size");
         ui.add(Slider::new(&mut self.block_size, 3..=9));
+
+        ui.add_space(16.0);
+
+        if self.block_size % 2 == 0 {
+            ui.label(format!(
+                "Correct {}-bit errors\nDetect {}-bit errors",
+                self.block_size / 2 - 1,
+                self.block_size / 2
+            ));
+        } else {
+            ui.label(format!("Correct {}-bit errors", self.block_size / 2,));
+        }
     }
 }
