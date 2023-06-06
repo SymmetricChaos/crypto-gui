fe0c3a76982a39be0fb605754a692bff00b1b786
SymmetricChaos <ajfraebel@gmail.com>
2023-06-06 07:57:31 -0400

    Update hamming.rs

--- codes/src/ecc/hamming.rs
+++ codes/src/ecc/hamming.rs
@@ -1,10 +1,8 @@
 use crate::{ecc::check_bitstring, errors::CodeError, traits::Code};
 
 pub struct HammingCode {
-    pub parity_bits: usize,
+    pub parity_bits: u32,
 }
-
-impl HammingCode {}
 
 impl Default for HammingCode {
     fn default() -> Self {
@@ -12,14 +10,34 @@ impl Default for HammingCode {
     }
 }
 
+impl HammingCode {
+    pub fn total_bits(&self) -> u32 {
+        2_u32.pow(self.parity_bits) - 1
+    }
+
+    pub fn data_bits(&self) -> u32 {
+        self.total_bits() - self.parity_bits
+    }
+}
+
 impl Code for HammingCode {
     fn encode(&self, text: &str) -> Result<String, CodeError> {
+        if self.parity_bits > 6 || self.parity_bits < 2 {
+            return Err(CodeError::state(
+                "only parity bits from 2 to 6 are supported",
+            ));
+        }
         check_bitstring(text)?;
 
         todo!("match on self.parity bits")
     }
 
     fn decode(&self, text: &str) -> Result<String, CodeError> {
+        if self.parity_bits > 6 || self.parity_bits < 2 {
+            return Err(CodeError::state(
+                "only parity bits from 2 to 6 are supported",
+            ));
+        }
         check_bitstring(text)?;
 
         todo!("match on self.parity bits")
