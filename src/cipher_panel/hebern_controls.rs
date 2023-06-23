f74e02415148ca2b8303fc0ee3b10c51a9a44c25
SymmetricChaos <ajfraebel@gmail.com>
2023-06-23 16:09:21 -0400

    Update hebern_controls.rs

--- src/cipher_panel/hebern_controls.rs
+++ src/cipher_panel/hebern_controls.rs
@@ -1,8 +1,8 @@
 use super::CipherFrame;
 use crate::ui_elements::control_string;
 use ciphers::{hebern::Hebern, Cipher};
-use egui::{Slider, TextEdit, Ui};
-use utils::preset_alphabet::Alphabet;
+use egui::{Slider, Ui};
+use utils::{functions::keyed_alphabet, preset_alphabet::Alphabet};
 
 pub struct HebernFrame {
     cipher: Hebern,
@@ -23,49 +23,46 @@ impl CipherFrame for HebernFrame {
         ui.add_space(10.0);
         ui.label("Alphabet");
         if control_string(ui, &mut self.alphabet_string).changed() {
-            self.cipher.set_alphabet(&self.alphabet_string)
+            self.cipher.set_alphabet(&self.alphabet_string);
+            for rotor in self.cipher.rotors.rotors.iter_mut() {
+                rotor.wiring_str = keyed_alphabet(&mut rotor.wiring_str, &mut self.alphabet_string);
+                _ = rotor.set(&self.cipher.alphabet);
+            }
         }
 
         ui.add_space(10.0);
         ui.label("Rotor Wiring");
         for rotor in self.cipher.rotors.rotors.iter_mut() {
             ui.horizontal(|ui| {
-                ui.add_enabled(rotor.editable, TextEdit::singleline(&mut rotor.wiring_str));
-                if rotor.editable {
-                    if ui.small_button("save").clicked() {
-                        match rotor.set(&self.cipher.alphabet) {
-                            Ok(_) => {
-                                rotor.editable = false;
-                                rotor.error.clear();
-                            }
-                            Err(e) => rotor.error = e.inner(),
-                        }
-                    }
-                } else {
-                    if ui.small_button("edit").clicked() {
-                        rotor.editable = true;
-                    }
+                if control_string(ui, &mut rotor.wiring_str).lost_focus() {
+                    rotor.wiring_str =
+                        keyed_alphabet(&mut rotor.wiring_str, &mut self.alphabet_string);
+                    _ = rotor.set(&self.cipher.alphabet);
                 }
-                // if ui.small_button("random").clicked() {
-                //     match rotor.randomize(&self.cipher.alphabet) {
-                //         Ok(_) => rotor.error.clear(),
-                //         Err(e) => rotor.error = e.inner(),
-                //     }
-                // }
-                if ui.small_button("fill").clicked() {
-                    match rotor.fill(&self.cipher.alphabet) {
-                        Ok(_) => rotor.error.clear(),
-                        Err(e) => rotor.error = e.inner(),
-                    }
-                }
-                ui.label(&rotor.error);
             });
+        }
+
+        if ui.small_button("+").clicked() {
+            if self.cipher.rotors.rotors.len() <= 8 {
+                self.cipher.rotors.add_rotor(&self.cipher.alphabet);
+            }
+        }
+        if ui.small_button("-").clicked() {
+            if self.cipher.rotors.rotors.len() >= 2 {
+                self.cipher.rotors.del_rotor();
+            }
         }
 
         ui.add_space(10.0);
         ui.label("Rotor Positions\nTo Be Changed Every Message");
         for rotor in &mut self.cipher.rotors.rotors {
-            ui.add(Slider::new(&mut rotor.position, 0..=26).clamp_to_range(true));
+            ui.add(
+                Slider::new(
+                    &mut rotor.position,
+                    0..=self.alphabet_string.chars().count(),
+                )
+                .clamp_to_range(true),
+            );
         }
     }
 
