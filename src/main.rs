b1fcf272d54c7f2e6b2d2b94a25cdf129025c8e4
SymmetricChaos <ajfraebel@gmail.com>
2023-07-01 09:49:42 -0400

    update build

--- src/main.rs
+++ src/main.rs
@@ -9,35 +9,30 @@ pub mod pages;
 
 pub mod ui_elements;
 
+// When compiling natively:
 #[cfg(not(target_arch = "wasm32"))]
-fn main() {
+fn main() -> eframe::Result<()> {
     let native_options = eframe::NativeOptions::default();
     eframe::run_native(
         "Classic Cryptography",
         native_options,
         Box::new(|cc| Box::new(app::ClassicCrypto::build_with_context(cc))),
     )
-    .expect("failed to start eframe");
 }
 
-// when compiling to web using trunk.
+// When compiling to web using trunk:
 #[cfg(target_arch = "wasm32")]
 fn main() {
-    // Make sure panics are logged using `console.error`.
-    console_error_panic_hook::set_once();
-
-    // Redirect tracing to console.log and friends:
-    tracing_wasm::set_as_global_default();
-
     let web_options = eframe::WebOptions::default();
 
     wasm_bindgen_futures::spawn_local(async {
-        eframe::start_web(
-            "the_canvas_id", // hardcode it
-            web_options,
-            Box::new(|cc| Box::new(app::ClassicCrypto::build_with_context(cc))),
-        )
-        .await
-        .expect("failed to start eframe");
+        eframe::WebRunner::new()
+            .start(
+                "crypto_gui_canvas", // hardcode it
+                web_options,
+                Box::new(|cc| Box::new(app::ClassicCrypto::build_with_context(cc))),
+            )
+            .await
+            .expect("failed to start eframe");
     });
 }
