ad4bec37cd5f381a0b8853f57c2891fa799ec692
SymmetricChaos <ajfraebel@gmail.com>
2023-07-17 07:54:32 -0400

    Update plugboard_generic.rs

--- ciphers/src/substitution/plugboard_generic.rs
+++ ciphers/src/substitution/plugboard_generic.rs
@@ -1,8 +1,7 @@
+use crate::errors::CipherError;
 use itertools::zip;
 use std::collections::HashMap;
 use std::hash::Hash;
-
-use crate::errors::CipherError;
 
 pub struct Plugboard<A> {
     wiring: HashMap<A, A>,
@@ -19,21 +18,41 @@ impl<A: Hash + Eq + Clone> Default for Plugboard<A> {
 impl<A: Hash + Eq + Clone> Plugboard<A> {
     pub fn build(left: &[A], right: &[A]) -> Result<Plugboard<A>, CipherError> {
         if left.len() != right.len() {
-            return Err(CipherError::General(
-                "the lists of left and right entries must be the same length".into(),
+            return Err(CipherError::general(
+                "the lists of left and right entries must be the same length",
             ));
         }
         let mut wiring = HashMap::with_capacity(left.len());
         for (l, r) in zip(left, right) {
             if l == r || wiring.contains_key(l) || wiring.contains_key(r) {
-                return Err(CipherError::General(
-                    "plugboard inputs cannot form chains or cycles".into(),
+                return Err(CipherError::general(
+                    "plugboard inputs cannot form chains or cycles",
                 ));
             }
             wiring.insert(l.clone(), r.clone());
             wiring.insert(r.clone(), l.clone());
         }
         Ok(Plugboard { wiring })
+    }
+
+    pub fn rebuild(&mut self, left: &[A], right: &[A]) -> Result<(), CipherError> {
+        if left.len() != right.len() {
+            return Err(CipherError::general(
+                "the lists of left and right entries must be the same length",
+            ));
+        }
+        self.wiring.clear();
+
+        for (l, r) in zip(left, right) {
+            if l == r || self.wiring.contains_key(l) || self.wiring.contains_key(r) {
+                return Err(CipherError::general(
+                    "plugboard inputs cannot form chains or cycles",
+                ));
+            }
+            self.wiring.insert(l.clone(), r.clone());
+            self.wiring.insert(r.clone(), l.clone());
+        }
+        Ok(())
     }
 
     pub fn swap<'a>(&'a self, input: &A) -> Option<&A> {
