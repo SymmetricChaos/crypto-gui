14384ee1245dff8bbe4447fb388d44bfb2004629
SymmetricChaos <ajfraebel@gmail.com>
2023-07-23 12:34:52 -0400

    Update m_of_n_controls.rs

--- src/code_panel/m_of_n_controls.rs
+++ src/code_panel/m_of_n_controls.rs
@@ -29,8 +29,25 @@ impl CodeFrame for MofNCodeFrame {
         if ui.add(Slider::new(&mut self.code.weight, 1..=10)).changed() {
             self.code.weight = self.code.weight.clamp(1, self.code.length - 1);
         }
-
         ui.add_space(16.0);
+
+        // use egui_extras::{Column, TableBuilder};
+        // let table = TableBuilder::new(ui)
+        //     .striped(true)
+        //     .resizable(true)
+        //     .cell_layout(egui::Layout::left_to_right(egui::Align::Center))
+        //     .column(Column::initial(70.0).range(20.0..=300.0))
+        //     .column(Column::remainder())
+        //     .min_scrolled_height(0.0);
+        // table.body(|mut body| {
+        //     for code in self.code.list_codes().iter() {
+        //         body.row(20.0, |mut row| {
+        //             row.col(|ui| {
+        //                 ui.mono(code);
+        //             });
+        //         });
+        //     }
+        // });
     }
 
     fn code(&self) -> &dyn codes::traits::Code {
