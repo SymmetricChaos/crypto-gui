22c3be4dcad928fa8a9f49942e19ef3a7c5ef314
SymmetricChaos <ajfraebel@gmail.com>
2023-09-10 08:47:04 -0400

    full key derivation

--- ciphers/src/vic.rs
+++ ciphers/src/vic.rs
@@ -1,7 +1,7 @@
 use itertools::Itertools;
-use utils::{preset_alphabet::Alphabet, text_functions::rank_str, vecstring::VecString};
+use utils::{preset_alphabet::Alphabet, text_functions::rank_str};
 
-use crate::{Cipher, CipherError};
+use crate::{transposition::Columnar, Cipher, CipherError};
 
 pub struct Vic {
     alphabet: String,
@@ -25,23 +25,24 @@ impl Vic {
     }
 
     fn chain_addition(text: &str, n: usize) -> String {
-        let mut v = text.chars().map(|c| u32::from(c) - 48).collect_vec();
+        let mut v = text.chars().map(|c| Self::char_to_digit(c)).collect_vec();
         for i in 0..n {
             let t = (v[i] + v[i + 1]) % 10;
             v.push(t)
         }
         v.into_iter()
+            .skip(text.chars().count())
             .map(|d| char::from_digit(d, 10).unwrap())
             .join("")
     }
 
     fn digital_addition(a: char, b: char) -> char {
-        let t = ((u32::from(a) - 48) + (u32::from(b) - 48)) % 10;
+        let t = (Self::char_to_digit(a) + Self::char_to_digit(b)) % 10;
         char::from_digit(t, 10).unwrap()
     }
 
     fn digital_subtraction(a: char, b: char) -> char {
-        let t = (10 + (u32::from(a) - 48) - (u32::from(b) - 48)) % 10;
+        let t = (10 + Self::char_to_digit(a) - Self::char_to_digit(b)) % 10;
         char::from_digit(t, 10).unwrap()
     }
 
@@ -55,12 +56,16 @@ impl Vic {
         out
     }
 
+    fn char_to_digit(c: char) -> u32 {
+        u32::from(c) - 48
+    }
+
     pub fn key_derivation_string(
         &self,
         key_group: &str,
         date: &str,
         phrase: &str,
-        pin: usize,
+        pin: u32,
     ) -> Result<String, CipherError> {
         let mut derivation = String::new();
         // Line-A
@@ -95,7 +100,8 @@ impl Vic {
 
         // Line-F
         let f = {
-            let mut temp = Self::chain_addition(&c, 5);
+            let mut temp = c.clone();
+            temp.push_str(&Self::chain_addition(&c, 5));
             temp.push_str("1234567890");
             temp
         };
@@ -128,15 +134,51 @@ impl Vic {
         // Line-K through Line-P (there is no Line-O)
         let block = Self::chain_addition(&h, 50);
         derivation.push_str("\nK: ");
-        derivation.push_str(&block[10..20]);
+        derivation.push_str(&block[..20]);
         derivation.push_str("\nL: ");
-        derivation.push_str(&block[20..30]);
+        derivation.push_str(&block[10..20]);
         derivation.push_str("\nM: ");
-        derivation.push_str(&block[30..40]);
+        derivation.push_str(&block[20..30]);
         derivation.push_str("\nN: ");
-        derivation.push_str(&block[40..50]);
+        derivation.push_str(&block[30..40]);
         derivation.push_str("\nP: ");
-        derivation.push_str(&block[50..60]);
+        derivation.push_str(&block[40..50]);
+
+        // Derive key lengths
+        let key_lengths = {
+            let mut last_digits = block.chars().rev();
+            let mut a = last_digits.next().unwrap();
+            let mut b = last_digits.next().unwrap();
+            while a == b {
+                a = b;
+                b = last_digits.next().unwrap();
+            }
+            (
+                (Self::char_to_digit(b) + pin) as usize,
+                (Self::char_to_digit(a) + pin) as usize,
+            )
+        };
+
+        derivation.push_str(&format!(
+            "\n\nThe last two unequal digits are {} and {} so the key lengths will be {} and {}\n",
+            key_lengths.0 - pin as usize,
+            key_lengths.1 - pin as usize,
+            key_lengths.0,
+            key_lengths.1
+        ));
+
+        // Line-Q
+        let mut columnar = Columnar::default();
+        columnar.assign_key(&j, "1234567890").unwrap();
+        let encrypted_block = columnar.encrypt(&block)?;
+        derivation.push_str("\nQ: ");
+        derivation.push_str(&encrypted_block[..key_lengths.0]);
+
+        derivation.push_str("\nR: ");
+        derivation.push_str(&encrypted_block[key_lengths.0..key_lengths.0 + key_lengths.1]);
+
+        derivation.push_str("\nS: ");
+        derivation.push_str(&self.sequencing(&block[40..50], "1234567890")?);
 
         Ok(derivation)
     }
@@ -164,7 +206,7 @@ mod vic_tests {
     const KEY_GROUP: &'static str = "72401";
     const DATE: &'static str = "139195";
     const PHRASE: &'static str = "TWASTHENIGHTBEFORECHRISTMAS";
-    const PIN: usize = 6;
+    const PIN: u32 = 6;
 
     #[test]
     fn derivation_test() {
