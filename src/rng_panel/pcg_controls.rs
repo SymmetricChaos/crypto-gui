58bf11aca67c761666882f8d8c3468780be2c0e8
SymmetricChaos <ajfraebel@gmail.com>
2023-10-01 08:58:57 -0400

    Create pcg_controls.rs

---
+++ src/rng_panel/pcg_controls.rs
@@ -0,0 +1,124 @@
+use egui::TextStyle;
+use rand::{thread_rng, Rng};
+use rngs::{
+    pcg::{Pcg, PcgTransform},
+    ClassicRng,
+};
+
+use crate::ui_elements::{filter_and_parse_u32, UiElements};
+
+use super::ClassicRngFrame;
+
+pub struct PcgFrame {
+    rng: Pcg,
+    state_string: String,
+    multiplier_string: String,
+    increment_string: String,
+}
+
+impl Default for PcgFrame {
+    fn default() -> Self {
+        Self {
+            rng: Default::default(),
+            state_string: String::from("1257924810"),
+            multiplier_string: String::from("1664525"),
+            increment_string: String::from("1013904223"),
+        }
+    }
+}
+
+impl PcgFrame {
+    fn input_control(ui: &mut egui::Ui, string: &mut String, n: &mut u32) {
+        if ui
+            .add_sized(
+                [40.0, 20.0],
+                egui::TextEdit::singleline(string)
+                    .font(TextStyle::Monospace)
+                    .clip_text(false),
+            )
+            .changed()
+        {
+            filter_and_parse_u32(n, string);
+        }
+    }
+
+    fn set_all_strings(&mut self) {
+        self.state_string = self.rng.state.to_string();
+        self.multiplier_string = self.rng.multiplier.to_string();
+        self.increment_string = self.rng.increment.to_string();
+    }
+}
+
+impl ClassicRngFrame for PcgFrame {
+    fn ui(&mut self, ui: &mut egui::Ui, _errors: &mut String) {
+        ui.subheading("Calculation");
+        ui.horizontal(|ui| {
+            ui.subheading("(");
+            Self::input_control(ui, &mut self.state_string, &mut self.rng.state);
+            ui.subheading(" × ");
+            Self::input_control(ui, &mut self.multiplier_string, &mut self.rng.multiplier);
+            ui.subheading(" + ");
+            Self::input_control(ui, &mut self.increment_string, &mut self.rng.increment);
+            ui.subheading(" = ");
+            let mut m =
+                (self.rng.multiplier as u64 * self.rng.state as u64) % self.rng.modulus as u64;
+            m = (m + self.rng.increment as u64) % self.rng.modulus as u64;
+            ui.false_control_string(format!("{m}"));
+        });
+
+        ui.add_space(16.0);
+
+        ui.subheading("Permutation");
+        ui.horizontal(|ui| {
+            ui.selectable_value(
+                &mut self.rng.transform,
+                PcgTransform::Rs,
+                PcgTransform::Rs.name(),
+            );
+            ui.selectable_value(
+                &mut self.rng.transform,
+                PcgTransform::Rr,
+                PcgTransform::Rr.name(),
+            );
+        });
+        ui.horizontal(|ui| {
+            ui.selectable_value(
+                &mut self.rng.transform,
+                PcgTransform::XshRs,
+                PcgTransform::XshRs.name(),
+            );
+            ui.selectable_value(
+                &mut self.rng.transform,
+                PcgTransform::XshRr,
+                PcgTransform::XshRr.name(),
+            );
+        });
+
+        ui.add_space(16.0);
+
+        ui.subheading("Output");
+        ui.label(format!("{}", self.rng.transform(m)));
+
+        if ui.button("step").clicked() {
+            self.rng.step();
+            self.set_all_strings();
+        }
+    }
+
+    fn rng(&self) -> &dyn rngs::ClassicRng {
+        &self.rng
+    }
+
+    fn randomize(&mut self) {
+        let mut rng = thread_rng();
+        self.rng.state = rng.gen();
+        self.rng.multiplier = rng.gen();
+        self.rng.increment = rng.gen();
+        self.rng.modulus = rng.gen();
+        self.set_all_strings();
+    }
+
+    fn reset(&mut self) {
+        *self = Self::default()
+    }
+}
