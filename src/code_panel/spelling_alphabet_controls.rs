35058cd0a951b25404bc2582cbd4e15ed9562d38
SymmetricChaos <ajfraebel@gmail.com>
2023-10-16 07:58:01 -0400

    Update spelling_alphabet_controls.rs

--- src/code_panel/spelling_alphabet_controls.rs
+++ src/code_panel/spelling_alphabet_controls.rs
@@ -24,7 +24,7 @@ impl CodeFrame for SpellingAlphabetFrame {
                 ui.selectable_value(
                     &mut self.code.variant,
                     SpellingAlphabetMode::Nato,
-                    "NATO (ICAO)",
+                    "NATO/ICAO (1956)",
                 );
                 ui.selectable_value(&mut self.code.variant, SpellingAlphabetMode::Ccb, "CCB");
                 ui.selectable_value(
@@ -70,14 +70,14 @@ impl CodeFrame for SpellingAlphabetFrame {
         ui.add_space(16.0);
 
         match self.code.variant {
-            SpellingAlphabetMode::Nato => ui.label("The most widely used international standard today is the one specified by the ICAO, though it is commonly called the NATO Phonetic Alphabet. Note the intentional mispellings for ALFA (for non-English speakers generally) and JULIET (for French speakers)."),
+            SpellingAlphabetMode::Nato => ui.label("The most widely used international standard today is the one specified by the ICAO, though it is commonly called the NATO Phonetic Alphabet. Note the intentionally unusual spellings for ALFA (for non-English speakers generally) and JULIET (for French speakers)."),
             SpellingAlphabetMode::Ccb => ui.label("The Combined Communications Board was formed during World War II to improve the interoperability of communications systems for UK and US forces. It was the immediate predececessor to the NATO/ICAO standard."),
             SpellingAlphabetMode::Wu1912 => ui.label("In 1912 Western Union introduced a spelling alphabet for its employees that mostly used place names to represent letters."),
             SpellingAlphabetMode::Wu1942 => ui.label("The 1942 update to the Western Union standard."),
             SpellingAlphabetMode::Usn1908 => ui.label("The first of two recommendations by the US Navy in 1908, this version using two syllable words."),
             SpellingAlphabetMode::Usn1908Alt => ui.label("The first of two recommendations by the US Navy in 1908, this version using one syllable words."),
             SpellingAlphabetMode::Us1941 => ui.label("A US military standard created for joint use by the Army and Navy."),
-            SpellingAlphabetMode::FirstLetter => ui.label("Arbitrary words can be used for a phonetic alphabet. Most commonly the first letter of each word is the letter being represented. This decodes any sequence of words into their first letters."),
+            SpellingAlphabetMode::FirstLetter => ui.label("Arbitrary words can be used for a phonetic alphabet. Most commonly the first letter of each word is the letter being represented. This decodes any sequence of words into their first letters but doesn't provide encoding."),
             SpellingAlphabetMode::Uka1904 => ui.label("This very early standard from the UK Army Signalling Regulations in 1904 is unusual in several ways. The non-word 'ACK' is used for A, rhyming words EDDY and FREDDY are both present, EMMA and ESSES do not start with the letter they represent."),
         };
         ui.add_space(16.0);
