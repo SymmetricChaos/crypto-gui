c9ea464f4506a00f3e51fb1d374039dab853b29c
SymmetricChaos <ajfraebel@gmail.com>
2023-10-22 09:18:18 -0400

    image for needle

--- src/code_panel/needle_controls.rs
+++ src/code_panel/needle_controls.rs
@@ -1,6 +1,8 @@
 use super::CodeFrame;
 use crate::ui_elements::UiElements;
 use codes::text_standards::needle::Needle;
+use rand::thread_rng;
+use utils::text_functions::shuffled_str;
 
 pub struct NeedleFrame {
     code: Needle,
@@ -16,13 +18,21 @@ impl Default for NeedleFrame {
 
 impl CodeFrame for NeedleFrame {
     fn ui(&mut self, ui: &mut egui::Ui) {
-        ui.subheading("Alphabet");
-        if ui.control_string(&mut self.code.alphabet).changed() {
-            self.code.set_map()
-        }
+        ui.horizontal(|ui| {
+            ui.subheading("Alphabet");
+            if ui.button("🎲").on_hover_text("shuffle").clicked() {
+                self.code.alphabet = shuffled_str(&self.code.alphabet, &mut thread_rng());
+                self.code.set_map()
+            }
+        });
+
         ui.add_space(16.0);
         ui.fill_code_columns(5, 4, self.code.chars_codes());
         ui.add_space(16.0);
+
+        ui.image(egui::include_image!("needle_telegraph.png"))
+            .rect
+            .set_height(765.0);
     }
 
     fn code(&self) -> &dyn codes::traits::Code {
