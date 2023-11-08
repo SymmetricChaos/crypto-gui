24d50d5c3f10bd022b282a97c972d6aa4616cf4f
SymmetricChaos <ajfraebel@gmail.com>
2023-11-07 19:57:39 -0500

    grammar

--- codes/src/text_standards/unified_english_braille_parser.rs
+++ codes/src/text_standards/unified_english_braille_parser.rs
@@ -1,4 +1,3 @@
-use pest::Parser;
 use pest_derive::Parser;
 
 #[derive(Parser)]
@@ -8,16 +7,42 @@ struct UebParser;
 #[cfg(test)]
 mod ueb_parser_tests {
     use super::*;
-
-    const TEXT: &'static str = "⠠⠞⠓⠑ ⠠⠟⠥⠊⠉⠅";
-
-    #[test]
-    fn parse() {
-        for x in UebParser::parse(Rule::passage, TEXT).into_iter() {
-            let y = x.flatten();
-            for a in y.into_iter() {
-                println!("{:?} {}", a.as_rule(), a.as_str())
+    use pest::iterators::Pairs;
+    use pest::Parser;
+    pub fn descend(pairs: Pairs<'_, Rule>) {
+        for pair in pairs.into_iter() {
+            match pair.as_rule() {
+                Rule::WHITESPACE => println!("WHITESPACE({})", pair.as_str()),
+                Rule::basic_letter => println!("basic_letter({})", pair.as_str()),
+                Rule::capitalize => println!("capitalize({})", pair.as_str()),
+                Rule::other_modifier => println!("other_modifier({})", pair.as_str()),
+                Rule::modifier => {
+                    println!("modifier({})", pair.as_str());
+                    descend(pair.into_inner())
+                }
+                Rule::letter => {
+                    println!("letter({})", pair.as_str());
+                    descend(pair.into_inner())
+                }
+                Rule::letter_sequence => {
+                    println!("letter_sequence({})", pair.as_str());
+                    descend(pair.into_inner())
+                }
+                Rule::passage => {
+                    println!("passage({})", pair.as_str());
+                    descend(pair.into_inner())
+                }
             }
         }
+    }
+
+    //Étienne háček Im Frühling
+    const TEXT: &'static str = "⠠⠘⠌⠑⠞⠊⠝⠝⠑ ⠓⠘⠌⠁⠘⠬⠉⠑⠅ ⠠⠊⠍ ⠠⠋⠗⠘⠒⠥⠓⠇⠬";
+
+    #[test]
+    #[ignore = "parsing experiment"]
+    fn example_parse() {
+        let pairs = UebParser::parse(Rule::passage, TEXT).unwrap();
+        descend(pairs)
     }
 }
