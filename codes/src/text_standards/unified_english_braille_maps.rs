9390b64caf827da3aba230973fb860003d2bccae
SymmetricChaos <ajfraebel@gmail.com>
2023-11-11 14:00:37 -0500

    numeric characters

--- codes/src/text_standards/unified_english_braille_maps.rs
+++ codes/src/text_standards/unified_english_braille_maps.rs
@@ -15,181 +15,7 @@ pub const LINE7: &'static str = "⠈⠘⠸⠐⠨⠰⠠";
 pub const BRAILLE_ORDER: [&'static str; 7] = [LINE1, LINE2, LINE3, LINE4, LINE5, LINE6, LINE7];
 
 // These eight characters are the UEB prefixes. All others characters are called roots as is the space.
-const PREFIXES: &'static str = "⠼⠈⠘⠸⠐⠨⠰⠠";
-
-pub enum LetterModifier {
-    Soldius,
-    HorizontalStroke,
-    Breve,
-    Macron,
-    Cedilla,
-    GraveAccent,
-    Circumflex,
-    Ring,
-    Tilde,
-    Diaeresis,
-    AcuteAccent,
-    Caron,
-}
-
-impl LetterModifier {
-    pub fn to_symbol(&self) -> char {
-        match self {
-            Self::Soldius => '̸',
-            Self::HorizontalStroke => '̶',
-            Self::Breve => '̆',
-            Self::Macron => '̄',
-            Self::Cedilla => '̧',
-            Self::GraveAccent => '̀',
-            Self::Circumflex => '̂',
-            Self::Ring => '̊',
-            Self::Tilde => '̃',
-            Self::Diaeresis => '̈',
-            Self::AcuteAccent => '́',
-            Self::Caron => '̌',
-        }
-    }
-}
-
-pub enum UebMode {
-    Shape,
-    Arrow,
-    Numeric,
-    HorizontalLine,
-    GradeOne,
-}
-
-impl UebMode {
-    pub const BRAILLE: [&'static str; 5] = ["⠫", "⠳", "⠼", "⠐⠒", "⠰"];
-    pub const LATIN: [&'static str; 5] = [
-        "[shape]",
-        "[arrow]",
-        "[numeric]",
-        "[horizontal-line]",
-        "[grade-1]",
-    ];
-
-    pub fn as_braille(&self) -> &'static str {
-        match self {
-            Self::Shape => Self::BRAILLE[0],
-            Self::Arrow => Self::BRAILLE[1],
-            Self::Numeric => Self::BRAILLE[2],
-            Self::HorizontalLine => Self::BRAILLE[3],
-            Self::GradeOne => Self::BRAILLE[4],
-        }
-    }
-
-    pub fn from_braille(s: &str) -> Option<Self> {
-        match s {
-            "⠫" => Some(Self::Shape),
-            "⠳" => Some(Self::Arrow),
-            "⠼" => Some(Self::Numeric),
-            "⠐⠒" => Some(Self::HorizontalLine),
-            "⠰" => Some(Self::GradeOne),
-            _ => None,
-        }
-    }
-
-    pub fn as_latin(&self) -> &'static str {
-        match self {
-            Self::Shape => Self::LATIN[0],
-            Self::Arrow => Self::LATIN[1],
-            Self::Numeric => Self::LATIN[2],
-            Self::HorizontalLine => Self::LATIN[3],
-            Self::GradeOne => Self::LATIN[4],
-        }
-    }
-
-    pub fn from_latin(s: &str) -> Option<Self> {
-        match s {
-            "[shape]" => Some(Self::Shape),
-            "[arrow]" => Some(Self::Arrow),
-            "[numeric]" => Some(Self::Numeric),
-            "[horizontal-line]" => Some(Self::HorizontalLine),
-            "[grade-1]" => Some(Self::GradeOne),
-            _ => None,
-        }
-    }
-}
-
-pub enum UebIndicator {
-    Subscript,
-    Superscript,
-    Script,
-    Bold,
-    Ligature,
-    Underline,
-    Italic,
-    Capital,
-}
-
-impl UebIndicator {
-    pub const BRAILLE: [&'static str; 8] = ["⠢", "⠔", "⠈⠆", "⠘⠆", "⠘⠖", "⠸⠆", "⠨⠆", "⠠⠠"];
-    pub const LATIN: [&'static str; 8] = [
-        "[subscript]",
-        "[superscript]",
-        "[script]",
-        "[bold]",
-        "[ligature]",
-        "[underline]",
-        "[italic]",
-        "[capital]",
-    ];
-
-    pub fn as_braille(&self) -> &'static str {
-        match self {
-            Self::Subscript => Self::BRAILLE[0],
-            Self::Superscript => Self::BRAILLE[1],
-            Self::Script => Self::BRAILLE[2],
-            Self::Bold => Self::BRAILLE[3],
-            Self::Ligature => Self::BRAILLE[4],
-            Self::Underline => Self::BRAILLE[5],
-            Self::Italic => Self::BRAILLE[6],
-            Self::Capital => Self::BRAILLE[7],
-        }
-    }
-
-    pub fn from_braille(s: &str) -> Option<Self> {
-        match s {
-            "⠢" => Some(Self::Subscript),
-            "⠔" => Some(Self::Superscript),
-            "⠈⠆" => Some(Self::Script),
-            "⠘⠆" => Some(Self::Bold),
-            "⠘⠖" => Some(Self::Ligature),
-            "⠸⠆" => Some(Self::Underline),
-            "⠨⠆" => Some(Self::Italic),
-            "⠠⠠" => Some(Self::Capital),
-            _ => None,
-        }
-    }
-
-    pub fn as_latin(&self) -> &'static str {
-        match self {
-            Self::Subscript => Self::LATIN[0],
-            Self::Superscript => Self::LATIN[1],
-            Self::Script => Self::LATIN[2],
-            Self::Bold => Self::LATIN[3],
-            Self::Ligature => Self::LATIN[4],
-            Self::Underline => Self::LATIN[5],
-            Self::Italic => Self::LATIN[6],
-            Self::Capital => Self::LATIN[7],
-        }
-    }
-
-    pub fn from_latin(s: &str) -> Option<Self> {
-        match s {
-            "[subscript]" => Some(Self::Subscript),
-            "[superscript]" => Some(Self::Superscript),
-            "[script]" => Some(Self::Script),
-            "[bold]" => Some(Self::Bold),
-            "[ligature]" => Some(Self::Ligature),
-            "[underline]" => Some(Self::Underline),
-            "[italic]" => Some(Self::Italic),
-            "[capital]" => Some(Self::Capital),
-            _ => None,
-        }
-    }
-}
+pub const PREFIXES: &'static str = "⠼⠈⠘⠸⠐⠨⠰⠠";
 
 const LETTERS: &'static str = "abcdefghijklmnopqrstuvwxyz";
 const LETTERS_BRAILLE: &'static str = "⠁⠃⠉⠙⠑⠋⠛⠓⠊⠚⠅⠇⠍⠝⠕⠏⠟⠗⠎⠞⠥⠧⠺⠭⠽⠵";
@@ -258,8 +84,8 @@ const DIACRITIC_BRAILLE: &[&'static str] = &[
 ];
 
 // Ellipsis is a specific punctuation symbol listed for UEB but it written as a sequence of full stops
-// The dashes are Unicode specified: en-dash, em-dash, and double em-dash.
-const PUNCTUATION: &'static str = ",;:.!?“”‘’«»\"'()[]<>{}/\\–—⸺_";
+// The dashes are Unicode specified: hyphen, em-dash, and double em-dash.
+const PUNCTUATION: &'static str = ",;:.!?“”‘’«»\"'()[]<>{}/\\-—⸺_";
 const PUNCTUATION_BRAILLE: &[&'static str] = &[
     "⠂",
     "⠆",
@@ -291,16 +117,13 @@ const PUNCTUATION_BRAILLE: &[&'static str] = &[
     "⠨⠤",
 ];
 
+const NUMERIC: [&'static str; 12] = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ",", "."];
+const NUMERIC_BRAILLE: [&'static str; 12] =
+    ["⠁", "⠃", "⠉", "⠙", "⠑", "⠋", "⠛", "⠓", "⠊", "⠚", "⠂", "⠲"];
+
 lazy_static! {
     pub static ref LETTER_MAP: BiMap<char, char> =
         bimap_from_iter(LETTERS.chars().zip(LETTERS_BRAILLE.chars()));
-    pub static ref MODE_MAP: BiMap<&'static str, &'static str> =
-        bimap_from_iter(UebMode::LATIN.into_iter().zip(UebMode::BRAILLE.into_iter()));
-    pub static ref INDICATOR_MAP: BiMap<&'static str, &'static str> = bimap_from_iter(
-        UebIndicator::LATIN
-            .into_iter()
-            .zip(UebIndicator::BRAILLE.into_iter())
-    );
     pub static ref SYMBOL_MAP: BiMap<char, &'static str> =
         bimap_from_iter(SYMBOLS.chars().zip(SYMBOLS_BRAILLE.into_iter().copied()));
     pub static ref PUNCTUATION_MAP: BiMap<char, &'static str> = bimap_from_iter(
@@ -316,6 +139,8 @@ lazy_static! {
     );
     pub static ref GREEK_MAP: BiMap<&'static str, &'static str> =
         bimap_from_iter(GREEK.into_iter().zip(GREEK_BRAILLE.into_iter()));
+    pub static ref NUMERIC_MAP: BiMap<&'static str, &'static str> =
+        bimap_from_iter(NUMERIC.into_iter().zip(NUMERIC_BRAILLE.into_iter()));
 }
 
 #[cfg(test)]
@@ -354,6 +179,15 @@ mod ueb_pairing_tests {
     fn letter_modifiers() {
         println!("Letter Modifiers");
         for (a, b) in DIACRITIC.into_iter().zip(DIACRITIC_BRAILLE.into_iter()) {
+            println!("{} {}", a, b)
+        }
+    }
+
+    #[test]
+    #[ignore = "numeric pairing test"]
+    fn numeric() {
+        println!("Numeric");
+        for (a, b) in NUMERIC.into_iter().zip(NUMERIC_BRAILLE.into_iter()) {
             println!("{} {}", a, b)
         }
     }
--- codes/src/text_standards/unified_english_braille_parser.rs
+++ codes/src/text_standards/unified_english_braille_parser.rs
@@ -4,138 +4,143 @@ use pest_derive::Parser;
 #[grammar = "text_standards/ueb.pest"] // relative to src
 struct UebParser;
 
+use crate::text_standards::unified_english_braille_maps::{
+    DIACRITIC_MAP, GREEK_MAP, LETTER_MAP, PUNCTUATION_MAP,
+};
+
+use pest::iterators::{Pair, Pairs};
+use unicode_normalization::UnicodeNormalization;
+
+use super::unified_english_braille_maps::NUMERIC_MAP;
+pub fn descend(pairs: Pairs<'_, Rule>, space: String) {
+    for pair in pairs.into_iter() {
+        let mut space = space.clone();
+        space.push(' ');
+        match pair.as_rule() {
+            Rule::WHITESPACE => println!("{space}WHITESPACE({})", pair.as_str()),
+            Rule::basic_letter => println!("{space}basic_letter({})", pair.as_str()),
+            Rule::capitalize => println!("{space}capitalize({})", pair.as_str()),
+            Rule::number => println!("{space}letter({})", pair.as_str()),
+            Rule::letter => println!("{space}letter({})", pair.as_str()),
+            Rule::character => println!("{space}character({})", pair.as_str()),
+            Rule::number => println!("{space}number({})", pair.as_str()),
+            Rule::numeric_symbols => println!("{space}numeric_symbols({})", pair.as_str()),
+            Rule::punctuation => println!("{space}punctuation({})", pair.as_str()),
+            Rule::passage => println!("{space}passage({})", pair.as_str()),
+            Rule::greek => println!("{space}greek({})", pair.as_str()),
+            Rule::diacritic => println!("{space}diacritic({})", pair.as_str()),
+        }
+
+        descend(pair.into_inner(), space)
+    }
+}
+
+pub fn decode_passage(pairs: Pairs<'_, Rule>) -> String {
+    let mut out = String::new();
+    for pair in pairs.into_iter() {
+        match pair.as_rule() {
+            Rule::passage => out.push_str(&decode_passage(pair.into_inner())),
+            Rule::WHITESPACE => out.push_str(" "),
+            Rule::character => {
+                decode_character(pair.into_inner(), &mut out);
+            }
+            _ => unreachable!(
+                "a passage consists only of WHITESPACE and letter_sequence at the top level"
+            ),
+        }
+    }
+    out.nfc().collect()
+}
+
+pub fn decode_character(pairs: Pairs<'_, Rule>, string: &mut String) {
+    for pair in pairs.into_iter() {
+        match pair.as_rule() {
+            Rule::letter => decode_letter(pair.into_inner(), string),
+            Rule::punctuation => string.push(*PUNCTUATION_MAP.get_by_right(pair.as_str()).unwrap()),
+            Rule::number => decode_number(pair.into_inner(), string),
+            _ => unreachable!("characters are only: letter, number and punctuation"),
+        }
+    }
+}
+
+pub fn decode_number(pairs: Pairs<'_, Rule>, string: &mut String) {
+    for pair in pairs.into_iter() {
+        string.push_str(NUMERIC_MAP.get_by_right(pair.as_str()).unwrap())
+    }
+}
+
+pub fn decode_letter(pairs: Pairs<'_, Rule>, string: &mut String) {
+    let mut capital = false;
+    let mut greek = false;
+    let mut diacritics = String::new();
+    for pair in pairs.into_iter() {
+        match pair.as_rule() {
+            Rule::basic_letter => {
+                let letter = if capital {
+                    if greek {
+
+                            GREEK_MAP
+                                .get_by_right(pair.as_str())
+                                .unwrap()
+                                .to_uppercase()
+                    } else {
+
+                            LETTER_MAP
+                                .get_by_right(&pair.as_str().chars().next().unwrap())
+                                .unwrap()
+                                .to_uppercase().to_string()
+                    }
+                } else {
+                    if greek {
+                        GREEK_MAP.get_by_right(pair.as_str()).unwrap().to_string()
+                    } else {
+                            LETTER_MAP
+                                .get_by_right(&pair.as_str().chars().next().unwrap())
+                                .unwrap().to_string()
+                    }
+                };
+                string.push_str(&letter);
+                string.push_str(&diacritics);
+            }
+            Rule::capitalize => {
+                capital = true;
+            }
+            Rule::greek => {
+                greek = true;
+            }
+            Rule::diacritic => {
+                diacritics.push_str(DIACRITIC_MAP.get_by_right(pair.as_str()).unwrap())
+            }
+            _ => unreachable!(
+                "letters should only contain the rules: basic_letter, capitalize, greek, and diacritic"
+            ),
+        }
+    }
+}
+
 #[cfg(test)]
 mod ueb_parser_tests {
-    use crate::text_standards::unified_english_braille_maps::{
-        DIACRITIC_MAP, GREEK_MAP, LETTER_MAP, PUNCTUATION_MAP,
-    };
 
     use super::*;
-    use pest::iterators::{Pair, Pairs};
+    const TEXT: &'static str = "Étienne! 123 háček 9 Im-Frühling Ω σ 7:30 a.m.";
+    const BRAILLE: &'static str =
+        "⠠⠘⠌⠑⠞⠊⠑⠝⠝⠑⠖ ⠼⠁⠃⠉⠀⠓⠘⠌⠁⠘⠬⠉⠑⠅ ⠼⠊⠀⠠⠊⠍⠤⠠⠋⠗⠘⠒⠥⠓⠇⠊⠝⠛ ⠠⠨⠺ ⠨⠎ ⠼⠛⠒⠼⠉⠚ ⠁⠲⠍⠲";
+
     use pest::Parser;
-    use unicode_normalization::UnicodeNormalization;
-    pub fn descend(pairs: Pairs<'_, Rule>, space: String) {
-        for pair in pairs.into_iter() {
-            let mut space = space.clone();
-            space.push(' ');
-            match pair.as_rule() {
-                Rule::WHITESPACE => println!("{space}WHITESPACE({})", pair.as_str()),
-                Rule::basic_letter => println!("{space}basic_letter({})", pair.as_str()),
-                Rule::capitalize => println!("{space}capitalize({})", pair.as_str()),
-                Rule::letter => {
-                    println!("{space}letter({})", pair.as_str());
-                    descend(pair.into_inner(), space)
-                }
-                Rule::character => {
-                    println!("{space}character({})", pair.as_str());
-                    descend(pair.into_inner(), space)
-                }
-                Rule::punctuation => {
-                    println!("{space}punctuation({})", pair.as_str());
-                    descend(pair.into_inner(), space)
-                }
-                Rule::passage => {
-                    println!("{space}passage({})", pair.as_str());
-                    descend(pair.into_inner(), space)
-                }
-                Rule::greek => println!("{space}greek({})", pair.as_str()),
-                Rule::diacritic => println!("{space}diacritic({})", pair.as_str()),
-            }
-        }
-    }
-
-    pub fn decode_passage(pairs: Pairs<'_, Rule>) -> String {
-        let mut out = String::new();
-        for pair in pairs.into_iter() {
-            match pair.as_rule() {
-                Rule::passage => out.push_str(&decode_passage(pair.into_inner())),
-                Rule::WHITESPACE => out.push_str(pair.as_str()),
-                Rule::character => {
-                    decode_character(pair.into_inner(), &mut out);
-                }
-                _ => unreachable!(
-                    "a passage consists only of WHITESPACE and letter_sequence at the top level"
-                ),
-            }
-        }
-        out.nfc().collect()
-    }
-
-    // pub fn decode_letter_sequence(pairs: Pairs<'_, Rule>, string: &mut String) {
-    //     for letter in pairs.into_iter() {
-    //         string.push_str(&decode_letter(letter.into_inner()))
-    //     }
-    // }
-
-    pub fn decode_character(pairs: Pairs<'_, Rule>, string: &mut String) {
-        for pair in pairs.into_iter() {
-            match pair.as_rule() {
-                Rule::letter => decode_letter(pair.into_inner(), string),
-                Rule::punctuation => {
-                    string.push(*PUNCTUATION_MAP.get_by_right(pair.as_str()).unwrap())
-                }
-                _ => unreachable!("characters are only: letter and punctuation"),
-            }
-        }
-    }
-
-    pub fn decode_letter(pairs: Pairs<'_, Rule>, string: &mut String) {
-        let mut capital = false;
-        let mut greek = false;
-        let mut diacritics = String::new();
-        for pair in pairs.into_iter() {
-            match pair.as_rule() {
-                Rule::basic_letter => {
-                    let letter = if capital {
-                        if greek {
-
-                                GREEK_MAP
-                                    .get_by_right(pair.as_str())
-                                    .unwrap()
-                                    .to_uppercase()
-                        } else {
-
-                                LETTER_MAP
-                                    .get_by_right(&pair.as_str().chars().next().unwrap())
-                                    .unwrap()
-                                    .to_uppercase().to_string()
-                        }
-                    } else {
-                        if greek {
-                            GREEK_MAP.get_by_right(pair.as_str()).unwrap().to_string()
-                        } else {
-                                LETTER_MAP
-                                    .get_by_right(&pair.as_str().chars().next().unwrap())
-                                    .unwrap().to_string()
-                        }
-                    };
-                    string.push_str(&letter);
-                    string.push_str(&diacritics);
-                }
-                Rule::capitalize => {
-                    capital = true;
-                }
-                Rule::greek => {
-                    greek = true;
-                }
-                Rule::diacritic => {
-                    diacritics.push_str(DIACRITIC_MAP.get_by_right(pair.as_str()).unwrap())
-                }
-                _ => unreachable!(
-                    "letters should only contain the rules: basic_letter, capitalize, greek, and diacritic"
-                ),
-            }
-        }
-    }
-
-    //Étienne! háček Im-Frühling Ω σ
-    const TEXT: &'static str = "⠠⠘⠌⠑⠞⠊⠑⠝⠝⠑⠖⠀⠓⠘⠌⠁⠘⠬⠉⠑⠅⠀⠠⠊⠍⠤⠠⠋⠗⠘⠒⠥⠓⠇⠊⠝⠛ ⠠⠨⠺ ⠨⠎";
 
     #[test]
     #[ignore = "parsing experiment"]
-    fn example_parse() {
-        let pairs = UebParser::parse(Rule::passage, TEXT).unwrap();
-        descend(pairs.clone(), String::new());
-        println!("{}", decode_passage(pairs));
+    fn parse_tree() {
+        let pairs = UebParser::parse(Rule::passage, BRAILLE).unwrap();
+        descend(pairs, String::new());
+    }
+
+    #[test]
+    #[ignore = "decoding experiment"]
+    fn decode() {
+        let pairs = UebParser::parse(Rule::passage, BRAILLE).unwrap();
+        let decoded = decode_passage(pairs);
+        println!("{}", decoded);
+        assert_eq!(decoded, TEXT)
     }
 }
