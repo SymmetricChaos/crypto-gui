e0f72c024b75636003f44bdaeda2532e0587b819
SymmetricChaos <ajfraebel@gmail.com>
2023-12-03 11:50:48 -0500

    Update morse_controls.rs

--- src/code_panel/morse_controls.rs
+++ src/code_panel/morse_controls.rs
@@ -19,6 +19,11 @@ impl CodeFrame for MorseFrame {
         ui.group(|ui| {
             ui.subheading("Variant");
             ui.horizontal(|ui| {
+                ui.selectable_value(
+                    &mut self.code.standard,
+                    MorseStandard::Itu,
+                    "ITU Morse (1865)",
+                );
                 if ui
                     .selectable_value(
                         &mut self.code.standard,
@@ -34,11 +39,7 @@ impl CodeFrame for MorseFrame {
                     }
                 }
                 if ui
-                    .selectable_value(
-                        &mut self.code.standard,
-                        MorseStandard::Gerke,
-                        "Gerke Code (1851)",
-                    )
+                    .selectable_value(&mut self.code.standard, MorseStandard::Gerke, "Gerke Code")
                     .clicked()
                 {
                     if self.code.representation == MorseRep::Ascii
@@ -47,10 +48,11 @@ impl CodeFrame for MorseFrame {
                         self.code.representation = MorseRep::HalfBlock
                     }
                 }
+                ui.selectable_value(&mut self.code.standard, MorseStandard::Greek, "Greek Morse");
                 ui.selectable_value(
                     &mut self.code.standard,
-                    MorseStandard::Itu,
-                    "ITU Morse (1865)",
+                    MorseStandard::Wabun,
+                    "Waban Code (Japanese Morse)",
                 );
             });
         });
@@ -61,11 +63,6 @@ impl CodeFrame for MorseFrame {
                 &mut self.code.representation,
                 MorseRep::HalfBlock,
                 "Halfblock (Line Code)",
-            );
-            ui.selectable_value(
-                &mut self.code.representation,
-                MorseRep::Binary,
-                "Binary (Line Code)",
             );
             ui.add_enabled_ui(self.code.standard == MorseStandard::Itu, |ui| {
                 ui.selectable_value(
@@ -86,6 +83,8 @@ impl CodeFrame for MorseFrame {
             MorseStandard::Itu => ui.label("The best known variant of Morse code is the standard adopted by the International Telegraphy Union. There are two kinds of marks, dits and dahs. A dit lasts for one unit of time and a dah for three times that. The space between marks is the length of a dit. Between letters a space the length of a dah is used. Sentences words a space the length of seven dits is used."),
             MorseStandard::American => ui.label("The original 1844 code created by Morse and Vail was much more complex than the better known international standard. Rather than two kinds of marks there were four. The dit was of one unit, the dash of two units, the long dash of five (used for the letter 'L'), and the longer dash of six (used for the number '0'). Gaps between marks also varied in size. A gap within a character could be either one or two units in length. Only line codes have standard ways of representing American Morse."),
             MorseStandard::Gerke => ui.label("Gerke considerably simplified Morse and Vail's code by standardizing the size of marks and gaps. There are two kinds of marks, dits and dahs. A dit lasts for one unit of time and a dah for three times that. The space between marks is the length of a dit. Between letters a space the length of a dah is used. Sentences words a space the length of seven dits is used. An exception, however, is made for '0' which was encoded as a single mark of length six as in American Morse. Gerke made various other changes: addition of letters commonly needed in German, removal of the letter 'I', and fixed width digits (excepting zero). Due to the long '0' only line codes have standard ways of representing Gerke's code."),
+            MorseStandard::Greek => ui.label("text"),
+            MorseStandard::Wabun => ui.label("text"),
         };
 
         ui.add_space(16.0);
