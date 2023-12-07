156764b0ff2c320c1c899435ae5ea79b85a4ff64
SymmetricChaos <ajfraebel@gmail.com>
2023-12-06 19:47:05 -0500

    Update morse_controls.rs

--- src/code_panel/morse_controls.rs
+++ src/code_panel/morse_controls.rs
@@ -49,11 +49,6 @@ impl CodeFrame for MorseFrame {
                     }
                 }
                 ui.selectable_value(&mut self.code.standard, MorseStandard::Greek, "Greek Morse");
-                ui.selectable_value(
-                    &mut self.code.standard,
-                    MorseStandard::Wabun,
-                    "Wabun Code (Japanese Morse)",
-                );
             });
         });
 
@@ -67,12 +62,7 @@ impl CodeFrame for MorseFrame {
             );
             // ASCII and words only work for codes with standardized sizes
             ui.add_enabled_ui(
-                [
-                    MorseStandard::Itu,
-                    MorseStandard::Greek,
-                    MorseStandard::Wabun,
-                ]
-                .contains(&self.code.standard),
+                [MorseStandard::Itu, MorseStandard::Greek].contains(&self.code.standard),
                 |ui| {
                     ui.selectable_value(
                         &mut self.code.representation,
@@ -94,7 +84,6 @@ impl CodeFrame for MorseFrame {
             MorseStandard::American => ui.label("The original 1844 code created by Morse and Vail was much more complex than the better known international standard. Rather than two kinds of marks there were four. The dit was of one unit, the dash of two units, the long dash of five (used for the letter 'L'), and the longer dash of six (used for the number '0'). Gaps between marks also varied in size. A gap within a character could be either one or two units in length. Only line codes have standard ways of representing American Morse."),
             MorseStandard::Gerke => ui.label("Gerke considerably simplified Morse and Vail's code by standardizing the size of marks and gaps. There are two kinds of marks, dits and dahs. A dit lasts for one unit of time and a dah for three times that. The space between marks is the length of a dit. Between letters a space the length of a dah is used. Sentences words a space the length of seven dits is used. An exception, however, is made for '0' which was encoded as a single mark of length six as in American Morse. Gerke made various other changes: addition of letters commonly needed in German, removal of the letter 'I', and fixed width digits (excepting zero). Due to the long '0' only line codes have standard ways of representing Gerke's code."),
             MorseStandard::Greek => ui.label("text"),
-            MorseStandard::Wabun => ui.label("text"),
         };
 
         ui.add_space(16.0);
