c8f8877cedb39d3e9faa8e440a6715d33c9176fc
SymmetricChaos <ajfraebel@gmail.com>
2023-12-28 12:22:48 -0500

    jump functions

--- rngs/src/xorshift.rs
+++ rngs/src/xorshift.rs
@@ -6,7 +6,7 @@ pub struct Xorshift {
 
 impl Default for Xorshift {
     fn default() -> Self {
-        Self { state: 1257924810 }
+        Self { state: 0x0BAD_5EED }
     }
 }
 
@@ -16,5 +16,31 @@ impl ClassicRng for Xorshift {
         self.state ^= self.state >> 17;
         self.state ^= self.state << 5;
         self.state
+    }
+}
+
+pub struct Xorshift128 {
+    pub state: [u32; 4],
+}
+
+impl Default for Xorshift128 {
+    fn default() -> Self {
+        Self {
+            state: [0x0BAD_5EED, 0x0BAD_5EED, 0x0BAD_5EED, 0x0BAD_5EED],
+        }
+    }
+}
+
+impl ClassicRng for Xorshift128 {
+    fn next_u32(&mut self) -> u32 {
+        let x = self.state[0];
+        let t = x ^ (x << 11);
+        self.state[0] = self.state[1];
+        self.state[1] = self.state[2];
+        self.state[2] = self.state[3];
+        let tw = self.state[3];
+        self.state[3] = tw ^ (tw >> 19) ^ (t ^ (t >> 8));
+
+        self.state[3]
     }
 }
--- rngs/src/xoshiro.rs
+++ rngs/src/xoshiro.rs
@@ -6,12 +6,12 @@ pub enum Transform {
     StarStar,
 }
 
-pub struct Xoshiro {
+pub struct Xoshiro256 {
     pub state: [u64; 4],
     pub transform: Transform,
 }
 
-impl Default for Xoshiro {
+impl Default for Xoshiro256 {
     fn default() -> Self {
         Self {
             state: [0, 0, 0, 0],
@@ -20,7 +20,20 @@ impl Default for Xoshiro {
     }
 }
 
-impl Xoshiro {
+impl Xoshiro256 {
+    const JUMP: [u64; 4] = [
+        0x180ec6d33cfd0aba,
+        0xd5a61266f0c9392c,
+        0xa9582618e03fc9aa,
+        0x39abdc4529b1661c,
+    ];
+    const LONG_JUMP: [u64; 4] = [
+        0x76e15d3efefdcbbf,
+        0xc5004e441c522fb3,
+        0x77710069854ee241,
+        0x39109bb02acbe635,
+    ];
+
     pub fn step(&mut self) {
         let t = self.state[1] << 17;
         self.state[2] ^= self.state[0];
@@ -31,7 +44,7 @@ impl Xoshiro {
         self.state[3] = self.state[3].rotate_left(45);
     }
 
-    pub fn transform(&mut self) -> u64 {
+    pub fn output(&mut self) -> u64 {
         match self.transform {
             Transform::PlusPlus => (self.state[0].wrapping_add(self.state[3]))
                 .rotate_left(23)
@@ -41,11 +54,57 @@ impl Xoshiro {
                 .wrapping_mul(9),
         }
     }
+
+    // Jumps forward by 2^128 steps
+    pub fn jump(&mut self) {
+        let mut s0 = 0;
+        let mut s1 = 0;
+        let mut s2 = 0;
+        let mut s3 = 0;
+        for i in 0..4 {
+            for b in 0..64 {
+                if Self::JUMP[i] & (1 << b) != 0 {
+                    s0 ^= self.state[0];
+                    s1 ^= self.state[1];
+                    s2 ^= self.state[2];
+                    s3 ^= self.state[3];
+                }
+                self.step()
+            }
+        }
+        self.state[0] = s0;
+        self.state[1] = s1;
+        self.state[2] = s2;
+        self.state[3] = s3;
+    }
+
+    // Jumps forward by 2^192 steps
+    pub fn long_jump(&mut self) {
+        let mut s0 = 0;
+        let mut s1 = 0;
+        let mut s2 = 0;
+        let mut s3 = 0;
+        for i in 0..4 {
+            for b in 0..64 {
+                if Self::LONG_JUMP[i] & (1 << b) != 0 {
+                    s0 ^= self.state[0];
+                    s1 ^= self.state[1];
+                    s2 ^= self.state[2];
+                    s3 ^= self.state[3];
+                }
+                self.step()
+            }
+        }
+        self.state[0] = s0;
+        self.state[1] = s1;
+        self.state[2] = s2;
+        self.state[3] = s3;
+    }
 }
 
-impl ClassicRng for Xoshiro {
+impl ClassicRng for Xoshiro256 {
     fn next_u32(&mut self) -> u32 {
-        let out = self.transform() as u32;
+        let out = (self.output() >> 32) as u32;
         self.step();
         out
     }
