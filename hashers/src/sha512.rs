b0637d7b9443c736434ce2c2d1657b20df386d13
SymmetricChaos <ajfraebel@gmail.com>
2024-01-15 17:43:49 -0500

    Update sha512.rs

--- hashers/src/sha512.rs
+++ hashers/src/sha512.rs
@@ -58,7 +58,51 @@ impl Default for Sha512 {
     }
 }
 
-impl Sha512 {}
+impl Sha512 {
+    const SHA384: [u64; 8] = [
+        0xcbbb9d5dc1059ed8,
+        0x629a292a367cd507,
+        0x9159015a3070dd17,
+        0x152fecd8f70e5939,
+        0x67332667ffc00b31,
+        0x8eb44a8768581511,
+        0xdb0c2e0d64f98fa7,
+        0x47b5481dbefa4fa4,
+    ];
+
+    const SHA512: [u64; 8] = [
+        0x6a09e667f3bcc908,
+        0xbb67ae8584caa73b,
+        0x3c6ef372fe94f82b,
+        0xa54ff53a5f1d36f1,
+        0x510e527fade682d1,
+        0x9b05688c2b3e6c1f,
+        0x1f83d9abfb41bd6b,
+        0x5be0cd19137e2179,
+    ];
+
+    const SHA512_256: [u64; 8] = [
+        0x22312194fc2bf72c,
+        0x9f555fa3c84c64c2,
+        0x2393b86b6f53b151,
+        0x963877195940eabd,
+        0x96283ee2a88effe3,
+        0xbe5e1e2553863992,
+        0x2b0199fc2c85b8aa,
+        0x0eb72ddc81c52ca2,
+    ];
+
+    const SHA512_224: [u64; 8] = [
+        0x8c3d37c819544da2,
+        0x73e1996689dcd4d6,
+        0x1dfab7ae32ff9c82,
+        0x679dd514582f9fcf,
+        0x0f6d2b697bd44da8,
+        0x77e36f7304c48942,
+        0x3f9d85a86a1d36c8,
+        0x1112e6ad91d692a1,
+    ];
+}
 
 impl ClassicHasher for Sha512 {
     fn hash(&self, bytes: &[u8]) -> Vec<u8> {
@@ -82,16 +126,7 @@ impl ClassicHasher for Sha512 {
         }
 
         // Step 3. Initialize variables
-        let mut v: [u64; 8] = [
-            0x6a09e667f3bcc908,
-            0xbb67ae8584caa73b,
-            0x3c6ef372fe94f82b,
-            0xa54ff53a5f1d36f1,
-            0x510e527fade682d1,
-            0x9b05688c2b3e6c1f,
-            0x1f83d9abfb41bd6b,
-            0x5be0cd19137e2179,
-        ];
+        let mut v: [u64; 8] = Self::SHA512.clone();
 
         // Step 4. Process message in chunks of 128 bytes (1024 bits)
         for block in input.chunks_exact(128) {
