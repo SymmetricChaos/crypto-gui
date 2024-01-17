ea06c66077fd890d25d96de547e5df1161eb649d
SymmetricChaos <ajfraebel@gmail.com>
2024-01-17 08:31:43 -0500

    Update mersenne_twister.rs

--- rngs/src/mersenne_twister.rs
+++ rngs/src/mersenne_twister.rs
@@ -1,24 +1,87 @@
 use crate::traits::ClassicRng;
 
 pub struct MersenneTwister {
+    w: usize,
     n: usize,
+    m: usize,
+    a: u32,
+    u: usize,
+    d: u32,
+    s: usize,
+    b: u32,
+    t: usize,
+    c: u32,
+    l: usize,
+    index: usize,
+    lower_mask: u32,
+    upper_mask: u32,
+    arr: [u32; 624],
 }
 
 impl Default for MersenneTwister {
     fn default() -> Self {
-        let n = 624;
-        let mut arr = [0u32; 624];
-
-        Self { n }
+        let arr = [0u32; 624];
+        let lower_mask = (1 << 31) - 1;
+        let upper_mask = !lower_mask;
+        Self {
+            w: 32,         // word size
+            n: 624,        // array size
+            m: 397,        // middle word
+            a: 0x9908b0df, // coefficients of the rational normal form twist matrix
+            u: 11,         // tempering bitshift
+            d: 0xffffffff, // tempering bitmask
+            s: 7,          // tempering bitshift
+            b: 0x9d2c5680, // tempering bitmask
+            t: 15,         // tempering bitshift
+            c: 0xefc60000, // tempering bitmask
+            l: 18,         // tempering bitshift
+            index: 0,
+            lower_mask,
+            upper_mask,
+            arr,
+        }
     }
 }
 
 impl MersenneTwister {
-    pub fn ksa(&mut self, key: &[u8]) {}
+    pub fn ksa(&mut self, key: u32) {
+        self.arr[0] = key;
+        for i in 1..self.n {
+            self.arr[i] = 1812433253_u32
+                .wrapping_mul(self.arr[i - 1] ^ (self.arr[i - 1] >> (self.w - 2)))
+                .wrapping_add(1)
+        }
+    }
+
+    pub fn twist(&mut self) {
+        for i in 0..self.n {
+            let x =
+                (self.arr[i] & self.upper_mask) | (self.arr[(i + 1) % self.n] & self.lower_mask);
+            let mut x_a = x >> 1;
+            if x % 2 != 0 {
+                x_a = x_a ^ self.a;
+            }
+            self.arr[i] = self.arr[(i + self.m) % self.n] ^ x_a
+        }
+        self.index = 0;
+    }
 }
 
 impl ClassicRng for MersenneTwister {
     fn next_u32(&mut self) -> u32 {
-        todo!()
+        if self.index >= self.n {
+            if self.index > self.n {
+                self.ksa(5489) // Seed from 5489
+            }
+            self.twist();
+        }
+        let mut y = self.arr[self.index];
+        y = y ^ ((y >> self.u) & self.d);
+        y = y ^ ((y >> self.s) & self.b);
+        y = y ^ ((y >> self.t) & self.c);
+        y = y ^ (y >> self.l);
+        self.index += 1;
+
+        y
     }
 }
