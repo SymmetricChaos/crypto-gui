fd14b5e029af5e8d951175efca75747cbc700000
SymmetricChaos <ajfraebel@gmail.com>
2024-02-18 07:54:17 -0500

    hasher categories

--- hashers/src/ids/hasher_categories.rs
+++ hashers/src/ids/hasher_categories.rs
@@ -4,12 +4,13 @@ use std::fmt::Display;
 
 #[derive(Debug, PartialEq, Eq)]
 pub enum HasherCategory {
-    Hasher,
+    NonCryptographic,
+    Cryptographic,
 }
 
 impl Default for HasherCategory {
     fn default() -> Self {
-        Self::Hasher
+        Self::NonCryptographic
     }
 }
 
@@ -25,7 +26,8 @@ impl HasherCategory {
 impl Display for HasherCategory {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         let name = match self {
-            Self::Hasher => "Hasher",
+            Self::NonCryptographic => "Non-cryptographic",
+            Self::Cryptographic => "Cryptographic",
         };
         write!(f, "{}", name)
     }
