3a31b0dc621299ae1af2fd8500569fb86e35f3fb
SymmetricChaos <ajfraebel@gmail.com>
2024-02-25 08:58:10 -0500

    Update poly1305.rs

--- hashers/src/poly1305.rs
+++ hashers/src/poly1305.rs
@@ -1,6 +1,5 @@
 use crate::{errors::HasherError, traits::ClassicHasher};
-use crypto_bigint::U192;
-use num::{pow::Pow, BigUint, FromPrimitive, One};
+use num::{BigUint, FromPrimitive, Zero};
 use utils::byte_formatting::ByteFormat;
 
 pub struct Poly1305 {
@@ -20,7 +19,42 @@ impl Default for Poly1305 {
 }
 
 impl Poly1305 {
-    //const MODULUS: BigUint = 2 ** 130 - 5;
+    pub fn restrict_key(&mut self) {
+        for i in [3, 7, 11, 15] {
+            // if v[i] >= 16 {
+            //     return Err(HasherError::general(
+            //         "bytes 3, 7, 11, and 15 must be less than 16 (top four bits cleared)",
+            //     ));
+            // }
+            self.key[i] &= 0b11110000;
+        }
+        for i in [4, 8, 12] {
+            // if v[i] % 4 != 0 {
+            //     return Err(HasherError::general(
+            //         "bytes 4, 8, 12 must be multiplies of four (bottom two bits cleared)",
+            //     ));
+            // }
+            self.key[i] &= 0b00000011;
+        }
+    }
+
+    pub fn key_from_string_lossy(&mut self, s: &str) -> Result<(), HasherError> {
+        if s.len() != 64 {
+            return Err(HasherError::general(
+                "key must be given as exactly 64 hex digits",
+            ));
+        } else {
+            if let Ok(v) = ByteFormat::Hex.text_to_bytes(s) {
+                self.key = v.try_into().expect("failed to convert Vec<u8> to [u8; 32]");
+                self.restrict_key();
+            } else {
+                return Err(HasherError::general(
+                    "key must be given as exactly 64 hex digits",
+                ));
+            }
+        }
+        Ok(())
+    }
 }
 
 impl ClassicHasher for Poly1305 {
@@ -35,7 +69,7 @@ impl ClassicHasher for Poly1305 {
 
         let key = BigUint::from_bytes_le(&self.key);
         let blocks = bytes.chunks_exact(16);
-        let mut accumulator = BigUint::one();
+        let mut accumulator = BigUint::zero();
 
         // Create and pad the last block. If the remainder is empty it is ignored.
         let mut last_block = blocks.remainder().to_vec();
@@ -47,11 +81,14 @@ impl ClassicHasher for Poly1305 {
                 last_block.push(0x00);
             }
         }
+        last_block.reverse();
 
         // Message is taken 16 bytes at a time.
         for block in blocks {
             let mut block = block.to_vec();
-            block.insert(0, 0x01);
+            block.push(0x01);
+            block.reverse();
+            println!("main: {:02x?}", &block);
             accumulator += BigUint::from_bytes_le(&block);
             accumulator *= &key;
             accumulator %= &modulus;
@@ -59,6 +96,7 @@ impl ClassicHasher for Poly1305 {
 
         // Final step
         if last_block.len() != 0 {
+            println!("last: {:02x?}", &last_block);
             accumulator += BigUint::from_bytes_le(&last_block);
             accumulator *= &key;
             accumulator %= &modulus;
@@ -80,20 +118,21 @@ impl ClassicHasher for Poly1305 {
 }
 
 #[cfg(test)]
-mod md5_tests {
+mod poly1305_tests {
     use super::*;
 
     #[test]
-    fn test_suite() {
+    fn test_zero_input() {
         let mut hasher = Poly1305::default();
         hasher.input_format = ByteFormat::Hex;
         hasher.output_format = ByteFormat::Hex;
-        //https://datatracker.ietf.org/doc/html/draft-agl-tls-chacha20poly1305-00#section-8
-        hasher.key = [
-            0x74, 0x68, 0x69, 0x73, 0x20, 0x69, 0x73, 0x20, 0x33, 0x32, 0x2d, 0x62, 0x79, 0x74,
-            0x65, 0x20, 0x6b, 0x65, 0x79, 0x20, 0x66, 0x6f, 0x72, 0x20, 0x50, 0x6f, 0x6c, 0x79,
-            0x31, 0x33, 0x30, 0x35,
-        ];
+        //https://datatracker.ietf.org/doc/html/draft-agl-tls-chacha20poly1305-00#section-7
+        hasher
+            .key_from_string_lossy(
+                "746869732069732033322d62797465206b657920666f7220506f6c7931333035",
+            )
+            .unwrap();
+
         assert_eq!(
             "49ec78090e481ec6c26b33b91ccc0307",
             hasher
@@ -102,5 +141,41 @@ mod md5_tests {
                 )
                 .unwrap()
         );
+    }
+
+    #[test]
+    fn test_input() {
+        let mut hasher = Poly1305::default();
+        hasher.input_format = ByteFormat::Hex;
+        hasher.output_format = ByteFormat::Hex;
+        //https://datatracker.ietf.org/doc/html/draft-agl-tls-chacha20poly1305-00#section-7
+        hasher
+            .key_from_string_lossy(
+                "746869732069732033322d62797465206b657920666f7220506f6c7931333035",
+            )
+            .unwrap();
+
+        assert_eq!(
+            "a6f745008f81c916a20dcc74eef2b2f0",
+            hasher
+                .hash_bytes_from_string("48656c6c6f20776f726c6421")
+                .unwrap()
+        );
+    }
+
+    //ab0812724a7f1e342742cbed374d94d136c6b8795d45b3819830f2c04491faf0990c62e48b8018b2c3e4a0fa3134cb67fa83e158c994d961c4cb21095c1bf9
+    #[test]
+    fn test_chunks() {
+        let mut hasher = Poly1305::default();
+        hasher.input_format = ByteFormat::Hex;
+        hasher.output_format = ByteFormat::Hex;
+        /*
+        main: [01, d1, 94, 4d, 37, ed, cb, 42, 27, 34, 1e, 7f, 4a, 72, 12, 08, ab]
+        main: [01, f0, fa, 91, 44, c0, f2, 30, 98, 81, b3, 45, 5d, 79, b8, c6, 36]
+        main: [01, 67, cb, 34, 31, fa, a0, e4, c3, b2, 18, 80, 8b, e4, 62, 0c, 99]
+        last: [00, 01, f9, 1b, 5c, 09, 21, cb, c4, 61, d9, 94, c9, 58, e1, 83, fa]
+        */
+        hasher
+                .hash_bytes_from_string("ab0812724a7f1e342742cbed374d94d136c6b8795d45b3819830f2c04491faf0990c62e48b8018b2c3e4a0fa3134cb67fa83e158c994d961c4cb21095c1bf9").unwrap();
     }
 }
