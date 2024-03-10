01ee2efdd3be02d7d91fca77943e307d78011ad1
SymmetricChaos <ajfraebel@gmail.com>
2024-03-10 09:16:59 -0400

    Update blake2_controls.rs

--- src/hasher_panel/blake2_controls.rs
+++ src/hasher_panel/blake2_controls.rs
@@ -20,7 +20,9 @@ pub struct Blake2Frame {
     hasher_b: Blake2b,
     hasher_s: Blake2s,
     key_string_b: String,
+    valid_key_b: bool,
     key_string_s: String,
+    valid_key_s: bool,
     hash_len_b: String,
     hash_len_s: String,
 }
@@ -32,7 +34,9 @@ impl Default for Blake2Frame {
             hasher_b: Default::default(),
             hasher_s: Default::default(),
             key_string_b: String::new(),
+            valid_key_b: true,
             key_string_s: String::new(),
+            valid_key_s: true,
             hash_len_b: String::from("32"),
             hash_len_s: String::from("16"),
         }
@@ -40,22 +44,74 @@ impl Default for Blake2Frame {
 }
 
 impl Blake2Frame {
-    fn key_control(ui: &mut egui::Ui, string: &mut String, bytes: &mut Vec<u8>) {
+    fn key_control_b(&mut self, ui: &mut egui::Ui) {
+        let string = &mut self.key_string_b;
         ui.horizontal(|ui| {
             if ui.control_string(string).changed() {
-                *string = string.chars().filter(|c| c.is_ascii_hexdigit()).collect();
-                if ui.button("🎲").on_hover_text("randomize").clicked() {
-                    let mut rng = thread_rng();
-                    rng.fill_bytes(bytes);
-                    *string = ByteFormat::Hex.byte_slice_to_text(&bytes)
-                }
-                if let Ok(new) = ByteFormat::Hex.text_to_bytes(string) {
-                    *bytes = new;
-                } else {
-                    ui.error_text("unable to parse input");
+                *string = string
+                    .chars()
+                    .filter(|c| c.is_ascii_hexdigit())
+                    .take(128) // 128 characters is 64 bytes
+                    .collect();
+                self.valid_key_b = string.len() % 2 != 0;
+                if self.valid_key_b {
+                    if let Ok(new) = ByteFormat::Hex.text_to_bytes(string) {
+                        match new.try_into() {
+                            Ok(key) => self.hasher_b.key = key,
+                            Err(_) => unreachable!(),
+                        }
+                    } else {
+                        unreachable!("unable to parse input");
+                    }
                 }
             };
+            if ui.button("🎲").on_hover_text("randomize").clicked() {
+                let mut rng = thread_rng();
+                rng.fill_bytes(&mut self.hasher_b.key);
+                *string = ByteFormat::Hex.byte_slice_to_text(&self.hasher_b.key)
+            };
         });
+
+        if !self.valid_key_b {
+            ui.error_text("invalid key");
+        } else {
+            ui.error_text("");
+        }
+    }
+
+    fn key_control_s(&mut self, ui: &mut egui::Ui) {
+        let string = &mut self.key_string_s;
+        ui.horizontal(|ui| {
+            if ui.control_string(string).changed() {
+                *string = string
+                    .chars()
+                    .filter(|c| c.is_ascii_hexdigit())
+                    .take(64) // 64 characters is 32 bytes
+                    .collect();
+                self.valid_key_s = string.len() % 2 != 0;
+                if self.valid_key_s {
+                    if let Ok(new) = ByteFormat::Hex.text_to_bytes(string) {
+                        match new.try_into() {
+                            Ok(key) => self.hasher_s.key = key,
+                            Err(_) => unreachable!(),
+                        }
+                    } else {
+                        unreachable!("unable to parse input");
+                    }
+                }
+            };
+            if ui.button("🎲").on_hover_text("randomize").clicked() {
+                let mut rng = thread_rng();
+                rng.fill_bytes(&mut self.hasher_s.key);
+                *string = ByteFormat::Hex.byte_slice_to_text(&self.hasher_s.key)
+            };
+        });
+
+        if !self.valid_key_s {
+            ui.error_text("invalid key");
+        } else {
+            ui.error_text("");
+        }
     }
 
     fn hash_len_control(ui: &mut egui::Ui, string: &mut String, value: &mut usize, max: usize) {
@@ -130,11 +186,11 @@ impl HasherFrame for Blake2Frame {
         match self.variant {
             Blake2Variant::Big => {
                 ui.label("BLAKE2b has a maximum key size of of 64 bytes (512 bits).");
-                Self::key_control(ui, &mut self.key_string_b, &mut self.hasher_b.key);
+                self.key_control_b(ui);
             }
             Blake2Variant::Small => {
                 ui.label("BLAKE2s has a maximum key size of of 32 bytes (256 bits).");
-                Self::key_control(ui, &mut self.key_string_s, &mut self.hasher_s.key)
+                self.key_control_s(ui);
             }
         };
 
