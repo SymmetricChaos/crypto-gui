bfb475dc34bb488ee426cf4677e9dd90b76fb8e5
SymmetricChaos <ajfraebel@gmail.com>
2024-03-15 20:46:33 -0400

    Update crc.rs

--- hashers/src/checksum/crc.rs
+++ hashers/src/checksum/crc.rs
@@ -1,10 +1,6 @@
 use crate::{errors::HasherError, traits::ClassicHasher};
 
-use utils::{
-    bit_polynomial::BitPolynomial,
-    bits::bit_vec_from_bytes,
-    byte_formatting::{ByteFormat, ByteFormatError},
-};
+use utils::{bit_polynomial::BitPolynomial, bits::bit_vec_from_bytes, byte_formatting::ByteFormat};
 
 pub enum CrcAlgorithm {
     Crc32,
@@ -16,12 +12,18 @@ impl CrcAlgorithm {
             CrcAlgorithm::Crc32 => 32,
         }
     }
+
+    pub fn generator(&self) -> BitPolynomial {
+        match self {
+            CrcAlgorithm::Crc32 => todo!(),
+        }
+    }
 }
 
 pub struct CyclicRedundancyCheckHash {
     pub input_format: ByteFormat,
     pub output_format: ByteFormat,
-    pub generator: BitPolynomial,
+    // pub generator: BitPolynomial,
     pub mode: CrcAlgorithm,
 }
 
@@ -30,22 +32,22 @@ impl Default for CyclicRedundancyCheckHash {
         Self {
             input_format: ByteFormat::Hex,
             output_format: ByteFormat::Hex,
-            generator: BitPolynomial::from_str("1101").unwrap(),
+            // generator: BitPolynomial::from_str("1101").unwrap(),
             mode: CrcAlgorithm::Crc32,
         }
     }
 }
 
 impl CyclicRedundancyCheckHash {
-    pub fn check_bits(&self) -> usize {
-        self.generator.degree()
-    }
+    // pub fn check_bits(&self) -> usize {
+    //     self.generator.degree()
+    // }
 
-    pub fn set_generator_from_hex(&mut self, text: &str) -> Result<(), ByteFormatError> {
-        let bytes = ByteFormat::Hex.text_to_bytes(text)?;
-        self.generator = BitPolynomial::from_bytes(&bytes);
-        Ok(())
-    }
+    // pub fn set_generator_from_hex(&mut self, text: &str) -> Result<(), ByteFormatError> {
+    //     let bytes = ByteFormat::Hex.text_to_bytes(text)?;
+    //     self.generator = BitPolynomial::from_bytes(&bytes);
+    //     Ok(())
+    // }
 }
 
 impl ClassicHasher for CyclicRedundancyCheckHash {
@@ -54,7 +56,7 @@ impl ClassicHasher for CyclicRedundancyCheckHash {
         let poly = BitPolynomial::from(bit_vec_from_bytes(bytes));
 
         // The remainder of the division is the CRC syndrome
-        let (_, r) = poly.div_rem(&self.generator);
+        let (_, r) = poly.div_rem(&self.mode.generator());
 
         // Convert the CRC syndrome into bytes for output
         ByteFormat::Bit.text_to_bytes(&r.bit_string()).unwrap()
