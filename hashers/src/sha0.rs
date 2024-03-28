059a8f396518ce6ffe4a397068522ad598fee313
SymmetricChaos <ajfraebel@gmail.com>
2024-03-28 08:50:48 -0400

    Update sha0.rs

--- hashers/src/sha0.rs
+++ hashers/src/sha0.rs
@@ -122,31 +122,3 @@ impl ClassicHasher for Sha0 {
         Ok(self.output_format.byte_slice_to_text(&out))
     }
 }
-
-#[cfg(test)]
-mod sha1_tests {
-    use super::*;
-
-    #[test]
-    fn test_suite() {
-        let mut hasher = Sha0::default();
-        hasher.input_format = ByteFormat::Utf8;
-        hasher.output_format = ByteFormat::Hex;
-        assert_eq!(
-            "da39a3ee5e6b4b0d3255bfef95601890afd80709",
-            hasher.hash_bytes_from_string("").unwrap()
-        );
-        assert_eq!(
-            "2fd4e1c67a2d28fced849ee1bb76e7391b93eb12",
-            hasher
-                .hash_bytes_from_string("The quick brown fox jumps over the lazy dog")
-                .unwrap()
-        );
-        assert_eq!(
-            "de9f2c7fd25e1b3afad3e85a0bd17d9b100db4b3",
-            hasher
-                .hash_bytes_from_string("The quick brown fox jumps over the lazy cog")
-                .unwrap()
-        );
-    }
-}
