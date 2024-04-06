f5c8cc6ee468bef167ed89c26da5257d07eed9fc
SymmetricChaos <ajfraebel@gmail.com>
2024-04-05 23:15:57 -0400

    bugs found and fixed

--- hashers/src/keccak.rs
+++ hashers/src/keccak.rs
@@ -278,9 +278,9 @@ impl Keccak {
         Self {
             input_format: ByteFormat::Hex,
             output_format: ByteFormat::Hex,
-            rate: 72 / 8,
-            capacity: 128 / 8,
-            output_size: 64 / 8,
+            rate: 576 / 8,
+            capacity: 1024 / 8,
+            output_size: 512 / 8,
             domain: Domain::Sha3,
         }
     }
@@ -396,12 +396,33 @@ mod keccak_tests {
     }
 
     #[test]
-    fn test() {
+    fn test_empty_input_all() {
+        let hasher = Keccak::sha3_224();
+        let output = hasher.hash_bytes_from_string("").unwrap();
+        assert_eq!(
+            "6b4e03423667dbb73b6e15454f0eb1abd4597f9a1b078e3f5b5a6bc7",
+            output
+        );
+
         let hasher = Keccak::sha3_256();
         let output = hasher.hash_bytes_from_string("").unwrap();
         assert_eq!(
             "a7ffc6f8bf1ed76651c14756a061d662f580ff4de43b49fa82d80a4b80f8434a",
             output
-        )
+        );
+
+        let hasher = Keccak::sha3_384();
+        let output = hasher.hash_bytes_from_string("").unwrap();
+        assert_eq!(
+            "0c63a75b845e4f7d01107d852e4c2485c51a50aaaa94fc61995e71bbee983a2ac3713831264adb47fb6bd1e058d5f004",
+            output
+        );
+
+        let hasher = Keccak::sha3_512();
+        let output = hasher.hash_bytes_from_string("").unwrap();
+        assert_eq!(
+            "a69f73cca23a9ac5c8b567dc185a756e97c982164fe25859e0d1dcc1475c80a615b2123af1f5f94c11e3e9402c3ac558f500199d95b6d3e301758586281dcd26",
+            output
+        );
     }
 }
