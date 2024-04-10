28ece07e76f7c4671af0d2927ac0bb4000c14a39
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2024-04-10 08:29:01 -0400

    Update sha3_controls.rs

--- src/hasher_panel/sha3_controls.rs
+++ src/hasher_panel/sha3_controls.rs
@@ -1,4 +1,4 @@
-use egui::{DragValue, RichText};
+use egui::DragValue;
 use hashers::{
     errors::HasherError,
     keccak::{KeccackState, Keccak},
@@ -24,6 +24,7 @@ pub struct Sha3Frame {
     variant: Sha3Variant,
     shake_output_len: usize,
     example_state: KeccackState,
+    example_round: usize,
 }
 
 impl Default for Sha3Frame {
@@ -33,6 +34,7 @@ impl Default for Sha3Frame {
             variant: Sha3Variant::Sha3_256,
             shake_output_len: 128,
             example_state: KeccackState::new(),
+            example_round: 0,
         }
     }
 }
@@ -94,7 +96,7 @@ impl HasherFrame for Sha3Frame {
         });
 
         ui.add_space(8.0);
-        ui.subheading("SHAKE Output Length");
+        ui.subheading("SHAKE Output Length (in bytes)");
         ui.add_enabled(
             self.variant == Sha3Variant::Shake128 || self.variant == Sha3Variant::Shake256,
             DragValue::new(&mut self.shake_output_len).clamp_range(1..=512),
@@ -103,12 +105,12 @@ impl HasherFrame for Sha3Frame {
         ui.add_space(16.0);
         ui.subheading("Discussion");
         match self.variant {
-            Sha3Variant::Sha3_224 => ui.label("SHA3-224 "),
-            Sha3Variant::Sha3_256 => ui.label("SHA3-256 "),
-            Sha3Variant::Sha3_384 => ui.label("SHA3-384 "),
-            Sha3Variant::Sha3_512 => ui.label("SHA3-512 "),
-            Sha3Variant::Shake128 => ui.label("SHAKE128 "),
-            Sha3Variant::Shake256 => ui.label("SHAKE256 "),
+            Sha3Variant::Sha3_224 => ui.label("SHA3-224 keeps 448 bits of state reserved and absorbs 1152 bits at a time."),
+            Sha3Variant::Sha3_256 => ui.label("SHA3-256 keeps 512 bits of state reserved and absorbs 1088 bits at a time."),
+            Sha3Variant::Sha3_384 => ui.label("SHA3-384 keeps 768 bits of state reserved and absorbs 832 bits at a time."),
+            Sha3Variant::Sha3_512 => ui.label("SHA3-512 keeps 1024 bits of state reserved and absorbs 576 bits at a time."),
+            Sha3Variant::Shake128 => ui.label("SHAKE128 keeps 256 bits of state reserved and absorbs 1344 bits at a time. It can be set to return any number of bits."),
+            Sha3Variant::Shake256 => ui.label("SHAKE256 keeps 512 bits of state reserved and absorbs 1088 bits at a time. It can be set to return any number of bits."),
         };
 
         ui.add_space(16.0);
@@ -116,25 +118,43 @@ impl HasherFrame for Sha3Frame {
             for y in 0..5 {
                 ui.horizontal(|ui| {
                     for x in 0..5 {
-                        ui.add(DragValue::new(&mut self.example_state[x][y]));
+                        ui.add(
+                            DragValue::new(&mut self.example_state[x][y])
+                                .hexadecimal(16, false, false),
+                        );
                     }
                 });
             }
 
-            if ui.button("Theta").clicked() {
-                self.example_state.theta()
+            ui.subheading("Round Number");
+            ui.add(DragValue::new(&mut self.example_round).clamp_range(0..=23));
+
+            ui.subheading("Steps");
+            ui.group(|ui| {
+                if ui.button("Theta").clicked() {
+                    self.example_state.theta()
+                }
+                if ui.button("Rho").clicked() {
+                    self.example_state.rho()
+                }
+                if ui.button("Pi").clicked() {
+                    self.example_state.pi()
+                }
+                if ui.button("Chi").clicked() {
+                    self.example_state.chi()
+                }
+                if ui.button("Iota").clicked() {
+                    self.example_state.iota(self.example_round)
+                }
+            });
+
+            if ui.button("Single Round").clicked() {
+                self.example_state.round(self.example_round);
+                self.example_round += 1;
             }
-            if ui.button("Rho").clicked() {
-                self.example_state.rho()
-            }
-            if ui.button("Pi").clicked() {
-                self.example_state.pi()
-            }
-            if ui.button("Chi").clicked() {
-                self.example_state.chi()
-            }
-            if ui.button("Iota").clicked() {
-                self.example_state.chi()
+
+            if ui.button("Full Permutation (24 Rounds)").clicked() {
+                self.example_state.keccak_f()
             }
         });
 
