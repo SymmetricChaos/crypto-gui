dc629c78070ce7cbb365fea8d71668b430fab91c
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2024-06-22 17:36:42 -0400

    seal is very confusing

--- ciphers/src/digital/seal.rs
+++ ciphers/src/digital/seal.rs
@@ -1,11 +1,13 @@
+use num::Integer;
+
 use crate::{Cipher, CipherError};
 
 // https://www.cs.ucdavis.edu/~rogaway/papers/seal.pdf
 pub struct Seal {
-    registers: [u32; 4],
     pub key: [u32; 5], // 160-bit key
     n: u32,
-    pub r: [u32; 256],
+    l: u32,            // number of output bits, limited to 524288 (64 * 1024 * 8)
+    pub r: [u32; 256], // r is sometimes not completely used depending on the value of l
     pub t: [u32; 512],
     pub s: [u32; 256],
 }
@@ -13,9 +15,9 @@ pub struct Seal {
 impl Default for Seal {
     fn default() -> Self {
         Self {
-            registers: [0; 4],
             key: [0; 5],
             n: 0,
+            l: 524288,
             r: [0; 256],
             t: [0; 512],
             s: [0; 256],
@@ -31,31 +33,30 @@ impl Seal {
         let mut d = self.key[3];
         let mut e = self.key[4];
 
-        // Initial 512 bit state
         let mut w = [0u32; 80];
         w[0] = i;
 
-        // Extend the 16 words to 80 words
-        for i in 16..80 {
-            w[i] = (w[i - 3] ^ w[i - 8] ^ w[i - 14] ^ w[i - 16]).rotate_left(1)
-        }
+        // // Extend the 16 words to 80 words
+        // for i in 16..80 {
+        //     w[i] = (w[i - 3] ^ w[i - 8] ^ w[i - 14] ^ w[i - 16]).rotate_left(1)
+        // }
 
-        for i in 0..80 {
+        for n in 0..80 {
             let mut f = 0;
             let mut g = 0;
-            if i < 20 {
+            if n < 20 {
                 f = (b & c) | (!b & d);
                 g = 0x5a827999;
             }
-            if i >= 20 && i < 40 {
+            if n >= 20 && n < 40 {
                 f = b ^ c ^ d;
                 g = 0x6ed9eba1;
             }
-            if i >= 40 && i < 60 {
+            if n >= 40 && n < 60 {
                 f = (b & c) | (b & d) | (c & d);
                 g = 0x8f1bbcdc;
             }
-            if i >= 60 {
+            if n >= 60 {
                 f = b ^ c ^ d;
                 g = 0xca62c1d6;
             }
@@ -65,7 +66,7 @@ impl Seal {
                 .wrapping_add(f)
                 .wrapping_add(e)
                 .wrapping_add(g)
-                .wrapping_add(w[i]);
+                .wrapping_add(w[n]);
             e = d;
             d = c;
             c = b.rotate_left(30);
@@ -82,17 +83,18 @@ impl Seal {
     }
 
     pub fn gamma(&self, i: u32) -> u32 {
-        self.g(i / 5)[(i as usize) % 5]
+        let (q, r) = i.div_mod_floor(&5);
+        self.g(q)[r as usize]
     }
 
     pub fn derive_tables(&mut self) {
-        for i in 0..512 {
+        for i in 0..self.t.len() {
             self.t[i] = self.gamma(i as u32);
         }
-        for i in 0..256 {
+        for i in 0..self.s.len() {
             self.s[i] = self.gamma(0x1000 + i as u32);
         }
-        for i in 0..256 {
+        for i in 0..self.r.len() {
             self.r[i] = self.gamma(0x2000 + i as u32);
         }
     }
@@ -122,6 +124,8 @@ mod seal_tests {
 
         cipher.derive_tables();
 
-        println!("{:08x?}", cipher.r);
+        println!("R: {:08x?}", &cipher.r[0..4]);
+        println!("T: {:08x?}", &cipher.t[0..4]);
+        println!("S: {:08x?}", &cipher.s[0..4]);
     }
 }
