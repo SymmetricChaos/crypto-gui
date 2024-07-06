438133166676a0387299f7a82f3413d4b6244a24
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2024-07-06 19:51:46 -0400

    Update fnv_controls.rs

--- src/hasher_panel/fnv_controls.rs
+++ src/hasher_panel/fnv_controls.rs
@@ -3,7 +3,7 @@ use crate::ui_elements::UiElements;
 use super::HasherFrame;
 use hashers::{
     errors::HasherError,
-    fnv::{Fnv, FnvSize, O128, O256, O32, O64, P128, P256, P32, P64},
+    fnv::{Fnv, FnvSize, O128, O32, O64, P128, P32, P64},
     traits::ClassicHasher,
 };
 
@@ -54,21 +54,22 @@ impl HasherFrame for FnvFrame {
         ui.add_space(16.0);
         ui.subheading("Hash Size");
         ui.label("The FNV primes are of a specific form, close to a power of 256, which the developers found to be highly effective at dispersing the bits of the input throughout the state. Four FNV variants are provided here but the original FNV paper also defines constants for 512 and 1024 versions of the algorithm. However these run more slowly, take more space to store, and offer no practical increase in collision resistance.");
+        ui.add_space(4.0);
         match self.hasher.size {
-            FnvSize::P32 => ui.label(format!(
+            FnvSize::P32 => ui.mono_strong(format!(
                 "The 32-bit FNV prime is {} and the initialization value is {}",
                 P32, O32,
             )),
-            FnvSize::P64 => ui.label(format!(
+            FnvSize::P64 => ui.mono_strong(format!(
                 "The 64-bit FNV prime is {} and the initialization value is {}",
                 P64, O64,
             )),
-            FnvSize::P128 => ui.label(format!(
+            FnvSize::P128 => ui.mono_strong(format!(
                 "The 128-bit FNV prime is {} and the initialization value is {}",
                 P128, O128,
             )),
             // The P256 constants don't format nicely so they are hard-coded here.
-            FnvSize::P256 => ui.label(
+            FnvSize::P256 => ui.mono_strong(
                 "The 256-bit FNV prime is 374144419156711147060143317175368453031918731002211 and the initialization value is 100029257958052580907070968620625704837092796014241193945225284501741471925557", 
             ),
         };
