7bbb51c546013395428450715bd536a0baa93350
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2024-07-19 20:51:24 -0400

    a5/1

--- ciphers/src/digital/stream_ciphers/a51.rs
+++ ciphers/src/digital/stream_ciphers/a51.rs
@@ -1,6 +1,46 @@
 use crate::Cipher;
 
-pub struct A51 {}
+pub struct Lfsr {
+    pub bits: Vec<Bit>,
+    pub taps: Vec<bool>,
+}
+
+impl Lfsr {
+    pub fn new(taps: Vec<bool>) -> Self {
+        Self {
+            bits: vec![0; taps.len()],
+            taps,
+        }
+    }
+
+    pub fn next_bit(&mut self) -> Bit {
+        let mut next_bit = Bit::zero();
+        for (bit, tap) in self.bits.iter().zip(self.taps.iter()) {
+            if *tap {
+                next_bit ^= *bit;
+            }
+        }
+        self.bits.pop();
+        self.bits.insert(0, next_bit);
+        next_bit
+    }
+}
+
+pub struct A51 {
+    lfsr1: Lfsr,
+    lfsr2: Lfsr,
+    lfsr3: Lfsr,
+}
+
+impl Default for A51 {
+    fn default() -> Self {
+        Self {
+            lfsr1: Default::default(),
+            lfsr2: Default::default(),
+            lfsr3: Default::default(),
+        }
+    }
+}
 
 impl Cipher for A51 {
     fn encrypt(&self, text: &str) -> Result<String, crate::CipherError> {
