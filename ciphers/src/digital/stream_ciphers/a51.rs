d14f52de22a74a0afd9400f1f214ed7fee107119
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2024-07-26 18:11:32 -0400

    Update a51.rs

--- ciphers/src/digital/stream_ciphers/a51.rs
+++ ciphers/src/digital/stream_ciphers/a51.rs
@@ -21,32 +21,26 @@ impl Default for A51 {
 }
 
 impl A51 {
-    fn step(&mut self) {
+    fn step(&mut self) -> Bit {
         let clock_bits = [
             self.lfsrs[0].bits[9],
             self.lfsrs[1].bits[11],
             self.lfsrs[2].bits[11],
         ];
-        // Hilariously long winded way to find majority bit
-        // If the sum is 2 or 3 then 1 is the majority
-        // If the sum is 0 or 1 then 0 is the majority
-        let n = clock_bits.into_iter().fold(0u8, |acc, b| acc + b);
 
-        if n < 2 {
-            // Majority 0
-            for (i, b) in clock_bits.into_iter().enumerate() {
-                if b == Bit::Zero {
-                    self.lfsrs[i].next_bit();
-                }
-            }
-        } else {
-            // Majority 1
-            for (i, b) in clock_bits.into_iter().enumerate() {
-                if b == Bit::One {
-                    self.lfsrs[i].next_bit();
-                }
+        // Calculate majority bit
+        let n = (clock_bits[0] & clock_bits[1])
+            | (clock_bits[0] & clock_bits[2])
+            | (clock_bits[1] & clock_bits[2]);
+
+        let mut out = Bit::Zero;
+        for (i, b) in clock_bits.into_iter().enumerate() {
+            if b == n {
+                out ^= self.lfsrs[i].next_bit();
             }
         }
+
+        out
     }
 }
 
