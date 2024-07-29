2bbb1a273b142f4610041d837222663219584a76
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2024-07-29 00:07:18 -0400

    corrected lfsr32

--- rngs/src/lfsr32.rs
+++ rngs/src/lfsr32.rs
@@ -1,16 +1,9 @@
 use crate::traits::ClassicRng;
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
-pub enum LfsrMode {
-    Fibonncci,
-    Galois,
-}
-
 pub struct Lfsr32 {
     pub register: u32,
     pub taps: u32,
     pub mask: u32,
-    pub mode: LfsrMode,
 }
 
 impl Default for Lfsr32 {
@@ -21,14 +14,32 @@ impl Default for Lfsr32 {
 
 impl Lfsr32 {
     pub fn from_taps(taps: u32) -> Self {
+        // Only 31 bits are usable
+        assert!(taps < 0xefffff);
         let n = 32 - taps.leading_zeros();
-        let mask = n.pow(2) - 1;
+        let mask = 2_u32.pow(n) - 1;
         Self {
             register: 1_u32,
             taps,
             mask,
-            mode: LfsrMode::Fibonncci,
         }
+    }
+
+    pub fn from_taps_and_register(taps: u32, register: u32) -> Self {
+        // Only 31 bits are usable
+        assert!(taps < 0xefffff);
+        let n = 32 - taps.leading_zeros();
+        let mask = n.pow(2) - 1;
+        Self {
+            register: register & mask,
+            taps,
+            mask,
+        }
+    }
+
+    pub fn get_bit(&self, idx: u32) -> u32 {
+        assert!(idx < 32);
+        (self.register & (1 << idx)).count_ones()
     }
 
     pub fn next_bit(&mut self) -> u32 {
@@ -41,16 +52,36 @@ impl Lfsr32 {
         bit
     }
 
-    pub fn peek_next_bit(&self) -> u32 {
-        (self.register & self.taps).count_ones() & 1
+    pub fn next_byte(&mut self) -> u8 {
+        let mut out = 0;
+        for _ in 0..8 {
+            out <<= 1;
+            out |= self.next_bit() as u8;
+        }
+        out
     }
 }
 
 impl ClassicRng for Lfsr32 {
     fn next_u32(&mut self) -> u32 {
-        todo!()
+        let mut out = 0;
+        for _ in 0..32 {
+            out <<= 1;
+            out |= self.next_bit();
+        }
+        out
     }
 }
 
 #[cfg(test)]
-mod lfsr32_tests {}
+mod lfsr32_tests {
+    use super::Lfsr32;
+
+    #[test]
+    fn test_init() {
+        let rng = Lfsr32::default();
+        assert_eq!(0b00000000000000001111111111111111, rng.mask);
+        assert_eq!(0b00000000000000001011010000000000, rng.taps);
+        assert_eq!(0b00000000000000000000000000000001, rng.register);
+    }
+}
