9c9c6ca07faae8e94c8a51aee936570238521401
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2024-08-28 14:57:30 -0400

    updated fnv

--- hashers/src/fnv.rs
+++ hashers/src/fnv.rs
@@ -1,26 +1,90 @@
 use crate::traits::ClassicHasher;
-use crypto_bigint::{ArrayEncoding, U256};
+use crypto_bigint::{ArrayEncoding, U1024, U256, U512};
+use std::fmt::Display;
+// use lazy_static::lazy_static;
+use num::FromPrimitive;
+use strum::EnumIter;
 use utils::byte_formatting::ByteFormat;
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq)]
+// lazy_static! {
+//     pub static ref P32: BigUint = BigUint::from_str("16777619").unwrap();
+//     pub static ref O32: BigUint = BigUint::from_str("2166136261").unwrap();
+//     pub static ref P64: BigUint = BigUint::from_str("1099511628211").unwrap();
+//     pub static ref O64: BigUint = BigUint::from_str("14695981039346656037").unwrap();
+//     pub static ref P128: BigUint = BigUint::from_str("309485009821345068724781371").unwrap();
+//     pub static ref O128: BigUint = BigUint::from_str("144066263297769815596495629667062367629").unwrap();
+//     pub static ref P256: BigUint = BigUint::from_str("374144419156711147060143317175368453031918731002211").unwrap();
+//     pub static ref O256: BigUint = BigUint::from_str("100029257958052580907070968620625704837092796014241193945225284501741471925557").unwrap();
+//     pub static ref P512: BigUint = BigUint::from_str("35835915874844867368919076489095108449946327955754392558399825615420669938882575126094039892345713852759").unwrap();
+//     pub static ref O512: BigUint = BigUint::from_str("9659303129496669498009435400716310466090418745672637896108374329434462657994582932197716438449813051892206539805784495328239340083876191928701583869517785").unwrap();
+//     pub static ref P1024: BigUint = BigUint::from_str("5016456510113118655434598811035278955030765345404790744303017523831112055108147451509157692220295382716162651878526895249385292291816524375083746691371804094271873160484737966720260389217684476157468082573").unwrap();
+//     pub static ref O1024: BigUint = BigUint::from_str("14197795064947621068722070641403218320880622795441933960878474914617582723252296732303717722150864096521202355549365628174669108571814760471015076148029755969804077320157692458563003215304957150157403644460363550505412711285966361610267868082893823963790439336411086884584107735010676915").unwrap();
+// }
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, EnumIter)]
 pub enum FnvSize {
-    P32,
-    P64,
-    P128,
-    P256,
+    L32,
+    L64,
+    L128,
+    L256,
+    L512,
+    L1024,
 }
 
-pub const P32: u32 = 16777619;
-pub const P64: u64 = 1099511628211;
-pub const P128: u128 = 309485009821345068724781371;
-pub const P256: U256 =
-    U256::from_be_hex("0000000000000000000001000000000000000000000000000000000000000163");
+impl Display for FnvSize {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            FnvSize::L32 => write!(f, "32-bit"),
+            FnvSize::L64 => write!(f, "64-bit"),
+            FnvSize::L128 => write!(f, "128-bit"),
+            FnvSize::L256 => write!(f, "256-bit"),
+            FnvSize::L512 => write!(f, "512-bit"),
+            FnvSize::L1024 => write!(f, "1024-bit"),
+        }
+    }
+}
 
-pub const O32: u32 = 2166136261;
-pub const O64: u64 = 14695981039346656037;
-pub const O128: u128 = 144066263297769815596495629667062367629;
-pub const O256: U256 =
-    U256::from_be_hex("DD268DBCAAC550362D98C384C4E576CCC8B1536847B6BBB31023B4C8CAEE0535");
+macro_rules! hash_bytes {
+    ($name: ident, $type: ty, $prime: expr, $offset: expr) => {
+        pub fn $name(&self, bytes: &[u8]) -> Vec<u8> {
+            let mut state = match self.zero_basis {
+                true => 0,
+                false => $offset,
+            };
+            for byte in bytes {
+                if self.alternate {
+                    state ^= <$type>::from_u8(*byte).unwrap();
+                    state = state.wrapping_mul($prime)
+                } else {
+                    state = state.wrapping_mul($prime);
+                    state ^= <$type>::from_u8(*byte).unwrap();
+                }
+            }
+            state.to_be_bytes().to_vec()
+        }
+    };
+}
+
+macro_rules! hash_bytes_crypto_bigint {
+    ($name: ident, $type: ty, $prime: expr, $offset: expr) => {
+        pub fn $name(&self, bytes: &[u8]) -> Vec<u8> {
+            let mut state = match self.zero_basis {
+                true => <$type>::ZERO,
+                false => $offset,
+            };
+            for byte in bytes {
+                if self.alternate {
+                    state ^= <$type>::from_u8(*byte);
+                    state = state.wrapping_mul(&$prime)
+                } else {
+                    state = state.wrapping_mul(&$prime);
+                    state ^= <$type>::from_u8(*byte);
+                }
+            }
+            state.to_be_byte_array().to_vec()
+        }
+    };
+}
 
 pub struct Fnv {
     pub input_format: ByteFormat,
@@ -35,7 +99,7 @@ impl Default for Fnv {
         Self {
             input_format: ByteFormat::Utf8,
             output_format: ByteFormat::Hex,
-            size: FnvSize::P64,
+            size: FnvSize::L64,
             alternate: true,
             zero_basis: false,
         }
@@ -68,90 +132,50 @@ impl Fnv {
         self
     }
 
-    pub fn hash_byte_32(&self, state: &mut u32, byte: u8) {
-        if self.alternate {
-            *state ^= byte as u32;
-            *state = state.wrapping_mul(P32)
-        } else {
-            *state = state.wrapping_mul(P32);
-            *state ^= byte as u32;
-        }
-    }
+    hash_bytes!(hash_bytes_32, u32, 16777619_u32, 2166136261_u32);
+    hash_bytes!(
+        hash_bytes_64,
+        u64,
+        1099511628211_u64,
+        14695981039346656037_u64
+    );
+    hash_bytes!(
+        hash_bytes_128,
+        u128,
+        309485009821345068724781371_u128,
+        144066263297769815596495629667062367629_u128
+    );
+    hash_bytes_crypto_bigint!(
+        hash_bytes_256,
+        U256,
+        U256::from_be_hex("0000000000000000000001000000000000000000000000000000000000000163"),
+        U256::from_be_hex("DD268DBCAAC550362D98C384C4E576CCC8B1536847B6BBB31023B4C8CAEE0535")
+    );
 
-    pub fn hash_byte_64(&self, state: &mut u64, byte: u8) {
-        if self.alternate {
-            *state ^= byte as u64;
-            *state = state.wrapping_mul(P64)
-        } else {
-            *state = state.wrapping_mul(P64);
-            *state ^= byte as u64;
-        }
-    }
+    hash_bytes_crypto_bigint!(
+        hash_bytes_512,
+        U512,
+        U512::from_be_hex("00000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000000000000000000000000157"),
+        U512::from_be_hex("b86db0b1171f4416dca1e50f309990acac87d059c90000000000000000000d21e948f68a34c192f62ea79bc942dbe7ce182036415f56e34bac982aac4afe9fd9")
+    );
 
-    pub fn hash_byte_128(&self, state: &mut u128, byte: u8) {
-        if self.alternate {
-            *state ^= byte as u128;
-            *state = state.wrapping_mul(P128)
-        } else {
-            *state = state.wrapping_mul(P128);
-            *state ^= byte as u128;
-        }
-    }
-
-    pub fn hash_byte_256(&self, state: &mut U256, byte: u8) {
-        if self.alternate {
-            *state ^= U256::from_u8(byte);
-            *state = state.wrapping_mul(&P256)
-        } else {
-            *state = state.wrapping_mul(&P256);
-            *state ^= U256::from_u8(byte);
-        }
-    }
+    hash_bytes_crypto_bigint!(
+        hash_bytes_1024,
+        U1024,
+        U1024::from_be_hex("000000000000000000000000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000018d"),
+        U1024::from_be_hex("0000000000000000005f7a76758ecc4d32e56d5a591028b74b29fc4223fdada16c3bf34eda3674da9a21d9000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000004c6d7eb6e73802734510a555f256cc005ae556bde8cc9c6a93b21aff4b16c71ee90b3")
+    );
 }
 
 impl ClassicHasher for Fnv {
     fn hash(&self, bytes: &[u8]) -> Vec<u8> {
         match self.size {
-            FnvSize::P32 => {
-                let mut state = match self.zero_basis {
-                    true => 0,
-                    false => O32,
-                };
-                for byte in bytes {
-                    self.hash_byte_32(&mut state, *byte)
-                }
-                state.to_be_bytes().to_vec()
-            }
-            FnvSize::P64 => {
-                let mut state = match self.zero_basis {
-                    true => 0,
-                    false => O64,
-                };
-                for byte in bytes {
-                    self.hash_byte_64(&mut state, *byte)
-                }
-                state.to_be_bytes().to_vec()
-            }
-            FnvSize::P128 => {
-                let mut state = match self.zero_basis {
-                    true => 0,
-                    false => O128,
-                };
-                for byte in bytes {
-                    self.hash_byte_128(&mut state, *byte)
-                }
-                state.to_be_bytes().to_vec()
-            }
-            FnvSize::P256 => {
-                let mut state = match self.zero_basis {
-                    true => U256::ZERO,
-                    false => O256,
-                };
-                for byte in bytes {
-                    self.hash_byte_256(&mut state, *byte)
-                }
-                state.to_be_byte_array().to_vec()
-            }
+            FnvSize::L32 => self.hash_bytes_32(bytes),
+            FnvSize::L64 => self.hash_bytes_64(bytes),
+            FnvSize::L128 => self.hash_bytes_128(bytes),
+            FnvSize::L256 => self.hash_bytes_256(bytes),
+            FnvSize::L512 => self.hash_bytes_512(bytes),
+            FnvSize::L1024 => self.hash_bytes_1024(bytes),
         }
     }
 
@@ -160,6 +184,8 @@ impl ClassicHasher for Fnv {
 
 #[cfg(test)]
 mod fnvhash_tests {
+    use strum::IntoEnumIterator;
+
     use super::*;
 
     #[test]
@@ -169,13 +195,36 @@ mod fnvhash_tests {
         hasher.input_format = ByteFormat::Utf8;
         hasher.output_format = ByteFormat::Hex;
 
-        hasher.size = FnvSize::P32;
+        hasher.size = FnvSize::L32;
         assert_eq!("e40c292c", hasher.hash_bytes_from_string("a").unwrap());
 
-        hasher.size = FnvSize::P64;
+        hasher.size = FnvSize::L64;
         assert_eq!(
             "af63dc4c8601ec8c",
             hasher.hash_bytes_from_string("a").unwrap()
         );
+
+        for h in FnvSize::iter() {
+            hasher.size = h;
+            let v = hasher.hash_bytes_from_string("a").unwrap();
+            assert!(v.len() != 0)
+        }
     }
+
+    // #[test]
+    // fn test_hex() {
+    //     println!("{:02x?}", P32.to_bytes_be());
+    //     println!("{:02x?}", P64.to_bytes_be());
+    //     println!("{:02x?}", P128.to_bytes_be());
+    //     println!("{:02x?}", P256.to_bytes_be());
+    //     println!("{:02x?}", P512.to_bytes_be());
+    //     println!("{:02x?}", P1024.to_bytes_be());
+
+    //     println!("{:02x?}", O32.to_bytes_be());
+    //     println!("{:02x?}", O64.to_bytes_be());
+    //     println!("{:02x?}", O128.to_bytes_be());
+    //     println!("{:02x?}", O256.to_bytes_be());
+    //     println!("{:02x?}", O512.to_bytes_be());
+    //     println!("{:02x?}", O1024.to_bytes_be());
+    // }
 }
--- src/hasher_panel/fnv_controls.rs
+++ src/hasher_panel/fnv_controls.rs
@@ -1,7 +1,8 @@
 use crate::ui_elements::UiElements;
 
 use super::HasherFrame;
-use hashers::fnv::{Fnv, FnvSize, O128, O32, O64, P128, P32, P64};
+use hashers::fnv::{Fnv, FnvSize};
+use strum::IntoEnumIterator;
 
 pub struct FnvFrame {
     hasher: Fnv,
@@ -15,8 +16,6 @@ impl Default for FnvFrame {
     }
 }
 
-impl FnvFrame {}
-
 impl HasherFrame for FnvFrame {
     fn ui(&mut self, ui: &mut egui::Ui, _errors: &mut String) {
         ui.add_space(16.0);
@@ -27,10 +26,9 @@ impl HasherFrame for FnvFrame {
 
         ui.add_space(16.0);
         ui.horizontal(|ui| {
-            ui.selectable_value(&mut self.hasher.size, FnvSize::P32, "32-bit");
-            ui.selectable_value(&mut self.hasher.size, FnvSize::P64, "64-bit");
-            ui.selectable_value(&mut self.hasher.size, FnvSize::P128, "128-bit");
-            ui.selectable_value(&mut self.hasher.size, FnvSize::P256, "256-bit");
+            for variant in FnvSize::iter() {
+                ui.selectable_value(&mut self.hasher.size, variant, variant.to_string());
+            }
         });
 
         ui.add_space(16.0);
@@ -52,21 +50,17 @@ impl HasherFrame for FnvFrame {
         ui.label("The FNV primes are of a specific form, close to a power of 256, which the developers found to be highly effective at dispersing the bits of the input throughout the state. Four FNV variants are provided here but the original FNV paper also defines constants for 512 and 1024 versions of the algorithm. However these run more slowly, take more space to store, and offer no practical increase in collision resistance.");
         ui.add_space(4.0);
         match self.hasher.size {
-            FnvSize::P32 => ui.mono_strong(format!(
-                "The 32-bit FNV prime is {} and the initialization value is {}",
-                P32, O32,
-            )),
-            FnvSize::P64 => ui.mono_strong(format!(
-                "The 64-bit FNV prime is {} and the initialization value is {}",
-                P64, O64,
-            )),
-            FnvSize::P128 => ui.mono_strong(format!(
-                "The 128-bit FNV prime is {} and the initialization value is {}",
-                P128, O128,
-            )),
-            // The P256 constants don't format nicely so they are hard-coded here.
-            FnvSize::P256 => ui.mono_strong(
-                "The 256-bit FNV prime is 374144419156711147060143317175368453031918731002211 and the initialization value is 100029257958052580907070968620625704837092796014241193945225284501741471925557", 
+            FnvSize::L32 => ui.mono_strong("32-bit FNV prime: 16777619\n Initial Value: 2166136261"),
+            FnvSize::L64 => ui.mono_strong("64-bit FNV prime: 1099511628211\n Initial Value: 14695981039346656037"),
+            FnvSize::L128 => ui.mono_strong("128-bit FNV prime: 309485009821345068724781371\n Initial Value: 144066263297769815596495629667062367629"),
+            FnvSize::L256 => ui.mono_strong(
+                "256-bit FNV prime: 374144419156711147060143317175368453031918731002211\n Initial Value: 100029257958052580907070968620625704837092796014241193945225284501741471925557", 
+            ),
+            FnvSize::L512 => ui.mono_strong(
+                "512-bit FNV prime: 35835915874844867368919076489095108449946327955754392558399825615420669938882575126094039892345713852759\n Initial Value: 9659303129496669498009435400716310466090418745672637896108374329434462657994582932197716438449813051892206539805784495328239340083876191928701583869517785", 
+            ),
+            FnvSize::L1024 => ui.mono_strong(
+                "1024-bit FNV prime: 5016456510113118655434598811035278955030765345404790744303017523831112055108147451509157692220295382716162651878526895249385292291816524375083746691371804094271873160484737966720260389217684476157468082573\n Initial Value: 14197795064947621068722070641403218320880622795441933960878474914617582723252296732303717722150864096521202355549365628174669108571814760471015076148029755969804077320157692458563003215304957150157403644460363550505412711285966361610267868082893823963790439336411086884584107735010676915", 
             ),
         };
 
