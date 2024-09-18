a1f57dd21c5620a838546b0806c5fbc5260e5884
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2024-09-18 19:00:21 -0400

    Update sm4.rs

--- ciphers/src/digital/block_ciphers/sm4.rs
+++ ciphers/src/digital/block_ciphers/sm4.rs
@@ -1,4 +1,4 @@
-use utils::byte_formatting::{fill_u32s_be, ByteFormat};
+use utils::byte_formatting::{fill_u32s_be, u32s_to_bytes_be, ByteFormat};
 
 use super::block_cipher::{BCMode, BCPadding, BlockCipher};
 
@@ -21,6 +21,15 @@ const SM4_SBOX: [u8; 256] = [
     0x18, 0xf0, 0x7d, 0xec, 0x3a, 0xdc, 0x4d, 0x20, 0x79, 0xee, 0x5f, 0x3e, 0xd7, 0xcb, 0x39, 0x48,
 ];
 
+const FAMILY_KEY: [u32; 4] = [0xa3b1bac6, 0x56aa3350, 0x677d9197, 0xb27022dc];
+
+const C: [u32; 32] = [
+    0x00070E15, 0x1C232A31, 0x383F464D, 0x545B6269, 0x70777E85, 0x8C939AA1, 0xA8AFB6BD, 0xC4CBD2D9,
+    0xE0E7EEF5, 0xFC030A11, 0x181F262D, 0x343B4249, 0x50575E65, 0x6C737A81, 0x888F969D, 0xA4ABB2B9,
+    0xC0C7CED5, 0xDCE3EAF1, 0xF8FF060D, 0x141B2229, 0x30373E45, 0x4C535A61, 0x686F767D, 0x848B9299,
+    0xA0A7AEB5, 0xBCC3CAD1, 0xD8DFE6ED, 0xF4FB0209, 0x10171E25, 0x2C333A41, 0x484F565D, 0x646B7279,
+];
+
 fn tau(a: u32) -> u32 {
     u32::from_be_bytes(a.to_be_bytes().map(|b| SM4_SBOX[b as usize]))
 }
@@ -41,8 +50,12 @@ fn t_prime(a: u32) -> u32 {
     lin_prime(tau(a))
 }
 
-fn round(state: &mut [u32; 4], key: u32) {
-    state[0] ^= tau(state[1] ^ state[2] ^ state[3] ^ key)
+fn f(state: &[u32; 4], key: u32) -> u32 {
+    state[0] ^ t(state[1] ^ state[2] ^ state[3] ^ key)
+}
+
+fn f_prime(state: &[u32; 4], key: u32) -> u32 {
+    state[0] ^ t_prime(state[1] ^ state[2] ^ state[3] ^ key)
 }
 
 pub struct Sm4 {
@@ -89,7 +102,18 @@ impl Sm4 {
     }
 
     pub fn ksa(&mut self, bytes: [u8; 32]) {
-        todo!()
+        let mut k = [0; 4];
+        fill_u32s_be(&mut k, &bytes);
+        for i in 0..4 {
+            k[i] ^= FAMILY_KEY[i];
+        }
+
+        for i in 0..32 {
+            let t = f_prime(&k, C[i]);
+            self.subkeys[i] = t;
+            k.rotate_left(1);
+            k[3] = t;
+        }
     }
 
     pub fn with_key(mut self, bytes: [u8; 32]) -> Self {
@@ -102,12 +126,26 @@ impl BlockCipher<16> for Sm4 {
     fn encrypt_block(&self, bytes: &mut [u8]) {
         let mut block = [0; 4];
         fill_u32s_be(&mut block, bytes);
-        todo!()
+
+        for key in self.subkeys {
+            let x = f(&block, key);
+            block.rotate_left(1);
+            block[3] = x;
+        }
+
+        u32s_to_bytes_be(bytes, &block);
     }
 
     fn decrypt_block(&self, bytes: &mut [u8]) {
         let mut block = [0; 4];
         fill_u32s_be(&mut block, bytes);
-        todo!()
+
+        for key in self.subkeys.into_iter().rev() {
+            let x = f(&block, key);
+            block.rotate_left(1);
+            block[3] = x;
+        }
+
+        u32s_to_bytes_be(bytes, &block);
     }
 }
