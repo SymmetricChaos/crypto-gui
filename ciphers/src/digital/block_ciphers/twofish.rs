59ca400c7b7ce66766bc58629bf38c87cecd4a0c
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2024-10-15 16:31:23 -0400

    twofish functions

--- ciphers/src/digital/block_ciphers/twofish.rs
+++ ciphers/src/digital/block_ciphers/twofish.rs
@@ -2,14 +2,68 @@ use utils::byte_formatting::{fill_u32s_le, u32s_to_bytes_le, ByteFormat};
 
 use super::block_cipher::{BCMode, BCPadding, BlockCipher};
 
-fn f() {
-    todo!()
+// 4-bit SBOX for q0 function
+const Q0: [[u8; 16]; 4] = [
+    [0x8, 0x1, 0x7, 0xD, 0x6, 0xF, 0x3, 0x2, 0x0, 0xB, 0x5, 0x9, 0xE, 0xC, 0xA, 0x4],
+    [0xE, 0xC, 0xB, 0x8, 0x1, 0x2, 0x3, 0x5, 0xF, 0x4, 0xA, 0x6, 0x7, 0x0, 0x9, 0xD],
+    [0xB, 0xA, 0x5, 0xE, 0x6, 0xD, 0x9, 0x0, 0xC, 0x8, 0xF, 0x3, 0x2, 0x4, 0x7, 0x1],
+    [0xD, 0x7, 0xF, 0x4, 0x1, 0x2, 0x6, 0xE, 0x9, 0xB, 0x3, 0x0, 0x8, 0x5, 0xC, 0xA],
+];
+
+// 4-bit SBOX for q1 function
+const Q1: [[u8; 16]; 4] = [
+    [0x2, 0x8, 0xB, 0xD, 0xF, 0x7, 0x6, 0xE, 0x3, 0x1, 0x9, 0x4, 0x0, 0xA, 0xC, 0x5],
+    [0x1, 0xE, 0x2, 0xB, 0x4, 0xC, 0x3, 0x7, 0x6, 0xD, 0xA, 0x5, 0xF, 0x9, 0x0, 0x8],
+    [0x4, 0xC, 0x7, 0x5, 0x1, 0x6, 0x9, 0xA, 0x0, 0xE, 0xD, 0x8, 0x2, 0xB, 0x3, 0xF],
+    [0xB, 0x9, 0x5, 0x1, 0xC, 0x3, 0xD, 0xE, 0x6, 0x4, 0x7, 0xF, 0x2, 0x0, 0x8, 0xA],
+];
+
+fn f(a: u32, b: u32, round: usize, subkeys: &[u32;40]) -> (u32,u32) {
+    let t0 = g(a);
+    let t1 = g(b.rotate_left(8));
+    (
+     t0.wrapping_add(t1).wrapping_add(subkeys[2*round+8]),    
+     t0.wrapping_add(t1.wrapping_mul(2)).wrapping_add(subkeys[2*round+9])
+    )
 }
 
 fn g(n: u32) {
     let mut x = n.to_le_bytes();
 
     todo!()
+}
+
+// Each 4-bit nibble is rotated one bit toward the LSB (to the right)
+fn nibble_ror_1(x: u8) -> u8 {
+    (((x) >> 1) & 0x77) | (((x) & 0x11) << 3)
+}
+
+fn q0(n: u8) -> u8{
+    let a0 = (n >> 4) & 15;
+    let b0 = n & 15;
+    let a1 = a0 ^ b0;
+    let b1 = a0 ^ nibble_ror_1(b0) ^ ((8*a0) & 15);
+    let a2 = Q0[0][a1 as usize];
+    let b2 = Q0[1][b1 as usize];
+    let a3 = a2 ^ b2;
+    let b3 = a2 ^ nibble_ror_1(b2) ^ ((8*a2) & 15);
+    let a4 = Q0[2][a3 as usize];
+    let b4 = Q0[3][b3 as usize];
+    (b4 << 4) | a4
+}
+
+fn q1() {
+    let a0 = (n >> 4) & 15;
+    let b0 = n & 15;
+    let a1 = a0 ^ b0;
+    let b1 = a0 ^ nibble_ror_1(b0) ^ ((8*a0) & 15);
+    let a2 = Q1[0][a1 as usize];
+    let b2 = Q1[1][b1 as usize];
+    let a3 = a2 ^ b2;
+    let b3 = a2 ^ nibble_ror_1(b2) ^ ((8*a2) & 15);
+    let a4 = Q1[2][a3 as usize];
+    let b4 = Q1[3][b3 as usize];
+    (b4 << 4) | a4
 }
 
 pub struct TwoFish128 {
@@ -39,17 +93,31 @@ impl Default for TwoFish128 {
 crate::block_cipher_builders! {TwoFish128, u128}
 
 impl TwoFish128 {
+    const K: u32 = 2; // Keylength in bits divided by 64
+
     pub fn sbox(&self, n: u32, i: usize) -> u32 {
         self.sboxes[i][n as usize]
     }
+    
+    pub fn ksa(&mut self, bytes: [u8; 16]) {
+        let mut words = [0_u32; 4];
+        fill_u32s_le(&mut word, bytes);
+    }
 
-    pub fn ksa_u32(&mut self, key: [u32; 8]) {}
+    pub fn ksa_u32(&mut self, key: [u32; 4]) {}
 
-    pub fn ksa(&mut self, bytes: [u8; 32]) {}
-
-    pub fn with_key(mut self, bytes: [u8; 32]) -> Self {
+    pub fn with_key(mut self, bytes: [u8; 16]) -> Self {
         self.ksa(bytes);
         self
+    }
+
+    pub fn with_key_u32(mut self, bytes: [u32; 4]) -> Self {
+        self.ksa(bytes);
+        self
+    }
+
+    fn h(x: u32, list: [u32; Self:K]) -> u32 {
+        todo!()
     }
 }
 
