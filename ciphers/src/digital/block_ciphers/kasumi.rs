2443b47de98d7e606598d48b61054883b6e4d855
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2024-10-22 14:56:29 -0400

    kasumi and three fish outlines

--- ciphers/src/digital/block_ciphers/kasumi.rs
+++ ciphers/src/digital/block_ciphers/kasumi.rs
@@ -1,1 +1,24 @@
+use utils::byte_formatting::ByteFormat;
+use super::block_cipher::{BCMode, BCPadding};
 
+pub struct Kasumi {
+    pub input_format: ByteFormat,
+    pub output_format: ByteFormat,
+    pub iv: u64,
+    pub mode: BCMode,
+    pub padding: BCPadding,
+}
+
+impl Default for Kasumi {
+    fn default() -> Self {
+        Self {
+            input_format: ByteFormat::Hex,
+            output_format: ByteFormat::Hex,
+            iv: 0,
+            mode: Default::default(),
+            padding: Default::default(),
+        }
+    }
+}
+
+crate::block_cipher_builders! {Kasumi, u64}
--- ciphers/src/digital/block_ciphers/threefish.rs
+++ ciphers/src/digital/block_ciphers/threefish.rs
@@ -1,1 +1,38 @@
+// use crypto_bigint::U256;
+// use utils::byte_formatting::ByteFormat;
+// use super::block_cipher::{BCMode, BCPadding, BlockCipher};
 
+// pub struct Threefish256 {
+//     pub input_format: ByteFormat,
+//     pub output_format: ByteFormat,
+//     pub iv: U256,
+//     pub mode: BCMode,
+//     pub padding: BCPadding,
+// }
+
+// impl Default for Threefish256 {
+//     fn default() -> Self {
+//         Self {
+//             input_format: ByteFormat::Hex,
+//             output_format: ByteFormat::Hex,
+//             iv: U256::ZERO,
+//             mode: Default::default(),
+//             padding: Default::default(),
+//         }
+//     }
+// }
+
+// crate::block_cipher_builders! {Threefish256, U256}
+
+
+// impl BlockCipher<32> for Threefish256 {
+//     fn encrypt_block(&self, bytes: &mut [u8]) {
+
+//     }
+
+//     fn decrypt_block(&self, bytes: &mut [u8]) {
+
+//     }
+// }
+
+// crate::impl_cipher_for_block_cipher!(Threefish256, 32);
