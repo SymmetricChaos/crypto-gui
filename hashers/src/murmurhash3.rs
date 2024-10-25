32b103d79122428ba212548755bdd44ff633c684
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2024-10-25 07:55:30 -0400

    Update murmurhash3.rs

--- hashers/src/murmurhash3.rs
+++ hashers/src/murmurhash3.rs
@@ -1,5 +1,5 @@
 use crate::traits::ClassicHasher;
-use utils::byte_formatting::ByteFormat;
+use utils::byte_formatting::{make_u64s_le, ByteFormat};
 
 fn final_mix(mut x: u32) -> u32 {
     x ^= x >> 16;
@@ -81,6 +81,71 @@ impl ClassicHasher for Murmur3_32 {
         state ^= bytes.len() as u32;
 
         state = final_mix(state);
+        state.to_be_bytes().to_vec()
+    }
+
+    crate::hash_bytes_from_string! {}
+}
+
+pub struct Murmur3_128 {
+    pub input_format: ByteFormat,
+    pub output_format: ByteFormat,
+    pub seed: u128,
+}
+
+impl Default for Murmur3_128 {
+    fn default() -> Self {
+        Self {
+            input_format: ByteFormat::Utf8,
+            output_format: ByteFormat::Hex,
+            seed: 0,
+        }
+    }
+}
+
+impl Murmur3_128 {
+    pub fn input(mut self, input: ByteFormat) -> Self {
+        self.input_format = input;
+        self
+    }
+
+    pub fn output(mut self, output: ByteFormat) -> Self {
+        self.output_format = output;
+        self
+    }
+
+    pub fn with_seed(mut self, seed: u128) -> Self {
+        self.seed = seed;
+        self
+    }
+}
+
+impl ClassicHasher for Murmur3_128 {
+    fn hash(&self, bytes: &[u8]) -> Vec<u8> {
+        let mut state = self.seed;
+
+        // Divide the input into 32-bit chunks and save the remainder
+        let chunks = bytes.chunks_exact(4);
+        let rem = chunks.remainder();
+        // For each full chunk mix it into the state and then mix the state
+        for block in chunks {
+            let k = make_u64s_le::<2>(block);
+            state ^= bmix(k);
+            state = state.rotate_left(13);
+            state = state.wrapping_mul(5).wrapping_add(0xe6546b64);
+        }
+        // Load any bytes in the remainder and mix them
+        let mut final_block = 0_u32;
+        for byte in rem {
+            final_block = final_block << 8;
+            final_block |= *byte as u32;
+        }
+        state ^= bmix(final_block);
+
+        // XOR in the length in bytes
+        state ^= bytes.len() as u32;
+        // Final mix
+        state = fmix(state);
         state.to_be_bytes().to_vec()
     }
 
