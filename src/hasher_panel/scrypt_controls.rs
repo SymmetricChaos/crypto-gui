0ac75ea8a7181ae1047e19f733b953e6b4d9dbf3
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2024-11-05 17:19:48 -0500

    Update scrypt_controls.rs

--- src/hasher_panel/scrypt_controls.rs
+++ src/hasher_panel/scrypt_controls.rs
@@ -3,11 +3,15 @@ use crate::ui_elements::UiElements;
 use super::HasherFrame;
 use egui::DragValue;
 use hashers::scrypt::Scrypt;
+use strum::IntoEnumIterator;
+use utils::byte_formatting::ByteFormat;
 
 pub struct ScryptFrame {
     hasher: Scrypt,
     salt: Vec<u8>,
     salt_string: String,
+    salt_format: ByteFormat,
+    salt_valid: bool,
 }
 
 impl Default for ScryptFrame {
@@ -16,14 +20,26 @@ impl Default for ScryptFrame {
             hasher: Default::default(),
             salt: Vec::new(),
             salt_string: String::new(),
+            salt_format: ByteFormat::Utf8,
+            salt_valid: true
         }
     }
 }
 
-impl ScryptFrame {}
+impl ScryptFrame {
+    fn set_salt(&mut self) {
+        if let Ok(salt) = self.salt_format.text_to_bytes(&self.salt_string) {
+            self.salt = salt;
+            self.salt_valid = true;
+        } else {
+            self.salt.clear();
+            self.salt_valid = false;
+        };
+    }
+}
 
 impl HasherFrame for ScryptFrame {
-    fn ui(&mut self, ui: &mut egui::Ui, errors: &mut String) {
+    fn ui(&mut self, ui: &mut egui::Ui, _errors: &mut String) {
         ui.hyperlink_to(
             "see the code",
             "https://github.com/SymmetricChaos/crypto-gui/blob/master/hashers/src/scrypt",
@@ -37,14 +53,31 @@ impl HasherFrame for ScryptFrame {
 
         ui.subheading("Cost Factor");
         ui.add(DragValue::new(&mut self.hasher.cost));
+        ui.add_space(4.0);
         ui.subheading("Block Size Factor");
         ui.add(DragValue::new(&mut self.hasher.blocksize_factor));
+        ui.add_space(4.0);
         ui.subheading("Parallelism");
         ui.add(DragValue::new(&mut self.hasher.parallelism));
+        ui.add_space(4.0);
         ui.subheading("Output Length");
         ui.add(DragValue::new(&mut self.hasher.key_len));
 
-        todo!()
+        ui.add_space(8.0);
+
+        ui.horizontal(|ui| {
+            for variant in ByteFormat::iter() {
+                if ui.selectable_value(&mut self.salt_format, variant, variant.to_string()).changed() {
+                    self.set_salt();
+                }
+            }
+        });
+        if ui.control_string(&mut self.salt_string).changed() {
+            self.set_salt();
+        }
+        if !self.salt_valid {
+            ui.error_text("SALT INVALID, CURRENTLY SET TO EMPTY");
+        }
     }
 
     crate::hash_string! {}
