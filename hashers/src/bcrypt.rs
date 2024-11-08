f71165d498b5609431ca4394336a6052b2e37faa
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2024-11-08 14:43:00 -0500

    moved and made some fixes to bcrypt

--- hashers/src/bcrypt.rs
+++ hashers/src/bcrypt.rs
@@ -1,5 +1,6 @@
 use crate::{
     auxiliary::blowfish_arrays::{PARRAY, SBOXES},
+    errors::HasherError,
     traits::ClassicHasher,
 };
 use utils::byte_formatting::{make_u32s_be, ByteFormat};
@@ -72,29 +73,34 @@ pub fn expand_key(
         *word ^= k;
     }
 
+    // Salt is created in be order
     let salt_halves = [
-        u32::from_le_bytes(salt[0..4].try_into().unwrap()),
-        u32::from_le_bytes(salt[4..8].try_into().unwrap()),
-        u32::from_le_bytes(salt[8..12].try_into().unwrap()),
-        u32::from_le_bytes(salt[12..16].try_into().unwrap()),
+        [
+            u32::from_be_bytes(salt[0..4].try_into().unwrap()),
+            u32::from_be_bytes(salt[4..8].try_into().unwrap()),
+        ],
+        [
+            u32::from_be_bytes(salt[8..12].try_into().unwrap()),
+            u32::from_be_bytes(salt[12..16].try_into().unwrap()),
+        ],
     ];
 
     let mut block: [u32; 2] = [0, 0];
 
     // Create the parray
     for i in 0..9 {
-        block[0] ^= salt_halves[2 * (i % 2)];
-        block[1] ^= salt_halves[2 * (i % 2) + 1];
+        block[0] ^= salt_halves[i % 2][0];
+        block[1] ^= salt_halves[i % 2][1];
         encrypt_u32s(&mut block, parray, sboxes);
         parray[2 * i] = block[0];
         parray[2 * i + 1] = block[1];
     }
 
-    // Create the sboxes
+    // Create the four sboxes
     for i in 0..4 {
         for j in 0..128 {
-            block[0] ^= salt_halves[2 * (i % 2)];
-            block[1] ^= salt_halves[2 * (i % 2) + 1];
+            block[0] ^= salt_halves[j % 2][0];
+            block[1] ^= salt_halves[j % 2][1];
             encrypt_u32s(&mut block, parray, sboxes);
             sboxes[i][j * 2] = block[0];
             sboxes[i][j * 2 + 1] = block[1];
@@ -122,47 +128,90 @@ impl Default for Bcrypt {
     }
 }
 
+impl Bcrypt {
+    pub fn input(mut self, input: ByteFormat) -> Self {
+        self.input_format = input;
+        self
+    }
+
+    pub fn output(mut self, output: ByteFormat) -> Self {
+        self.output_format = output;
+        self
+    }
+
+    pub fn salt(mut self, salt: [u8; 16]) -> Self {
+        self.salt = salt;
+        self
+    }
+
+    pub fn cost(mut self, cost: usize) -> Self {
+        self.cost = cost;
+        self
+    }
+
+    pub fn full_len(mut self, full_len: bool) -> Self {
+        self.full_len = full_len;
+        self
+    }
+
+    pub fn direct(password: &[u8], salt: [u8; 16], cost: usize) -> Result<Vec<u8>, HasherError> {
+        if password.is_empty() || password.len() > 72 {
+            return Err(HasherError::general(
+                "password cannot be empty or be greater than 72 bytes",
+            ));
+        }
+        if cost >= 31 {
+            return Err(HasherError::general(
+                "cost cannot be less than 4 or greater than 31",
+            ));
+        }
+        Ok(Bcrypt::default().salt(salt).cost(cost).hash(password))
+    }
+}
+
 impl ClassicHasher for Bcrypt {
     fn hash(&self, bytes: &[u8]) -> Vec<u8> {
-        assert!(self.cost < 32);
+        assert!(self.cost >= 4 && self.cost < 32);
         assert!(bytes.len() > 0 && bytes.len() <= 72);
 
         let mut parray = PARRAY;
         let mut sboxes = SBOXES;
-        eks_blowfish_setup(
-            &bytes[0..72], // truncate long inputs
-            self.salt,
-            &mut parray,
-            &mut sboxes,
-            self.cost,
-        );
+        eks_blowfish_setup(bytes, self.salt, &mut parray, &mut sboxes, self.cost);
 
         // The string "OrpheanBeholderScryDoubt" as u32s
         let mut ctext: [u32; 6] = [
             0x4f727068, 0x65616e42, 0x65686f6c, 0x64657253, 0x63727944, 0x6f756274,
         ];
 
-        // Official implementations discard the last byte
         let mut out = [0; 24];
 
         for i in 0..3 {
             // Each 8 byte block is encrypted 64 times then copied into the output
+            // As if encrypting the bytes in ECB mode
             let a = 2 * i;
-            let b = 2 * i + 1;
             for _ in 0..64 {
-                encrypt_u32s(&mut ctext[a..b], &parray, &sboxes);
+                encrypt_u32s(&mut ctext[a..a + 2], &parray, &sboxes);
             }
 
             out[4 * a..][..4].copy_from_slice(&ctext[a].to_be_bytes());
-            out[4 * b..][..4].copy_from_slice(&ctext[b].to_be_bytes());
+            out[4 * (a + 1)..][..4].copy_from_slice(&ctext[a + 1].to_be_bytes());
         }
 
         if self.full_len {
             out.to_vec()
         } else {
+            // Official implementations discard the last byte for some reason
             out[0..23].to_vec()
         }
     }
 
     crate::hash_bytes_from_string! {}
 }
+
+crate::basic_hash_tests!(
+    test1, Bcrypt::default().salt([
+        0x14, 0x4b, 0x3d, 0x69, 0x1a, 0x7b, 0x4e, 0xcf, 0x39, 0xcf, 0x73, 0x5c, 0x7f, 0xa7,
+        0xa7, 0x9c,
+    ]).cost(6), "\0",
+    "557e94f34bf286e8719a26be94ac1e16d95ef9f819dee0";
+);
--- hashers/src/bcrypt/mod.rs
+++
@@ -1,1 +0,0 @@
-pub mod bcrypt_hasher;
