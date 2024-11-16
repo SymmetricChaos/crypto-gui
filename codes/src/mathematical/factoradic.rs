cb53cb04df06b3ae5c0b352466e6569ef05284d4
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2024-11-16 16:34:20 -0500

    factoradic revision

--- codes/src/mathematical/factoradic.rs
+++ codes/src/mathematical/factoradic.rs
@@ -3,153 +3,97 @@ use lazy_static::lazy_static;
 use num::Integer;
 use regex::Regex;
 
-use crate::{
-    errors::CodeError,
-    letter_word_code::{IOMode, IntegerCodeMaps},
-    traits::Code,
-};
+use crate::{errors::CodeError, traits::Code};
 
 lazy_static! {
     pub static ref TUPLE: Regex = Regex::new(r"(([0-9]+:)*[0-9]+)").unwrap();
 }
 
-pub struct Factoradic {
-    pub maps: IntegerCodeMaps,
-    pub mode: IOMode,
+pub fn encode_u64(n: u64) -> String {
+    if n == 0 {
+        return String::from("0");
+    }
+    let mut out = Vec::new();
+    let mut divisor = 1;
+    let mut n = n;
+    while n != 0 {
+        let (q, r) = n.div_rem(&divisor);
+        out.push(r);
+        n = q;
+        divisor += 1;
+    }
+    format!("{}", out.iter().rev().join(":"))
 }
+
+pub fn decode_u64(s: &str) -> Option<u64> {
+    let mut base = 1;
+    let mut ctr = 1;
+    let mut value = 0;
+    for n in s.rsplit(":").filter(|s| !s.is_empty()) {
+        let x = u64::from_str_radix(n.trim(), 10)
+            .expect("captures by the regex should always be valid numbers");
+        if x >= ctr {
+            return None;
+        }
+        value += x * base;
+        base *= ctr;
+        ctr += 1;
+    }
+    Some(value)
+}
+
+pub fn extract_code_groups(text: &str) -> Vec<Option<u64>> {
+    let mut output = Vec::new();
+
+    for cap in TUPLE.captures_iter(text) {
+        let s = match cap.get(1) {
+            Some(m) => m.as_str(),
+            None => {
+                output.push(None);
+                continue;
+            }
+        };
+        output.push(decode_u64(s));
+    }
+
+    output
+}
+
+pub struct Factoradic {}
 
 impl Default for Factoradic {
     fn default() -> Self {
-        let mut maps = IntegerCodeMaps::new();
-        maps.alphabet = String::from("ETAOINSHRDLCUMWFGYPBVKJXQZ");
-        Factoradic {
-            maps,
-            mode: IOMode::Integer,
-        }
+        Factoradic {}
     }
 }
 
-impl Factoradic {
-    pub fn encode_usize(&self, n: usize) -> String {
-        if n == 0 {
-            return String::from("0");
-        }
-        let mut out = Vec::new();
-        let mut divisor = 1;
-        let mut n = n;
-        while n != 0 {
-            let (q, r) = n.div_rem(&divisor);
-            out.push(r);
-            n = q;
-            divisor += 1;
-        }
-        format!("{}", out.iter().rev().join(":"))
-    }
-
-    fn code_string_to_num(s: &str) -> Option<usize> {
-        let mut base = 1;
-        let mut ctr = 1;
-        let mut value = 0;
-        for n in s.rsplit(":").filter(|s| !s.is_empty()) {
-            let x = usize::from_str_radix(n.trim(), 10)
-                .expect("captures by the regex should always be valid numbers");
-            if x >= ctr {
-                return None;
-            }
-            value += x * base;
-            base *= ctr;
-            ctr += 1;
-        }
-        Some(value)
-    }
-
-    pub fn recognize_code(text: &str) -> Vec<Option<usize>> {
-        let mut output = Vec::new();
-
-        for cap in TUPLE.captures_iter(text) {
-            let s = match cap.get(1) {
-                Some(m) => m.as_str(),
-                None => {
-                    output.push(None);
-                    continue;
-                }
-            };
-            output.push(Self::code_string_to_num(s));
-        }
-
-        output
-    }
-}
+impl Factoradic {}
 
 impl Code for Factoradic {
     fn encode(&self, text: &str) -> Result<String, CodeError> {
-        let mut output = String::new();
+        let mut v = Vec::new();
 
-        if self.mode == IOMode::Letter {
-            for c in text.chars() {
-                let n = self.maps.char_to_int(c)?;
-                output.push_str(&self.encode_usize(n));
-                output.push(' ');
-            }
-        } else if self.mode == IOMode::Word {
-            for w in text.split(" ") {
-                let n = self.maps.word_to_int(w)?;
-                output.push_str(&self.encode_usize(n));
-                output.push(' ');
-            }
-        } else {
-            for w in text.split(" ") {
-                let n =
-                    usize::from_str_radix(w, 10).map_err(|e| CodeError::Input(e.to_string()))?;
-                output.push_str(&self.encode_usize(n));
-                output.push(' ');
-            }
+        for w in text.split(",") {
+            let n =
+                u64::from_str_radix(w.trim(), 10).map_err(|e| CodeError::Input(e.to_string()))?;
+            v.push(encode_u64(n));
         }
-        output.pop();
-        Ok(output)
+
+        Ok(v.join(", "))
     }
 
     fn decode(&self, text: &str) -> Result<String, CodeError> {
-        let mut output = String::new();
+        let mut v = Vec::new();
 
-        if self.mode == IOMode::Letter {
-            for section in Self::recognize_code(&text) {
-                if let Some(code) = section {
-                    if let Ok(c) = self.maps.int_to_char(code) {
-                        output.push(c);
-                    } else {
-                        output.push('�');
-                    }
-                } else {
-                    output.push('�');
-                }
-            }
-        } else if self.mode == IOMode::Word {
-            for section in Self::recognize_code(&text) {
-                if let Some(code) = section {
-                    if let Ok(s) = self.maps.int_to_word(code) {
-                        output.push_str(s);
-                        output.push(' ');
-                    } else {
-                        output.push_str("� ");
-                    }
-                } else {
-                    output.push_str("� ");
-                }
-            }
-            output.pop();
-        } else {
-            for section in Self::recognize_code(&text) {
-                if let Some(code) = section {
-                    output.push_str(&code.to_string());
-                    output.push(' ');
-                } else {
-                    output.push_str("� ");
-                }
+        for group in extract_code_groups(text) {
+            if let Some(n) = group {
+                v.push(n.to_string());
+            } else {
+                v.push(String::from("INVALID"));
             }
         }
 
-        Ok(output)
+        Ok(v.join(", "))
     }
 }
 
@@ -157,8 +101,8 @@ impl Code for Factoradic {
 mod factoradic_tests {
     use super::*;
 
-    const PLAINTEXT: &'static str = "ETAOIN";
-    const ENCODEDTEXT: &'static str = "0 1:0 1:0:0 1:1:0 2:0:0 2:1:0";
+    const PLAINTEXT: &'static str = "0, 1, 2, 3, 4, 5";
+    const ENCODEDTEXT: &'static str = "0, 1:0, 1:0:0, 1:1:0, 2:0:0, 2:1:0";
 
     #[test]
     #[ignore]
--- src/code_panel/factoradic_controls.rs
+++ src/code_panel/factoradic_controls.rs
@@ -1,18 +1,15 @@
 use super::CodeFrame;
-use crate::ui_elements::{integer_letter_code_controls, integer_word_code_controls, UiElements};
-use codes::{letter_word_code::IOMode, mathematical::factoradic::Factoradic};
-use strum::IntoEnumIterator;
+use crate::ui_elements::UiElements;
+use codes::mathematical::factoradic::{encode_u64, Factoradic};
 
 pub struct FactoradicFrame {
     code: Factoradic,
-    words_string: String,
 }
 
 impl Default for FactoradicFrame {
     fn default() -> Self {
         Self {
             code: Default::default(),
-            words_string: String::new(),
         }
     }
 }
@@ -25,56 +22,20 @@ impl CodeFrame for FactoradicFrame {
         );
         ui.add_space(8.0);
 
-        ui.subheading("Example");
+        ui.subheading("Example Calculation");
         ui.mono(
             "factoradic number     3:4:2:2:1:0\n\ndigits                3   4  2  2  1  0\nplace values       ×120 ×24 ×6 ×2 ×1 ×1\ndigit values        360  96 12  4  1  0\n\ntheir sum is 473",
         );
 
-        ui.group(|ui| {
-            ui.subheading("Mode");
-            for variant in IOMode::iter() {
-                ui.selectable_value(&mut self.code.mode, variant, variant.to_string());
-            }
-        });
-        ui.add_space(8.0);
+        ui.add_space(16.0);
 
-        match self.code.mode {
-            IOMode::Letter => {
-                integer_letter_code_controls(ui, &mut self.code.maps.alphabet);
-                ui.two_column_table(
-                    "Character",
-                    "Code",
-                    Box::new(
-                        self.code
-                            .maps
-                            .ints_chars()
-                            .map(|(a, b)| (b, self.code.encode_usize(a))),
-                    ),
-                );
-            }
-            IOMode::Word => {
-                integer_word_code_controls(ui, &mut self.words_string, &mut self.code.maps);
-                ui.two_column_table(
-                    "Word",
-                    "Code",
-                    Box::new(
-                        self.code
-                            .maps
-                            .ints_words()
-                            .map(|(a, b)| (b, self.code.encode_usize(a))),
-                    ),
-                );
-            }
-            IOMode::Integer => {
-                ui.label("Convert between numbers and their factoradic encodings. When decoding the '�' symbol appears when an invalid code is encoutered.");
-                ui.add_space(16.0);
-                ui.two_column_table(
-                    "Integer",
-                    "Code",
-                    Box::new((0..10).into_iter().map(|n| (n, self.code.encode_usize(n)))),
-                );
-            }
-        }
+        ui.subheading("Small Example Values");
+        ui.two_column_table(
+            "Integer",
+            "Code",
+            Box::new((0..10).into_iter().map(|n| (n, encode_u64(n)))),
+        );
+
         ui.add_space(16.0);
     }
 
