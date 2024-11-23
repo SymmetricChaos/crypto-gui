64a4cd7acbe4a63177a86c791fec34cc48baea6b
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2024-11-23 10:19:08 -0500

    full dft

--- codes/src/other/fourier.rs
+++ codes/src/other/fourier.rs
@@ -1,8 +1,8 @@
-use itertools::Itertools;
-use num::{complex::Complex64, Complex, FromPrimitive, Zero};
-
-use crate::traits::Code;
+use crate::{errors::CodeError, traits::Code};
 use core::f64;
+use itertools::Itertools;
+use nalgebra::ComplexField;
+use num::{complex::Complex64, Complex, FromPrimitive, Num, Zero};
 
 fn dft(v: &Vec<Complex<f64>>) -> Vec<Complex<f64>> {
     let tau = Complex64::from_f64(f64::consts::TAU).unwrap();
@@ -21,21 +21,69 @@ fn dft(v: &Vec<Complex<f64>>) -> Vec<Complex<f64>> {
     out
 }
 
-pub struct Fourier {}
+fn dft_inv(v: &Vec<Complex<f64>>) -> Vec<Complex<f64>> {
+    let tau = Complex64::from_f64(f64::consts::TAU).unwrap();
+    let mut out = Vec::with_capacity(v.len());
+    let l = v.len();
+    let lc = Complex64::from_usize(l).unwrap();
+    let idxs = (0..l)
+        .map(|i| Complex64::from_usize(i).unwrap())
+        .collect_vec();
+    for k in 0..l {
+        out.push(Complex64::zero());
+        for n in 0..l {
+            out[k] += v[n] * (tau * Complex64::i() * idxs[n] * (idxs[k] / lc)).exp()
+        }
+        out[k] = out[k] / lc;
+    }
+    out
+}
+
+pub struct Fourier {
+    rounded: bool,
+}
 
 impl Default for Fourier {
     fn default() -> Self {
-        Self {}
+        Self { rounded: true }
     }
 }
 
 impl Code for Fourier {
     fn encode(&self, text: &str) -> Result<String, crate::errors::CodeError> {
-        todo!()
+        let mut v = Vec::new();
+        for s in text.split(",") {
+            v.push(
+                Complex64::from_str_radix(s.trim(), 10)
+                    .or_else(|e| Err(CodeError::Input(e.to_string())))?,
+            );
+        }
+        if self.rounded {
+            Ok(dft(&v)
+                .into_iter()
+                .map(|z| z.round().to_string())
+                .join(", "))
+        } else {
+            Ok(dft(&v).into_iter().map(|z| z.to_string()).join(", "))
+        }
     }
 
     fn decode(&self, text: &str) -> Result<String, crate::errors::CodeError> {
-        todo!()
+        let mut v = Vec::new();
+        for s in text.split(",") {
+            v.push(
+                Complex64::from_str_radix(s.trim(), 10)
+                    .or_else(|e| Err(CodeError::Input(e.to_string())))?,
+            );
+        }
+        if self.rounded {
+            Ok(dft_inv(&v)
+                .into_iter()
+                .map(|z| z.round().to_string())
+                .join(", "))
+        } else {
+            Ok(dft_inv(&v).into_iter().map(|z| z.to_string()).join(", "))
+        }
     }
 }
 
@@ -56,8 +104,26 @@ mod fourier_tests {
             .map(|s| Complex64::from_str_radix(s, 10).unwrap())
             .to_vec();
 
+        // Do note the rounding
         let ft: Vec<Complex<f64>> = dft(&v0).into_iter().map(|z| z.round()).collect_vec();
 
         assert_eq!(v1, ft);
+    }
+
+    // Input and output are written according to the simplest formatting rules
+    // For this test we write the third element of the input as -0-1i because when decoding the zero has a negative sign
+    const PLAINTEXT: &'static str = "1+0i, 2-1i, -0-1i, -1+2i";
+    const CODETEXT: &'static str = "2+0i, -2-2i, 0-2i, 4+4i";
+
+    #[test]
+    fn encode_test() {
+        let code = Fourier::default();
+        assert_eq!(CODETEXT, code.encode(PLAINTEXT).unwrap());
+    }
+
+    #[test]
+    fn decode_test() {
+        let code = Fourier::default();
+        assert_eq!(PLAINTEXT, code.decode(CODETEXT).unwrap());
     }
 }
