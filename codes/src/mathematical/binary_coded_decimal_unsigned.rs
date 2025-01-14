8c8af4ad125a1a5d943a2de5d367f52888b8c76c
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2025-01-14 18:01:16 -0500

    unsigned bcd and some macros

--- codes/src/mathematical/binary_coded_decimal_unsigned.rs
+++ codes/src/mathematical/binary_coded_decimal_unsigned.rs
@@ -4,14 +4,9 @@ use paste::paste;
 use utils::byte_formatting::ByteFormat;
 
 const ARR: [u8; 10] = [0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9];
-const POS: u8 = 0xC;
-const NEG: u8 = 0xD;
-const R32MIN: i32 = -9999999;
-const R32MAX: i32 = 9999999;
-const R64MIN: i64 = -999999999999999;
-const R64MAX: i64 = 999999999999999;
-const R128MIN: i128 = -9999999999999999999999999999999;
-const R128MAX: i128 = 9999999999999999999999999999999;
+const R32MAX: u32 = 99999999;
+const R64MAX: u64 = 9999999999999999;
+const R128MAX: u128 = 99999999999999999999999999999999;
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum WordWidth {
@@ -20,49 +15,35 @@ pub enum WordWidth {
     W128,
 }
 
-macro_rules! encode_decode_signed {
-    ($t: ty, $u: ty, $bytes: literal, $min: ident, $max: ident) => {
+macro_rules! encode_decode_unsigned {
+    ($t: ty, $bytes: literal, $max: ident) => {
         paste! {
-            pub fn [<encode_ $t>](&self, values: &[$t]) -> Result<Vec<$u>, CodeError> {
+            pub fn [<encode_ $t>](&self, values: &[$t]) -> Result<Vec<$t>, CodeError> {
                 let mut out = Vec::with_capacity(values.len());
                 for &value in values {
-                    if value < $min || value > $max {
+                    if value > $max {
                         return Err(CodeError::Input(
-                            format!("the allowed range is {}..={}", $min, $max)
+                            format!("the allowed range is 0..={}",  $max)
                         ));
                     }
-                    let t = value.abs() as $u;
-                    let mut word = 0 as $u;
-                    let ten = 10 as $u;
-                    for i in (0..($bytes-1)).rev() {
-                        word |= (t / (ten.pow(i))) % 10;
+
+                    let t = value;
+                    let mut word = 0 as $t;
+                    let ten = 10 as $t;
+                    for i in (0..$bytes).rev() {
                         word <<= 4;
-                    }
-                    if value.is_negative() {
-                        word |= NEG as $u;
-                    } else {
-                        word |= POS as $u;
+                        word |= (t / (ten.pow(i))) % 10;
                     }
                     out.push(word);
                 }
                 Ok(out)
             }
 
-            pub fn [<decode_ $u>](&self, values: &[$u]) -> Result<Vec<$t>, CodeError> {
+            pub fn [<decode_ $t>](&self, values: &[$t]) -> Result<Vec<$t>, CodeError> {
                 let mut out = Vec::with_capacity(values.len());
                 for value in values {
-                    let negative = if value & 0xF == NEG as $u {
-                        true
-                    } else if value & 0xF == POS as $u {
-                        false
-                    } else {
-                        return Err(CodeError::Input(format!(
-                            "invalid sign nibble in value {:08x?}, only 0xC and 0xD are allowed",
-                            value
-                        )));
-                    };
                     let mut n = 0;
-                    for i in (1..$bytes).rev() {
+                    for i in (0..$bytes).rev() {
                         let nibble = ((value >> (4 * i)) & 0xF) as u8;
                         if ARR.contains(&nibble) {
                             n *= 10;
@@ -74,9 +55,6 @@ macro_rules! encode_decode_signed {
                             )));
                         }
                     }
-                    if negative {
-                        n *= -1;
-                    }
                     out.push(n);
                 }
 
@@ -86,12 +64,12 @@ macro_rules! encode_decode_signed {
     };
 }
 
-pub struct BinaryCodedDecimal {
+pub struct BinaryCodedDecimalUnsigned {
     pub formatting: ByteFormat,
     pub width: WordWidth,
 }
 
-impl Default for BinaryCodedDecimal {
+impl Default for BinaryCodedDecimalUnsigned {
     fn default() -> Self {
         Self {
             formatting: ByteFormat::Hex,
@@ -100,47 +78,47 @@ impl Default for BinaryCodedDecimal {
     }
 }
 
-impl BinaryCodedDecimal {
-    encode_decode_signed!(i32, u32, 8, R32MIN, R32MAX);
-    encode_decode_signed!(i64, u64, 16, R64MIN, R64MAX);
-    encode_decode_signed!(i128, u128, 32, R128MIN, R128MAX);
+impl BinaryCodedDecimalUnsigned {
+    encode_decode_unsigned!(u32, 8, R32MAX);
+    encode_decode_unsigned!(u64, 16, R64MAX);
+    encode_decode_unsigned!(u128, 32, R128MAX);
 }
 
-impl Code for BinaryCodedDecimal {
+impl Code for BinaryCodedDecimalUnsigned {
     fn encode(&self, text: &str) -> Result<String, crate::errors::CodeError> {
         match self.width {
             WordWidth::W32 => {
                 let mut values = Vec::new();
                 for value in text.split(",").map(|s| s.trim()) {
-                    let n = i32::from_str_radix(value.trim(), 10)
+                    let n = u32::from_str_radix(value.trim(), 10)
                         .map_err(|e| CodeError::Input(e.to_string()))?;
                     values.push(n);
                 }
                 Ok(self
                     .formatting
-                    .u32_slice_to_text_be(&self.encode_i32(&values)?))
+                    .u32_slice_to_text_be(&self.encode_u32(&values)?))
             }
             WordWidth::W64 => {
                 let mut values = Vec::new();
                 for value in text.split(",").map(|s| s.trim()) {
-                    let n = i64::from_str_radix(value.trim(), 10)
+                    let n = u64::from_str_radix(value.trim(), 10)
                         .map_err(|e| CodeError::Input(e.to_string()))?;
                     values.push(n);
                 }
                 Ok(self
                     .formatting
-                    .u64_slice_to_text_be(&self.encode_i64(&values)?))
+                    .u64_slice_to_text_be(&self.encode_u64(&values)?))
             }
             WordWidth::W128 => {
                 let mut values = Vec::new();
                 for value in text.split(",").map(|s| s.trim()) {
-                    let n = i128::from_str_radix(value.trim(), 10)
+                    let n = u128::from_str_radix(value.trim(), 10)
                         .map_err(|e| CodeError::Input(e.to_string()))?;
                     values.push(n);
                 }
                 Ok(self
                     .formatting
-                    .u128_slice_to_text_be(&self.encode_i128(&values)?))
+                    .u128_slice_to_text_be(&self.encode_u128(&values)?))
             }
         }
     }
@@ -188,70 +166,59 @@ impl Code for BinaryCodedDecimal {
 mod bcd_tests {
     use super::*;
 
-    const PLAINTEXT32: &'static str = "12345, -9876543";
-    const ENCODEDTEXT32: &'static str = "0012345c9876543d";
+    const PLAINTEXT32: &'static str = "12345, 9876543";
+    const ENCODEDTEXT32: &'static str = "0001234509876543";
 
-    const PLAINTEXT64: &'static str = "1234567890, -876543211000, 567567567567567";
-    const ENCODEDTEXT64: &'static str = "000001234567890c000876543211000d567567567567567c";
+    const PLAINTEXT64: &'static str = "1234567890, 876543211000, 567567567567567";
+    const ENCODEDTEXT64: &'static str = "000000123456789000008765432110000567567567567567";
 
     #[test]
-    fn test_i32_and_u32() {
+    fn test_words_32() {
         assert_eq!(
-            vec![0x0012345c_u32, 0x9876543d],
-            BinaryCodedDecimal::default()
-                .encode_i32(&[12345_i32, -9876543])
+            vec![0x00012345_u32, 0x09876543],
+            BinaryCodedDecimalUnsigned::default()
+                .encode_u32(&[12345_u32, 9876543])
                 .unwrap()
         );
-
         assert_eq!(
-            vec![12345_i32, -9876543],
-            BinaryCodedDecimal::default()
-                .decode_u32(&[0x0012345c_u32, 0x9876543d])
+            vec![12345_u32, 9876543],
+            BinaryCodedDecimalUnsigned::default()
+                .decode_u32(&[0x00012345_u32, 0x09876543])
                 .unwrap()
         );
     }
 
     #[test]
     fn encode_test_32() {
-        let code = BinaryCodedDecimal::default();
+        let code = BinaryCodedDecimalUnsigned::default();
         assert_eq!(ENCODEDTEXT32, code.encode(PLAINTEXT32).unwrap());
     }
 
     #[test]
-    fn encode_i32() {
-        assert_eq!(
-            vec![0x0012345c_u32, 0x9876543d],
-            BinaryCodedDecimal::default()
-                .encode_i32(&[12345_i32, -9876543])
-                .unwrap()
-        );
-    }
-
-    #[test]
     fn decode_test_32() {
-        let code = BinaryCodedDecimal::default();
+        let code = BinaryCodedDecimalUnsigned::default();
         assert_eq!(PLAINTEXT32, code.decode(ENCODEDTEXT32).unwrap())
     }
 
     #[test]
-    fn test_i64_and_u64() {
+    fn test_words_64() {
         assert_eq!(
             vec![
-                0x000001234567890c_u64,
-                0x000876543211000d,
-                0x567567567567567c
+                0x00000001234567890_u64,
+                0x0000876543211000,
+                0x0567567567567567
             ],
-            BinaryCodedDecimal::default()
-                .encode_i64(&[1234567890_i64, -876543211000, 567567567567567])
+            BinaryCodedDecimalUnsigned::default()
+                .encode_u64(&[1234567890_u64, 876543211000, 567567567567567])
                 .unwrap()
         );
         assert_eq!(
-            vec![1234567890_i64, -876543211000, 567567567567567],
-            BinaryCodedDecimal::default()
+            vec![1234567890_u64, 876543211000, 567567567567567],
+            BinaryCodedDecimalUnsigned::default()
                 .decode_u64(&[
-                    0x000001234567890c_u64,
-                    0x000876543211000d,
-                    0x567567567567567c
+                    0x0000001234567890_u64,
+                    0x0000876543211000,
+                    0x0567567567567567
                 ])
                 .unwrap()
         );
@@ -259,14 +226,14 @@ mod bcd_tests {
 
     #[test]
     fn encode_test_64() {
-        let mut code = BinaryCodedDecimal::default();
+        let mut code = BinaryCodedDecimalUnsigned::default();
         code.width = WordWidth::W64;
         assert_eq!(ENCODEDTEXT64, code.encode(PLAINTEXT64).unwrap());
     }
 
     #[test]
     fn decode_test_64() {
-        let mut code = BinaryCodedDecimal::default();
+        let mut code = BinaryCodedDecimalUnsigned::default();
         code.width = WordWidth::W64;
         assert_eq!(PLAINTEXT64, code.decode(ENCODEDTEXT64).unwrap())
     }
