8a0bab2e5020ad45246f95224c34f4ac7c663203
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2025-01-14 18:01:30 -0500

    updated bcd controls

--- src/code_panel/bcd_controls.rs
+++ src/code_panel/bcd_controls.rs
@@ -1,17 +1,24 @@
 use crate::ui_elements::UiElements;
 
 use super::CodeFrame;
-use codes::mathematical::binary_coded_decimal::{BinaryCodedDecimal, WordWidth};
+use codes::mathematical::{
+    binary_coded_decimal::{BinaryCodedDecimal, WordWidth},
+    binary_coded_decimal_unsigned::BinaryCodedDecimalUnsigned,
+};
 use utils::byte_formatting::ByteFormat;
 
 pub struct BcdFrame {
-    code: BinaryCodedDecimal,
+    signed: BinaryCodedDecimal,
+    unsigned: BinaryCodedDecimalUnsigned,
+    is_signed: bool,
 }
 
 impl Default for BcdFrame {
     fn default() -> Self {
         Self {
-            code: Default::default(),
+            signed: Default::default(),
+            unsigned: Default::default(),
+            is_signed: true,
         }
     }
 }
@@ -27,23 +34,30 @@ impl CodeFrame for BcdFrame {
         ui.group(|ui| {
             ui.subheading("Width");
             ui.horizontal(|ui| {
-                ui.selectable_value(&mut self.code.width, WordWidth::W32, "32-bit Words");
-                ui.selectable_value(&mut self.code.width, WordWidth::W64, "64-bit Words");
+                ui.selectable_value(&mut self.signed.width, WordWidth::W32, "32-bit Words");
+                ui.selectable_value(&mut self.signed.width, WordWidth::W64, "64-bit Words");
+                ui.selectable_value(&mut self.signed.width, WordWidth::W128, "128-bit Words");
             });
         });
 
         ui.add_space(8.0);
+        ui.checkbox(&mut self.is_signed, "Signed");
+
+        ui.add_space(12.0);
         ui.group(|ui| {
             ui.subheading("Byte Format");
             ui.horizontal(|ui| {
-                ui.selectable_value(&mut self.code.formatting, ByteFormat::Hex, "Hexadecimal");
-                ui.selectable_value(&mut self.code.formatting, ByteFormat::Base64, "Base64");
-                ui.selectable_value(&mut self.code.formatting, ByteFormat::Binary, "Binary");
+                ui.selectable_value(&mut self.signed.formatting, ByteFormat::Hex, "Hexadecimal");
+                ui.selectable_value(&mut self.signed.formatting, ByteFormat::Base64, "Base64");
+                ui.selectable_value(&mut self.signed.formatting, ByteFormat::Binary, "Binary");
             });
         });
     }
 
     fn code(&self) -> &dyn codes::traits::Code {
-        &self.code
+        match self.is_signed {
+            true => &self.signed,
+            false => &self.unsigned,
+        }
     }
 }
