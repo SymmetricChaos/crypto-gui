f6db1a85073f07701f75430fb1b7b3ac1f67276c
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2025-02-13 20:17:30 -0500

    creating a blake3 implementation of my own

---
+++ hashers/src/blake/blake3_self.rs
@@ -0,0 +1,189 @@
+// BLAKE3 but implemented myself rather than just copying the reference
+
+use crate::{blake_double_round, traits::StatefulHasher};
+use std::cmp::min;
+use utils::byte_formatting::make_u32s_le;
+
+// https://github.com/BLAKE3-team/BLAKE3
+// https://github.com/BLAKE3-team/BLAKE3-specs/blob/master/blake3.pdf
+// https://github.com/BLAKE3-team/BLAKE3/blob/master/reference_impl/reference_impl.rs
+
+const OUT_LEN: usize = 32;
+const KEY_LEN: usize = 32;
+
+// Each chunk of 1024 bytes (256 words) is divided up into blocks of 64 bytes (16 words).
+// Chunks are arranged into a tree structure while blocks are a simple array within each chunk
+const BLOCK_LEN: usize = 64;
+const CHUNK_LEN: usize = 1024;
+
+// Bitflags that can be set for chunks
+const CHUNK_START: u32 = 1 << 0;
+const CHUNK_END: u32 = 1 << 1;
+const PARENT: u32 = 1 << 2;
+const ROOT: u32 = 1 << 3;
+const KEYED_HASH: u32 = 1 << 4;
+const DERIVE_KEY_CONTEXT: u32 = 1 << 5;
+const DERIVE_KEY_MATERIAL: u32 = 1 << 6;
+
+// Same IV as BLAKE2s, sqrt of the first eight primes
+const IV: [u32; 8] = [
+    0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19,
+];
+
+const MSG_PERMUTATION: [usize; 16] = [2, 6, 3, 10, 7, 0, 4, 13, 1, 11, 12, 5, 9, 14, 15, 8];
+
+const ROTS: [u32; 4] = [16, 12, 8, 7];
+const WORD_ORDER: [usize; 16] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+fn permute(m: &mut [u32; 16]) {
+    let mut permuted = [0; 16];
+    for i in 0..16 {
+        permuted[i] = m[MSG_PERMUTATION[i]];
+    }
+    *m = permuted;
+}
+
+// The compression function.
+// Compresses a block into a chaining value.
+fn compress(
+    chaining_value: &[u32; 8],
+    block_words: &[u32; 16],
+    counter: u64,
+    block_len: u32,
+    flags: u32,
+) -> [u32; 16] {
+    let mut state = [
+        chaining_value[0],
+        chaining_value[1],
+        chaining_value[2],
+        chaining_value[3],
+        chaining_value[4],
+        chaining_value[5],
+        chaining_value[6],
+        chaining_value[7],
+        IV[0],
+        IV[1],
+        IV[2],
+        IV[3],
+        counter as u32,
+        (counter >> 32) as u32,
+        block_len,
+        flags,
+    ];
+    let mut block = *block_words;
+
+    blake_double_round!(&mut state, &block, ROTS, WORD_ORDER);
+    permute(&mut block);
+    blake_double_round!(&mut state, &block, ROTS, WORD_ORDER);
+    permute(&mut block);
+    blake_double_round!(&mut state, &block, ROTS, WORD_ORDER);
+    permute(&mut block);
+    blake_double_round!(&mut state, &block, ROTS, WORD_ORDER);
+    permute(&mut block);
+    blake_double_round!(&mut state, &block, ROTS, WORD_ORDER);
+    permute(&mut block);
+    blake_double_round!(&mut state, &block, ROTS, WORD_ORDER);
+    permute(&mut block);
+    blake_double_round!(&mut state, &block, ROTS, WORD_ORDER);
+
+    for i in 0..8 {
+        state[i] ^= state[i + 8];
+        state[i + 8] ^= chaining_value[i];
+    }
+    state
+}
+
+fn first_8_words(compression_output: [u32; 16]) -> [u32; 8] {
+    compression_output[0..8].try_into().unwrap()
+}
+
+pub struct Chunk {}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+pub enum Blake3Mode {
+    Hash,
+    KeyedHash,
+    DeriveKey,
+}
+
+pub struct Blake3 {
+    key_words: [u32; 8],
+    flags: u32,
+}
+
+impl Blake3 {
+    fn new_internal(key_words: [u32; 8], flags: u32) -> Self {
+        Self { key_words, flags }
+    }
+
+    /// Construct a new Hasher for the regular hash function.
+    pub fn new() -> Self {
+        Self {
+            key_words: [0u32; 8],
+            flags: 0,
+        }
+    }
+
+    /// Construct a new Hasher for the keyed hash function.
+    pub fn new_keyed(key: &[u8; KEY_LEN]) -> Self {
+        // The same as Self::new() but with the key material instead of the default IV and they KEYED_HASH mode set
+        Self {
+            key_words: make_u32s_le(key),
+            flags: KEYED_HASH,
+        }
+    }
+
+    /// Construct a new `Hasher` for the key derivation function. The context
+    /// string should be hardcoded, globally unique, and application-specific.
+    pub fn new_derive_key(context: &str) -> Self {
+        // The context is converted into a IV by hashing it in the DERIVE_KEY_CONTEXT mode
+        let mut context_hasher = Self::new_internal(IV, DERIVE_KEY_CONTEXT);
+        context_hasher.update(context.as_bytes());
+        let context_key = context_hasher.finalize();
+        // The hasher used in DERIVE_KEY_MATERIAL mode
+        Self::new_internal(make_u32s_le::<8>(&context_key), DERIVE_KEY_MATERIAL)
+    }
+}
+
+impl StatefulHasher for Blake3 {
+    fn update(&mut self, bytes: &[u8]) {
+        // while !input.is_empty() {
+        //     // If the current chunk is complete, finalize it and reset the
+        //     // chunk state. More input is coming, so this chunk is not ROOT.
+        //     if self.chunk_state.len() == CHUNK_LEN {
+        //         let chunk_cv = self.chunk_state.output().chaining_value();
+        //         let total_chunks = self.chunk_state.chunk_counter + 1;
+        //         self.add_chunk_chaining_value(chunk_cv, total_chunks);
+        //         self.chunk_state = ChunkState::new(self.key_words, total_chunks, self.flags);
+        //     }
+
+        //     // Compress input bytes into the current chunk state.
+        //     let want = CHUNK_LEN - self.chunk_state.len();
+        //     let take = min(want, input.len());
+        //     self.chunk_state.update(&input[..take]);
+        //     input = &input[take..];
+        // }
+        todo!()
+    }
+
+    fn finalize(self) -> Vec<u8> {
+        // Starting with the Output from the current chunk, compute all the
+        // parent chaining values along the right edge of the tree, until we
+        // have the root Output.
+        // let mut output = self.chunk_state.output();
+        // let mut parent_nodes_remaining = self.cv_stack_len as usize;
+        // while parent_nodes_remaining > 0 {
+        //     parent_nodes_remaining -= 1;
+        //     output = parent_output(
+        //         self.cv_stack[parent_nodes_remaining],
+        //         output.chaining_value(),
+        //         self.key_words,
+        //         self.flags,
+        //     );
+        // }
+        // output.root_output_bytes(out_slice);
+        todo!()
+    }
+
+    crate::stateful_hash_helpers!();
+}
--- src/hasher_panel/blake3_controls.rs
+++ src/hasher_panel/blake3_controls.rs
@@ -1,133 +1,133 @@
-use crate::ui_elements::{control_string, UiElements};
+// use crate::ui_elements::{control_string, UiElements};
 
-use super::HasherFrame;
-use egui::Button;
-use hashers::{
-    blake::blake3::{Blake3, Blake3Mode},
-    errors::HasherError,
-};
-use rand::{thread_rng, RngCore};
-use utils::byte_formatting::ByteFormat;
+// use super::HasherFrame;
+// use egui::Button;
+// use hashers::{
+//     blake::blake3::{Blake3, Blake3Mode},
+//     errors::HasherError,
+// };
+// use rand::{thread_rng, RngCore};
+// use utils::byte_formatting::ByteFormat;
 
-pub struct Blake3Frame {
-    hasher: Blake3,
-    key_string: String,
-    context_string: String,
-    valid_key: bool,
-}
+// pub struct Blake3Frame {
+//     hasher: Blake3,
+//     key_string: String,
+//     context_string: String,
+//     valid_key: bool,
+// }
 
-impl Default for Blake3Frame {
-    fn default() -> Self {
-        Self {
-            hasher: Default::default(),
-            key_string: String::new(),
-            context_string: String::new(),
-            valid_key: false,
-        }
-    }
-}
+// impl Default for Blake3Frame {
+//     fn default() -> Self {
+//         Self {
+//             hasher: Default::default(),
+//             key_string: String::new(),
+//             context_string: String::new(),
+//             valid_key: false,
+//         }
+//     }
+// }
 
-impl Blake3Frame {
-    fn key_control(&mut self, ui: &mut egui::Ui) {
-        let string = &mut self.key_string;
-        let enabled = self.hasher.mode == Blake3Mode::Keyed;
-        ui.subheading("Key");
-        ui.label(
-            "Exactly 64 hexadecimal digits must be supplied, specifying all 256 bits of the key.",
-        );
-        ui.horizontal(|ui| {
-            if control_string(ui, string, enabled).changed() {
-                *string = string
-                    .chars()
-                    .filter(|c| c.is_ascii_hexdigit())
-                    .take(64)
-                    .collect();
-            };
-            if string.len() != 64 {
-                self.valid_key = false;
-            } else {
-                self.valid_key = true;
-            }
-            if ui
-                .add_enabled(enabled, Button::new("🎲"))
-                .on_hover_text("randomize")
-                .clicked()
-            {
-                let mut rng = thread_rng();
-                rng.fill_bytes(&mut self.hasher.key);
-                *string = ByteFormat::Hex.byte_slice_to_text(&mut self.hasher.key)
-            }
-        });
+// impl Blake3Frame {
+//     fn key_control(&mut self, ui: &mut egui::Ui) {
+//         let string = &mut self.key_string;
+//         let enabled = self.hasher.mode == Blake3Mode::Keyed;
+//         ui.subheading("Key");
+//         ui.label(
+//             "Exactly 64 hexadecimal digits must be supplied, specifying all 256 bits of the key.",
+//         );
+//         ui.horizontal(|ui| {
+//             if control_string(ui, string, enabled).changed() {
+//                 *string = string
+//                     .chars()
+//                     .filter(|c| c.is_ascii_hexdigit())
+//                     .take(64)
+//                     .collect();
+//             };
+//             if string.len() != 64 {
+//                 self.valid_key = false;
+//             } else {
+//                 self.valid_key = true;
+//             }
+//             if ui
+//                 .add_enabled(enabled, Button::new("🎲"))
+//                 .on_hover_text("randomize")
+//                 .clicked()
+//             {
+//                 let mut rng = thread_rng();
+//                 rng.fill_bytes(&mut self.hasher.key);
+//                 *string = ByteFormat::Hex.byte_slice_to_text(&mut self.hasher.key)
+//             }
+//         });
 
-        if self.valid_key {
-            if let Ok(new) = ByteFormat::Hex.text_to_bytes(string) {
-                match new.try_into() {
-                    Ok(key) => self.hasher.key = key,
-                    Err(_) => unreachable!(),
-                }
-            } else {
-                unreachable!("unable to parse input");
-            }
-        } else {
-            if self.hasher.mode == Blake3Mode::Keyed {
-                ui.error_text("invalid key");
-            } else {
-                ui.error_text("");
-            }
-        }
-    }
+//         if self.valid_key {
+//             if let Ok(new) = ByteFormat::Hex.text_to_bytes(string) {
+//                 match new.try_into() {
+//                     Ok(key) => self.hasher.key = key,
+//                     Err(_) => unreachable!(),
+//                 }
+//             } else {
+//                 unreachable!("unable to parse input");
+//             }
+//         } else {
+//             if self.hasher.mode == Blake3Mode::Keyed {
+//                 ui.error_text("invalid key");
+//             } else {
+//                 ui.error_text("");
+//             }
+//         }
+//     }
 
-    fn context_control(&mut self, ui: &mut egui::Ui) {
-        let string = &mut self.context_string;
-        let enabled = self.hasher.mode == Blake3Mode::KeyDerivation;
-        ui.subheading("Context String");
-        ui.label("For key derivation a static globally unique string must be provided. The suggested format is [application] [creation timestamp] [purpose].");
-        ui.horizontal(|ui| {
-            control_string(ui, string, enabled);
-        });
-    }
-}
+//     fn context_control(&mut self, ui: &mut egui::Ui) {
+//         let string = &mut self.context_string;
+//         let enabled = self.hasher.mode == Blake3Mode::KeyDerivation;
+//         ui.subheading("Context String");
+//         ui.label("For key derivation a static globally unique string must be provided. The suggested format is [application] [creation timestamp] [purpose].");
+//         ui.horizontal(|ui| {
+//             control_string(ui, string, enabled);
+//         });
+//     }
+// }
 
-impl HasherFrame for Blake3Frame {
-    fn ui(&mut self, ui: &mut egui::Ui, _errors: &mut String) {
-        ui.hyperlink_to(
-            "see the code",
-            "https://github.com/SymmetricChaos/crypto-gui/blob/master/hashers/src/blake",
-        );
-        ui.add_space(8.0);
+// impl HasherFrame for Blake3Frame {
+//     fn ui(&mut self, ui: &mut egui::Ui, _errors: &mut String) {
+//         ui.hyperlink_to(
+//             "see the code",
+//             "https://github.com/SymmetricChaos/crypto-gui/blob/master/hashers/src/blake",
+//         );
+//         ui.add_space(8.0);
 
-        ui.byte_io_mode_hasher(
-            &mut self.hasher.input_format,
-            &mut self.hasher.output_format,
-        );
-        ui.add_space(16.0);
+//         ui.byte_io_mode_hasher(
+//             &mut self.hasher.input_format,
+//             &mut self.hasher.output_format,
+//         );
+//         ui.add_space(16.0);
 
-        ui.horizontal(|ui| {
-            ui.selectable_value(&mut self.hasher.mode, Blake3Mode::Unkeyed, "Unkeyed");
-            ui.selectable_value(&mut self.hasher.mode, Blake3Mode::Keyed, "Keyed");
-            ui.selectable_value(
-                &mut self.hasher.mode,
-                Blake3Mode::KeyDerivation,
-                "Key Derivation",
-            );
-        });
+//         ui.horizontal(|ui| {
+//             ui.selectable_value(&mut self.hasher.mode, Blake3Mode::Unkeyed, "Unkeyed");
+//             ui.selectable_value(&mut self.hasher.mode, Blake3Mode::Keyed, "Keyed");
+//             ui.selectable_value(
+//                 &mut self.hasher.mode,
+//                 Blake3Mode::KeyDerivation,
+//                 "Key Derivation",
+//             );
+//         });
 
-        ui.add_space(16.0);
+//         ui.add_space(16.0);
 
-        self.key_control(ui);
+//         self.key_control(ui);
 
-        ui.add_space(16.0);
+//         ui.add_space(16.0);
 
-        self.context_control(ui);
+//         self.context_control(ui);
 
-        ui.add_space(16.0);
+//         ui.add_space(16.0);
 
-        ui.subheading("Hash Length");
-        ui.label("BLAKE3 allows a variety of output lengths, with a default of 32 bytes (256 bits). While up to 2^64 bytes can be returned this interface limits output to 256 bytes (2048 bits). Unlike BLAKE2 there is no domain seperation for different lengths so short outputs are prefixes of long ones.");
-        ui.add(egui::DragValue::new(&mut self.hasher.hash_len).range(1..=256));
-    }
+//         ui.subheading("Hash Length");
+//         ui.label("BLAKE3 allows a variety of output lengths, with a default of 32 bytes (256 bits). While up to 2^64 bytes can be returned this interface limits output to 256 bytes (2048 bits). Unlike BLAKE2 there is no domain seperation for different lengths so short outputs are prefixes of long ones.");
+//         ui.add(egui::DragValue::new(&mut self.hasher.hash_len).range(1..=256));
+//     }
 
-    fn hash_string(&self, text: &str) -> Result<String, HasherError> {
-        todo!()
-    }
-}
+//     fn hash_string(&self, text: &str) -> Result<String, HasherError> {
+//         todo!()
+//     }
+// }
