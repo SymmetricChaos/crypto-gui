34eb636d513faa103dbce444c7af72528589e1ec
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2025-02-24 11:12:25 -0500

    naor-reingold description and improvements

--- rngs/src/naor_reingold.rs
+++ rngs/src/naor_reingold.rs
@@ -5,26 +5,39 @@ use utils::{
     math_functions::{mod_mul_64, mod_pow_64},
 };
 
-pub fn valid_nr_constants(p: u64, q: u64, g: u64) -> bool {
-    p > q && is_prime64(p) && is_prime64(q) && (p - 1) % q == 0 && mod_pow_64(g, q, p) == 1
+pub fn valid_nr_constants(p: u64, q: u64, g: u64, arr: &Vec<u64>) -> bool {
+    p > q
+        && is_prime64(p)
+        && is_prime64(q)
+        && (p - 1) % q == 0
+        && mod_pow_64(g, q, p) == 1
+        && arr.len() < 64
 }
 
-pub fn valid_nr_constants_verbose(p: u64, q: u64, g: u64) -> Result<(), String> {
-    let mut err = String::new();
+pub fn valid_nr_constants_verbose(
+    p: u64,
+    q: u64,
+    g: u64,
+    arr: &Vec<u64>,
+) -> Result<(), Vec<&'static str>> {
+    let mut err = Vec::new();
     if p <= q {
-        err.push_str("p must be greater than q;");
+        err.push("p must be greater than q");
     }
     if !is_prime64(p) {
-        err.push_str("p must be prime;");
+        err.push("p must be prime");
     }
     if !is_prime64(q) {
-        err.push_str("q must be prime;");
+        err.push("q must be prime");
     }
     if (p - 1) % q != 0 {
-        err.push_str("p-1 must be a multiple of q;");
+        err.push("q must be a factor of p-1");
     }
     if mod_pow_64(g, q, p) != 1 {
-        err.push_str("g must be of multiplicative order q in the finite field F_p;");
+        err.push("g must be of multiplicative order q in the ring F_p");
+    }
+    if arr.len() >= 64 {
+        err.push("arr must have a length less than 64");
     }
 
     if err.is_empty() {
@@ -36,7 +49,6 @@ pub fn valid_nr_constants_verbose(p: u64, q: u64, g: u64) -> Result<(), String> 
 
 pub struct NaorReingold {
     p: u64,
-    q: u64, // not actually used in the generating process
     generator: u64,
     arr: Vec<u64>,
     ctr: u64,
@@ -46,7 +58,6 @@ impl Default for NaorReingold {
     fn default() -> Self {
         Self {
             p: 1223,
-            q: 47,
             generator: 27,
             arr: vec![7, 6, 5, 4, 3, 2],
             ctr: 1,
@@ -55,14 +66,33 @@ impl Default for NaorReingold {
 }
 
 impl NaorReingold {
-    pub fn init(p: u64, q: u64, g: u64, a: Vec<u64>, x: u64) -> Result<Self, String> {
-        match valid_nr_constants_verbose(p, q, g) {
+    // Initialize a Naor-Reingold PRNG, returns None if invalid constants are given
+    pub fn init(p: u64, q: u64, generator: u64, arr: Vec<u64>, ctr: u64) -> Option<Self> {
+        match valid_nr_constants(p, q, generator, &arr) {
+            true => Some(Self {
+                p,
+                generator,
+                arr: arr.iter().map(|a| a % p).collect(),
+                ctr,
+            }),
+            false => None,
+        }
+    }
+
+    // Initialize a Naor-Reingold PRNG, returns a list of failures if invalid constants are given
+    pub fn init_verbose(
+        p: u64,
+        q: u64,
+        generator: u64,
+        arr: Vec<u64>,
+        ctr: u64,
+    ) -> Result<Self, Vec<&'static str>> {
+        match valid_nr_constants_verbose(p, q, generator, &arr) {
             Ok(_) => Ok(Self {
                 p,
-                q,
-                generator: g,
-                arr: a,
-                ctr: x,
+                generator,
+                arr: arr.iter().map(|a| a % p).collect(),
+                ctr,
             }),
             Err(e) => Err(e),
         }
@@ -86,7 +116,12 @@ impl ClassicRng for NaorReingold {
         let out = mod_pow_64(self.generator, e, self.p);
 
         // Increment
+        // Any function that visits every possible value in 0..(2^(self.arr.len())) can be used
+        // but an increment is the simplest and it is not clear that others offer additional
+        // security
         self.ctr += 1;
+        // Reduce
+        self.ctr %= 1 << self.arr.len() as u64;
 
         out as u32
     }
