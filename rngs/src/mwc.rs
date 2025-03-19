14345e515a084b9dfb6cb2f62d251e596c3e3e31
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2025-03-19 17:21:14 -0400

    Update mwc.rs

--- rngs/src/mwc.rs
+++ rngs/src/mwc.rs
@@ -1,31 +1,51 @@
-use std::num::Wrapping;
-
 use crate::ClassicRng;
 
-// const A1: u128 = 0xffebb71d94fcdaf9;
+const A1: u128 = 0xffeb_b71d_94fc_daf9;
+const MOD: u128 = (0xffff_ffff_ffff_ffff << 64) | (A1 - 1);
+const JUMP: u128 = 0xa72f_9a35_4720_8003_2f65_fed2_e840_0983;
+const LONG_JUMP: u128 = 0xe6f7_8144_67f3_fcdd_3946_49cf_d676_9c91;
 
 pub struct MultiplyWithCarry128 {
-    a: Wrapping<u128>,
-    x: Wrapping<u128>,
-    c: Wrapping<u128>,
+    x: u128,
+    c: u128,
 }
 
 impl Default for MultiplyWithCarry128 {
     fn default() -> Self {
         Self {
-            a: Wrapping(0xffebb71d94fcdaf9),
-            x: Wrapping(0x0BAD_5EED),
-            c: Wrapping(1),
+            x: 0x0BAD_5EED_0BAD_5EED,
+            c: 1,
         }
+    }
+}
+
+impl MultiplyWithCarry128 {
+    pub fn jump(&mut self) {
+        let s = self.x << 64 + self.c;
+        let t = s.wrapping_mul(JUMP) % MOD;
+        self.x = t >> 64;
+        self.c = t & 0xffff_ffff_ffff_ffff;
+    }
+
+    pub fn long(&mut self) {
+        let s = self.x << 64 + self.c;
+        let t = s.wrapping_mul(LONG_JUMP) % MOD;
+        self.x = t >> 64;
+        self.c = t & 0xffff_ffff_ffff_ffff;
+    }
+
+    pub fn step(&mut self) {
+        let t = A1.wrapping_mul(self.x).wrapping_add(self.c);
+        self.x = t & 0xffff_ffff_ffff_ffff;
+        self.c = t >> 64;
     }
 }
 
 impl MultiplyWithCarry128 {
     pub fn from_u64(seed: u64) -> Self {
         Self {
-            a: Wrapping(0xffebb71d94fcdaf9),
-            x: Wrapping(seed as u128),
-            c: Wrapping(1),
+            x: seed as u128,
+            c: 1,
         }
     }
 }
@@ -33,17 +53,50 @@ impl MultiplyWithCarry128 {
 impl ClassicRng for MultiplyWithCarry128 {
     fn next_u32(&mut self) -> u32 {
         let out = self.x;
-        let t = self.a * self.x + self.c;
-        self.x = t;
-        self.c = t >> 64;
-        out.0 as u32
+        self.step();
+        out as u32
     }
 
     fn next_u64(&mut self) -> u64 {
         let out = self.x;
-        let t = self.a * self.x + self.c;
-        self.x = t;
-        self.c = t >> 64;
-        out.0 as u64
+        self.step();
+        out as u64
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn outputs() {
+        let mut rng = MultiplyWithCarry128::from_u64(0x0BAD5EED0BAD5EED);
+        let mut v = Vec::new();
+        for _ in 0..10 {
+            v.push(rng.next_u64());
+        }
+        assert_eq!(
+            vec![
+                841433077584322285,
+                17482153403755996806,
+                8398668349764859332,
+                5636601027830058678,
+                9606858525927533180,
+                1767612197145370004,
+                10650035520294345329,
+                12122253265975378252,
+                8964605576554650523,
+                6633924877535988019,
+            ],
+            v
+        );
+    }
+
+    #[test]
+    fn test_jump() {
+        let mut rng = MultiplyWithCarry128::from_u64(0x0BAD5EED0BAD5EED);
+        rng.jump();
+        println!("{} {}", rng.x, rng.c)
     }
 }
