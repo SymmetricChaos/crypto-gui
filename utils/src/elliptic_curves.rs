dfab3eebb387d6fe28b8cb7f7ab831bb71e1eacc
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2025-03-21 12:33:29 -0400

    more ecc

--- utils/src/elliptic_curves.rs
+++ utils/src/elliptic_curves.rs
@@ -1,96 +1,123 @@
 use crypto_bigint::{NonZero, U256};
-use paste::paste;
+// use paste::paste;
 
-macro_rules! large_point {
-    ($t:ty, $n:ident) => {
-        paste! {
-            #[derive(Debug)]
-            pub struct  [<Point $t >] {
-                x: $t,
-                y: $t,
-            }
+// macro_rules! large_point {
+//     ($t:ty, $n:ident) => {
+//         paste! {
+//             #[derive(Debug)]
+//             pub struct  [<Point $t >] {
+//                 x: $t,
+//                 y: $t,
+//             }
 
-            impl [<Point $t >] {
-                pub const fn from_be_hex(x: &str, y: &str) -> Self {
-                    Self {
-                        x: <$t>::from_be_hex(x),
-                        y: <$t>::from_be_hex(y),
-                    }
-                }
+//             impl [<Point $t >] {
+//                 pub const fn from_be_hex(x: &str, y: &str) -> Self {
+//                     Self {
+//                         x: <$t>::from_be_hex(x),
+//                         y: <$t>::from_be_hex(y),
+//                     }
+//                 }
 
-                pub const fn from_u64(x: u64, y: u64) -> Self {
-                    Self {
-                        x: <$t>::from_u64(x),
-                        y: <$t>::from_u64(y),
-                    }
-                }
+//                 pub const fn from_u64(x: u64, y: u64) -> Self {
+//                     Self {
+//                         x: <$t>::from_u64(x),
+//                         y: <$t>::from_u64(y),
+//                     }
+//                 }
 
-                pub fn to_string(&self) -> String {
-                    format!(
-                        "({},{})",
-                        self.x.to_string().trim_matches('0'),
-                        self.y.to_string().trim_matches('0')
-                    )
-                }
-            }
-            pub fn [< on_curve_ $n >] (n: &[<Point $t >], a: &$t, b: &$t, p: &$t) -> bool {
-                let p = NonZero::new(*p).unwrap();
-                let x3 = n.x.mul_mod(&n.x, &p).mul_mod(&n.x, &p);
-                x3.add_mod(&n.x.mul_mod(&a, &p), &p).add_mod(&b, &p) == n.y.mul_mod(&n.y, &p)
-            }
+//                 pub fn to_string(&self) -> String {
+//                     format!(
+//                         "({},{})",
+//                         self.x.to_string().trim_matches('0'),
+//                         self.y.to_string().trim_matches('0')
+//                     )
+//                 }
+//             }
+//             pub fn [< on_curve_ $n >] (n: &[<Point $t >], a: &$t, b: &$t, m: &$t) -> bool {
+//                 let p = NonZero::new(*m).unwrap();
+//                 let x3 = n.x.mul_mod(&n.x, &p).mul_mod(&n.x, &p);
+//                 x3.add_mod(&n.x.mul_mod(&a, &p), &p).add_mod(&b, &p) == n.y.mul_mod(&n.y, &p)
+//             }
 
-            pub fn [< ec_add_ $n >](n1: &[<Point $t >], n2: &[<Point $t >], p: &$t) -> [<Point $t >] {
-                let modulus = NonZero::new(*p).unwrap();
-                let dx = n2.x.sub_mod(&n1.x, &modulus);
-                let dy = n2.y.sub_mod(&n1.y, &modulus);
-                let s: $t = dy.mul_mod(&dx.inv_mod(&p).unwrap(), &modulus);
-                let x = s
-                    .mul_mod(&s, &modulus)
-                    .sub_mod(&n1.x.add_mod(&n2.x, &modulus), &p);
-                let y = s
-                    .mul_mod(&n1.x.sub_mod(&x, &p), &modulus)
-                    .sub_mod(&n1.y, &modulus);
-                [<Point $t >] { x, y }
-            }
+//             pub fn [< ec_add_ $n >](lhs: &[<Point $t >], rhs: &[<Point $t >], m: &$t) -> [<Point $t >] {
+//                 let modulus = NonZero::new(*m).unwrap();
+//                 let dx = rhs.x.sub_mod(&lhs.x, &modulus);
+//                 let dy = rhs.y.sub_mod(&lhs.y, &modulus);
+//                 let s: $t = dy.mul_mod(&dx.inv_mod(&modulus).unwrap(), &modulus);
+//                 let x = s
+//                     .mul_mod(&s, &modulus)
+//                     .sub_mod(&lhs.x.add_mod(&rhs.x, &modulus), &modulus);
+//                 let y = s
+//                     .mul_mod(&lhs.x.sub_mod(&x, &modulus), &modulus)
+//                     .sub_mod(&lhs.y, &modulus);
+//                 [<Point $t >] { x, y }
+//             }
+
+//             pub fn [< ec_mul_ $n >](lhs: &[<Point $t >], rhs: &[<Point $t >], m: &$t) -> [<Point $t >] {
+//                 let modulus = NonZero::new(*m).unwrap();
+//                 let bits = lhs.bits();
+//                 [<Point $t >] { x, y }
+//             }
+//         }
+//     };
+// }
+
+#[derive(Debug, Clone)]
+pub struct PointU256 {
+    x: U256,
+    y: U256,
+}
+impl PointU256 {
+    pub const fn from_be_hex(x: &str, y: &str) -> Self {
+        Self {
+            x: <U256>::from_be_hex(x),
+            y: <U256>::from_be_hex(y),
         }
-    };
+    }
+    pub const fn from_u64(x: u64, y: u64) -> Self {
+        Self {
+            x: <U256>::from_u64(x),
+            y: <U256>::from_u64(y),
+        }
+    }
+    pub fn to_string(&self) -> String {
+        format!(
+            "({},{})",
+            self.x.to_string().trim_matches('0'),
+            self.y.to_string().trim_matches('0')
+        )
+    }
+}
+pub fn on_curve_u256(n: &PointU256, a: &U256, b: &U256, m: &U256) -> bool {
+    let p = NonZero::new(*m).unwrap();
+    let x3 = n.x.mul_mod(&n.x, &p).mul_mod(&n.x, &p);
+    x3.add_mod(&n.x.mul_mod(&a, &p), &p).add_mod(&b, &p) == n.y.mul_mod(&n.y, &p)
 }
 
-large_point!(U256, u256);
-
-const P: PointU256 = PointU256::from_be_hex(
-    "6b17d1f2e12c4247f8bce6e563a440f277037d812deb33a0f4a13945d898c296",
-    "4fe342e2fe1a7f9b8ee7eb4a7c0f9e162bce33576b315ececbb6406837bf51f5",
-);
-
-const Q: PointU256 = PointU256::from_be_hex(
-    "c97445f45cdef9f0d3e05e1e585fc297235b82b5be8ff3efca67c59852018192",
-    "b28ef557ba31dfcbdd21ac46e2a91e3c304f44cb87058ada2cb815151e610046",
-);
-
-// pub struct CurveP256 {
-//     p: U256,
-//     n: U256,
-//     b: U256,
-// }
-
-// impl Default for CurveP256 {
-//     fn default() -> Self {
-//         Self {
-//             // 115792089210356248762697446949407573530086143415290314195533631308867097853951
-//             p: U256::from_be_hex(
-//                 "FFFFFFFF00000001000000000000000000000000FFFFFFFFFFFFFFFFFFFFFFFF",
-//             ),
-//             // 115792089210356248762697446949407573529996955224135760342422259061068512044369
-//             n: U256::from_be_hex(
-//                 "FFFFFFFF00000000FFFFFFFFFFFFFFFFBCE6FAADA7179E84F3B9CAC2FC632551",
-//             ),
-//             b: U256::from_be_hex(
-//                 "5ac635d8aa3a93e7b3ebbd55769886bc651d06b0cc53b0f63bce3c3e27d2604b",
-//             ),
-//         }
-//     }
-// }
+pub fn ec_add_u256(lhs: &PointU256, rhs: &PointU256, m: &U256) -> PointU256 {
+    let modulus = NonZero::new(*m).unwrap();
+    let dx = rhs.x.sub_mod(&lhs.x, &modulus);
+    let dy = rhs.y.sub_mod(&lhs.y, &modulus);
+    let s: U256 = dy.mul_mod(&dx.inv_mod(&modulus).unwrap(), &modulus);
+    let x = s
+        .mul_mod(&s, &modulus)
+        .sub_mod(&lhs.x.add_mod(&rhs.x, &modulus), &modulus);
+    let y = s
+        .mul_mod(&lhs.x.sub_mod(&x, &modulus), &modulus)
+        .sub_mod(&lhs.y, &modulus);
+    PointU256 { x, y }
+}
+pub fn ec_scalar_mul_u256(lhs: &PointU256, rhs: &U256, m: &U256) -> PointU256 {
+    let mut out = PointU256::from_u64(0, 0);
+    let mut temp = lhs.clone();
+    for i in 0..256 {
+        if rhs.bit(i).into() {
+            out = ec_add_u256(&out, &temp, &m);
+        }
+        temp = ec_add_u256(&temp, &temp, &m)
+    }
+    out
+}
 
 #[cfg(test)]
 mod tests {
