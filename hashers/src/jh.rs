76426a4688ae0bbe3983df2e83720f9c352faee6
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2025-04-08 20:56:58 -0400

    jh round constants

--- hashers/src/jh.rs
+++ hashers/src/jh.rs
@@ -46,7 +46,33 @@ pub fn round(nibbles: &mut [u8; 256], round_constant: [u8; 64]) {
 }
 
 // Generate the next round constant
-pub fn next_rc() {}
+pub fn next_rc(round_constant: &mut [u8; 64]) {
+    let mut t = [0; 64];
+    for i in 0..64 {
+        t[i] = SBOXES[0][round_constant[i] as usize];
+    }
+
+    for i in (0..64).step_by(2) {
+        t[i + 1] ^= (((t[i]) << 1) ^ ((t[i]) >> 3) ^ (((t[i]) >> 2) & 2)) & 0xf;
+        t[i] ^= (((t[i + 1]) << 1) ^ ((t[i + 1]) >> 3) ^ (((t[i + 1]) >> 2) & 2)) & 0xf;
+    }
+
+    // Permutation Pi6
+    for i in (0..64).step_by(4) {
+        t.swap(i + 2, i + 3);
+    }
+
+    // Permutation P'6
+    for i in 0..32 {
+        round_constant[i] = t[i << 1];
+        round_constant[i + 32] = t[(i << 1) + 1];
+    }
+
+    // Permutation Phi6
+    for i in (32..64).step_by(2) {
+        round_constant.swap(i, i + 1);
+    }
+}
 
 pub struct Jh {
     state: [u64; 16],
@@ -92,6 +118,7 @@ impl StatefulHasher for Jh {
     fn update(&mut self, mut bytes: &[u8]) {
         crate::compression_routine!(self.buffer, bytes, BLOCK_LEN, {
             self.bits_taken += 512;
+            todo!()
         });
     }
 
@@ -106,6 +133,28 @@ impl StatefulHasher for Jh {
     }
 
     crate::stateful_hash_helpers!();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn rc_generation() {
+        let mut rc = RC0;
+        for _ in 0..41 {
+            next_rc(&mut rc);
+        }
+        assert_eq!(
+            [
+                0x7, 0x3, 0xb, 0xd, 0x6, 0x9, 0x7, 0x8, 0xc, 0x5, 0x9, 0xf, 0x2, 0xb, 0x2, 0x1,
+                0x9, 0x4, 0x4, 0x9, 0xb, 0x3, 0x6, 0x7, 0x7, 0x0, 0xf, 0xb, 0x3, 0x1, 0x3, 0xf,
+                0xb, 0xe, 0x2, 0xd, 0xa, 0x2, 0x8, 0xf, 0x6, 0xb, 0x0, 0x4, 0x2, 0x7, 0x5, 0xf,
+                0x0, 0x7, 0x1, 0xa, 0x1, 0xb, 0x1, 0x9, 0x3, 0xd, 0xd, 0xe, 0x2, 0x0, 0x7, 0x2
+            ],
+            rc
+        )
+    }
 }
 
 crate::stateful_hash_tests!(
