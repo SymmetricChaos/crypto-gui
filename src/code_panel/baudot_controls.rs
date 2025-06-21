6541f3fece60377dbb1deb84484f3379ec50fb18
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2025-06-21 13:29:21 -0400

    more baudot updates

--- src/code_panel/baudot_controls.rs
+++ src/code_panel/baudot_controls.rs
@@ -25,9 +25,9 @@ impl CodeFrame for BaudotFrame {
         ui.add_space(8.0);
 
         ui.label("The second International Telegraphy Alphabet (ITA2) was based on work by Baudot and Murray. It became a widely used standard that made ITA1 obsolete. This variant is still in minor usage and is the origin of the 'baud' as a five-bit unit of information.");
-        ui.add_space(16.0);
-        ui.checkbox(&mut self.code.spaced, "Use Spaces");
-        ui.add_space(16.0);
+        ui.add_space(8.0);
+        ui.checkbox(&mut self.code.spaced, "Print as Groups of Five");
+        ui.add_space(8.0);
 
         ui.fill_code_columns(16, 4, self.code.codes_chars());
         ui.add_space(16.0);
