8951906fabf7de763c29d50d2c0f9579216bc621
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2025-07-09 14:51:52 -0400

    losing mind working on 5c6

--- ciphers/src/digital/block_ciphers/rc6.rs
+++ ciphers/src/digital/block_ciphers/rc6.rs
@@ -9,7 +9,7 @@ const BLOCKSIZE: usize = 16;
 const WORDSIZE: usize = 4;
 const BLOCKWORDS: usize = BLOCKSIZE / WORDSIZE;
 const ROUNDS: usize = 20;
-const ROUND_KEYS: usize = (2 * ROUNDS) + 4;
+const NUM_ROUND_KEYS: usize = (2 * ROUNDS) + 4;
 
 pub fn bytes_to_words(s: &[u8]) -> [u32; BLOCKWORDS] {
     [
@@ -32,7 +32,7 @@ pub fn words_to_bytes(s: &[u32]) -> [u8; BLOCKSIZE] {
 struct Rc6 {
     pub input_format: ByteFormat,
     pub output_format: ByteFormat,
-    pub round_keys: [u32; ROUND_KEYS],
+    pub round_keys: [u32; NUM_ROUND_KEYS],
     pub iv: u128,
     pub mode: BCMode,
     pub padding: BCPadding,
@@ -43,7 +43,7 @@ impl Default for Rc6 {
         Self {
             input_format: ByteFormat::Hex,
             output_format: ByteFormat::Hex,
-            round_keys: [0; ROUND_KEYS],
+            round_keys: [0; NUM_ROUND_KEYS],
             iv: 0,
             mode: BCMode::default(),
             padding: BCPadding::default(),
@@ -59,29 +59,35 @@ impl Rc6 {
 
     pub fn ksa_128(&mut self, key: &[u8]) {
         assert_eq!(key.len(), 16);
-        let c = max(key.len().div_ceil(WORDSIZE), 1); // number of words in the key
-        let mut l = vec![0_u32; c];
-        for i in (0..key.len()).rev() {
-            l[i / WORDSIZE] = (l[i / WORDSIZE].shl(8_u32)).wrapping_add(key[i] as u32)
+        let key_words = 4; // number of words in the key
+
+        let mut s = [0; NUM_ROUND_KEYS];
+        s[0] = P32;
+        for i in 1..NUM_ROUND_KEYS {
+            s[i] = s[i - 1].wrapping_add(Q32)
         }
 
-        let mut s = [0; ROUND_KEYS];
-        s[0] = P32;
-        for i in 1..ROUND_KEYS {
-            s[i] = s[i - 1].wrapping_add(Q32)
+        let mut l = [0_u32; 4];
+        for i in (0..key.len()).rev() {
+            l[i / WORDSIZE] = (l[i / WORDSIZE].shl(8_u32)).wrapping_add(key[i] as u32)
         }
 
         let mut i = 0;
         let mut j = 0;
         let mut a = 0;
         let mut b = 0;
-        for _ in 0..(3 * max(ROUND_KEYS, c)) {
-            s[i] = (s[i].wrapping_add(a).wrapping_add(b)).rotate_left(3);
-            a = s[i];
-            l[j] = (l[j].wrapping_add(a).wrapping_add(b)).rotate_left(a.wrapping_add(b));
-            b = l[j];
-            i = (i + 1) % ROUND_KEYS;
-            j = (j + 1) % c;
+        let v = 3 * max(NUM_ROUND_KEYS, key_words);
+        for _ in 1..(v + 1) {
+            a = s[i].wrapping_add(a).wrapping_add(b).rotate_left(3);
+            s[i] = a;
+            b = l[j]
+                .wrapping_add(a)
+                .wrapping_add(b)
+                .rotate_left(a.wrapping_add(b));
+            l[j] = b;
+
+            i = (i + 1) % NUM_ROUND_KEYS;
+            j = (j + 1) % key_words;
         }
 
         self.round_keys = s;
@@ -122,29 +128,29 @@ impl BlockCipher<BLOCKSIZE> for Rc6 {
         // }
         // A = A + S[2r + 2]
         // C = C + S[2r + 3]
-        let mut block = bytes_to_words(bytes);
+        let [mut a, mut b, mut c, mut d] = bytes_to_words(bytes);
 
-        block[1] = block[1].wrapping_add(self.round_keys[0]);
-        block[3] = block[3].wrapping_add(self.round_keys[1]);
+        b = b.wrapping_add(self.round_keys[0]);
+        d = d.wrapping_add(self.round_keys[1]);
 
         for i in 1..ROUNDS {
-            let t = (block[1].wrapping_mul(2_u32.wrapping_mul(block[1].wrapping_add(1))))
+            let t = b
+                .wrapping_mul(b.wrapping_add(b).wrapping_add(1))
                 .rotate_left(5);
-            let u = (block[3].wrapping_mul(2_u32.wrapping_mul(block[3].wrapping_add(1))))
+            let u = d
+                .wrapping_mul(d.wrapping_add(d).wrapping_add(1))
                 .rotate_left(5);
-            block[0] = (block[0] ^ t)
-                .rotate_left(u % 32)
-                .wrapping_add(self.round_keys[2 * i]);
-            block[2] = (block[2] ^ u)
-                .rotate_left(t % 32)
+            a = (a ^ t).rotate_left(u).wrapping_add(self.round_keys[2 * i]);
+            c = (c ^ u)
+                .rotate_left(t)
                 .wrapping_add(self.round_keys[2 * i + 1]);
-            (block[0], block[1], block[2], block[3]) = (block[1], block[2], block[3], block[0]);
+            (a, b, c, d) = (b, c, d, a);
         }
 
-        block[0] = block[0].wrapping_add(self.round_keys[2 * ROUNDS + 2]);
-        block[2] = block[2].wrapping_add(self.round_keys[2 * ROUNDS + 3]);
+        a = a.wrapping_add(self.round_keys[2 * ROUNDS + 2]);
+        c = c.wrapping_add(self.round_keys[2 * ROUNDS + 3]);
 
-        utils::byte_formatting::overwrite_bytes(bytes, &words_to_bytes(&block));
+        utils::byte_formatting::overwrite_bytes(bytes, &words_to_bytes(&[a, b, c, d]));
     }
 
     fn decrypt_block(&self, bytes: &mut [u8]) {
@@ -160,32 +166,28 @@ impl BlockCipher<BLOCKSIZE> for Rc6 {
         // }
         // D = D - S[1]
         // B = B - S[0]
-        let mut block = bytes_to_words(bytes);
+        let [mut a, mut b, mut c, mut d] = bytes_to_words(bytes);
 
-        block[2] = block[2].wrapping_sub(self.round_keys[2 * ROUNDS + 3]);
-        block[0] = block[0].wrapping_sub(self.round_keys[2 * ROUNDS + 2]);
+        c = c.wrapping_sub(self.round_keys[2 * ROUNDS + 3]);
+        a = a.wrapping_sub(self.round_keys[2 * ROUNDS + 2]);
 
         for i in (1..ROUNDS).rev() {
-            (block[0], block[1], block[2], block[3]) = (block[3], block[0], block[1], block[2]);
+            (a, b, c, d) = (d, a, b, c);
 
-            let u = (block[3].wrapping_mul(2_u32.wrapping_mul(block[3].wrapping_add(1))))
+            let t = b
+                .wrapping_mul(b.wrapping_add(b).wrapping_add(1))
                 .rotate_left(5);
-            let t = (block[1].wrapping_mul(2_u32.wrapping_mul(block[1].wrapping_add(1))))
+            let u = d
+                .wrapping_mul(d.wrapping_add(d).wrapping_add(1))
                 .rotate_left(5);
-            block[2] = (block[2]
-                .wrapping_sub(self.round_keys[2 * i + 1])
-                .rotate_right(t % 32))
-                ^ u;
-            block[0] = (block[0]
-                .wrapping_sub(self.round_keys[2 * i])
-                .rotate_right(u % 32))
-                ^ t;
+            c = (c.wrapping_sub(self.round_keys[2 * i + 1]).rotate_right(t)) ^ u;
+            a = (a.wrapping_sub(self.round_keys[2 * i]).rotate_right(u)) ^ t;
         }
 
-        block[3] = block[3].wrapping_sub(self.round_keys[1]);
-        block[1] = block[1].wrapping_sub(self.round_keys[0]);
+        d = d.wrapping_sub(self.round_keys[1]);
+        b = b.wrapping_sub(self.round_keys[0]);
 
-        utils::byte_formatting::overwrite_bytes(bytes, &words_to_bytes(&block));
+        utils::byte_formatting::overwrite_bytes(bytes, &words_to_bytes(&[a, b, c, d]));
     }
 
     crate::block_cipher_getters!();
@@ -212,11 +214,16 @@ mod basic_tests {
 }
 
 crate::test_block_cipher!(
-    test_1, Rc6::default().with_key_128(&[0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]),
+
+    test_1, Rc6::default().with_key_128(&[0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]),
+    [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
+    [0x8f, 0xc3, 0xa5, 0x36, 0x56, 0xb1, 0xf7, 0x78, 0xc1, 0x29, 0xdf, 0x4e, 0x98, 0x48, 0xa4, 0x1e];
+
+    test_2, Rc6::default().with_key_128(&[0x80, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]),
     [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
     [0x1A, 0xD5, 0x78, 0xA0, 0x2A, 0x08, 0x16, 0x28, 0x50, 0xA1, 0x5A, 0x15, 0x52, 0xA1, 0x7A, 0xD4];
 
-    test_2, Rc6::default().with_key_128(&[0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]),
+    test_3, Rc6::default().with_key_128(&[0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]),
     [0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00],
     [0x91, 0x2E, 0x9C, 0xF1, 0x47, 0x30, 0x35, 0xA8, 0x44, 0x3A, 0x82, 0x49, 0x5C, 0x07, 0x30, 0xD3];
 );
