c340b9b18a8a6e57eafb49ef5deed3576b19c32f
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2025-07-19 16:28:06 -0400

    fixed philox

--- rngs/src/philox.rs
+++ rngs/src/philox.rs
@@ -115,7 +115,7 @@ impl Philox4_32 {
 
     fn round(key: &[u32; 2], ctr: &mut [u32; 4]) {
         let (hi1, lo1) = mul32(PHILOX_M4_32[0], ctr[0]);
-        let (hi2, lo2) = mul32(PHILOX_M4_32[1], ctr[1]);
+        let (hi2, lo2) = mul32(PHILOX_M4_32[1], ctr[2]);
         ctr[0] = hi2 ^ ctr[1] ^ key[0];
         ctr[1] = lo2;
         ctr[2] = hi1 ^ ctr[3] ^ key[1];
@@ -157,103 +157,143 @@ impl ClassicRng for Philox4_32 {
     }
 }
 
-// #[derive(Debug, Default)]
-// pub struct Philox2_64 {
-//     pub key: u64,
-//     pub ctr: [u64; 2],
-// }
+#[derive(Debug)]
+pub struct Philox2_64 {
+    pub key: u64,
+    pub ctr: [u64; 2],
+    pub saved: [u64; 2],
+    idx: usize,
+    pub rounds: usize,
+}
 
-// impl Philox2_64 {
-//     pub fn bumpkey(&mut self) {
-//         self.key = self.key.wrapping_add(PHILOX_W_64[0])
-//     }
+impl Default for Philox2_64 {
+    fn default() -> Self {
+        Self {
+            key: 0,
+            ctr: [0, 0],
+            saved: [0, 0],
+            idx: 0,
+            rounds: 10,
+        }
+    }
+}
 
-//     fn round(&mut self) {
-//         let (hi, lo) = mul64(PHILOX_M2_64, self.ctr[0]);
-//         self.ctr[0] = hi ^ self.ctr[1] ^ self.key;
-//         self.ctr[1] = lo;
-//     }
+impl Philox2_64 {
+    fn bumpkey(key: &mut u64) {
+        *key = key.wrapping_add(PHILOX_W_64[0])
+    }
 
-//     pub fn next_array(&mut self) -> [u64; 2] {
-//         for _ in 0..9 {
-//             self.round();
-//             self.bumpkey();
-//         }
-//         self.round();
-//         self.ctr
-//     }
-// }
+    fn round(key: u64, ctr: &mut [u64; 2]) {
+        let (hi, lo) = mul64(PHILOX_M2_64, ctr[0]);
+        ctr[0] = hi ^ ctr[1] ^ key;
+        ctr[1] = lo;
+    }
 
-// impl ClassicRng for Philox2_64 {
-//     fn next_u32(&mut self) -> u32 {
-//         for _ in 0..9 {
-//             self.round();
-//             self.bumpkey();
-//         }
-//         self.round();
-//         self.ctr[0] as u32
-//     }
+    pub fn next_array(&mut self) -> [u64; 2] {
+        let mut key = self.key;
+        let mut ctr = self.ctr;
+        for _ in 0..(self.rounds - 1) {
+            Self::round(key, &mut ctr);
+            Self::bumpkey(&mut key);
+        }
+        Self::round(key, &mut ctr);
+        self.saved = ctr;
+        self.saved
+    }
+}
 
-//     fn next_u64(&mut self) -> u64 {
-//         for _ in 0..9 {
-//             self.round();
-//             self.bumpkey();
-//         }
-//         self.round();
-//         self.ctr[0]
-//     }
-// }
+impl ClassicRng for Philox2_64 {
+    // Because we are saving the output array and making partial indexes into it, its not possible to define .next_u64() efficiently by just directly outputting words
+    fn next_u32(&mut self) -> u32 {
+        if self.idx == 0 {
+            self.next_array();
+            self.ctr[0] = self.ctr[0].wrapping_add(1);
+            if self.ctr[0] == 0 {
+                self.ctr[1] = self.ctr[1].wrapping_add(1);
+            }
+        }
+        self.idx = (self.idx + 1) % 4;
+        if self.idx % 2 == 0 {
+            (self.saved[self.idx / 2] >> 32) as u32
+        } else {
+            self.saved[self.idx / 2] as u32
+        }
+    }
+}
 
-// #[derive(Debug, Default)]
-// pub struct Philox4_64 {
-//     pub key: [u64; 2],
-//     pub ctr: [u64; 4],
-// }
+#[derive(Debug)]
+pub struct Philox4_64 {
+    pub key: [u64; 2],
+    pub ctr: [u64; 4],
+    pub saved: [u64; 4],
+    idx: usize,
+    pub rounds: usize,
+}
 
-// impl Philox4_64 {
-//     pub fn bumpkey(&mut self) {
-//         self.key[0] = self.key[0].wrapping_add(PHILOX_W_64[0]);
-//         self.key[1] = self.key[1].wrapping_add(PHILOX_W_64[1]);
-//     }
+impl Default for Philox4_64 {
+    fn default() -> Self {
+        Self {
+            key: [0, 0],
+            ctr: [0, 0, 0, 0],
+            saved: [0, 0, 0, 0],
+            idx: 0,
+            rounds: 10,
+        }
+    }
+}
 
-//     fn round(&mut self) {
-//         let (hi1, lo1) = mul64(PHILOX_M4_64[0], self.ctr[0]);
-//         let (hi2, lo2) = mul64(PHILOX_M4_64[1], self.ctr[1]);
-//         self.ctr[0] = hi2 ^ self.ctr[1] ^ self.key[0];
-//         self.ctr[1] = lo2;
-//         self.ctr[2] = hi1 ^ self.ctr[3] ^ self.key[1];
-//         self.ctr[3] = lo1;
-//     }
+impl Philox4_64 {
+    pub fn bumpkey(key: &mut [u64; 2]) {
+        key[0] = key[0].wrapping_add(PHILOX_W_64[0]);
+        key[1] = key[1].wrapping_add(PHILOX_W_64[1]);
+    }
 
-//     pub fn next_array(&mut self) -> [u64; 4] {
-//         for _ in 0..9 {
-//             self.round();
-//             self.bumpkey();
-//         }
-//         self.round();
-//         self.ctr
-//     }
-// }
+    fn round(key: &[u64; 2], ctr: &mut [u64; 4]) {
+        let (hi1, lo1) = mul64(PHILOX_M4_64[0], ctr[0]);
+        let (hi2, lo2) = mul64(PHILOX_M4_64[1], ctr[2]);
+        ctr[0] = hi2 ^ ctr[1] ^ key[0];
+        ctr[1] = lo2;
+        ctr[2] = hi1 ^ ctr[3] ^ key[1];
+        ctr[3] = lo1;
+    }
 
-// impl ClassicRng for Philox4_64 {
-//     fn next_u32(&mut self) -> u32 {
-//         for _ in 0..9 {
-//             self.round();
-//             self.bumpkey();
-//         }
-//         self.round();
-//         self.ctr[0] as u32
-//     }
+    pub fn next_array(&mut self) -> [u64; 4] {
+        let mut key = self.key;
+        let mut ctr = self.ctr;
+        for _ in 0..(self.rounds - 1) {
+            Self::round(&key, &mut ctr);
+            Self::bumpkey(&mut key);
+        }
+        Self::round(&key, &mut ctr);
+        self.saved = ctr;
+        self.saved
+    }
+}
 
-//     fn next_u64(&mut self) -> u64 {
-//         for _ in 0..9 {
-//             self.round();
-//             self.bumpkey();
-//         }
-//         self.round();
-//         self.ctr[0]
-//     }
-// }
+impl ClassicRng for Philox4_64 {
+    // Because we are saving the output array and making partial indexes into it, its not possible to define .next_u64() efficiently by just directly outputting words
+    fn next_u32(&mut self) -> u32 {
+        if self.idx == 0 {
+            self.next_array();
+            self.ctr[0] = self.ctr[0].wrapping_add(1);
+            if self.ctr[0] == 0 {
+                self.ctr[1] = self.ctr[1].wrapping_add(1);
+            }
+            if self.ctr[1] == 0 {
+                self.ctr[2] = self.ctr[2].wrapping_add(1);
+            }
+            if self.ctr[2] == 0 {
+                self.ctr[3] = self.ctr[3].wrapping_add(1);
+            }
+        }
+        self.idx = (self.idx + 1) % 8;
+        if self.idx % 2 == 0 {
+            (self.saved[self.idx / 2] >> 32) as u32
+        } else {
+            self.saved[self.idx / 2] as u32
+        }
+    }
+}
 
 #[cfg(test)]
 mod tests {
@@ -295,79 +335,79 @@ mod tests {
             rng.next_array()
         );
 
-        rng.key = [0x243f6a88, 0x85a308d3];
-        rng.ctr = [0x13198a2e, 0x03707344, 0xa4093822, 0x299f31d0];
+        rng.key = [0xa4093822, 0x299f31d0];
+        rng.ctr = [0x243f6a88, 0x85a308d3, 0x13198a2e, 0x03707344];
         assert_eq!(
             [0xd16cfe09, 0x94fdcceb, 0x5001e420, 0x24126ea1],
             rng.next_array()
         );
     }
 
-    // #[test]
-    // fn sequence2_64() {
-    //     let mut rng = Philox2_64::default();
+    #[test]
+    fn sequence2_64() {
+        let mut rng = Philox2_64::default();
 
-    //     rng.key = 0;
-    //     rng.ctr = [0, 0];
-    //     assert_eq!([0xca00a0459843d731, 0x66c24222c9a845b5], rng.next_array());
+        rng.key = 0;
+        rng.ctr = [0, 0];
+        assert_eq!([0xca00a0459843d731, 0x66c24222c9a845b5], rng.next_array());
 
-    //     rng.key = 0xffffffffffffffff;
-    //     rng.ctr = [0xffffffffffffffff, 0xffffffffffffffff];
-    //     assert_eq!([0x65b021d60cd8310f, 0x4d02f3222f86df20], rng.next_array());
+        rng.key = 0xffffffffffffffff;
+        rng.ctr = [0xffffffffffffffff, 0xffffffffffffffff];
+        assert_eq!([0x65b021d60cd8310f, 0x4d02f3222f86df20], rng.next_array());
 
-    //     rng.key = 0xa4093822299f31d0;
-    //     rng.ctr = [0x243f6a8885a308d3, 0x13198a2e03707344];
-    //     assert_eq!([0x0a5e742c2997341c, 0xb0f883d38000de5d], rng.next_array());
-    // }
+        rng.key = 0xa4093822299f31d0;
+        rng.ctr = [0x243f6a8885a308d3, 0x13198a2e03707344];
+        assert_eq!([0x0a5e742c2997341c, 0xb0f883d38000de5d], rng.next_array());
+    }
 
-    // #[test]
-    // fn sequence4_64() {
-    //     let mut rng = Philox4_64::default();
+    #[test]
+    fn sequence4_64() {
+        let mut rng = Philox4_64::default();
 
-    //     rng.key = [0, 0];
-    //     rng.ctr = [0, 0, 0, 0];
-    //     assert_eq!(
-    //         [
-    //             0x16554d9eca36314c,
-    //             0xdb20fe9d672d0fdc,
-    //             0xd7e772cee186176b,
-    //             0x7e68b68aec7ba23b
-    //         ],
-    //         rng.next_array()
-    //     );
+        rng.key = [0, 0];
+        rng.ctr = [0, 0, 0, 0];
+        assert_eq!(
+            [
+                0x16554d9eca36314c,
+                0xdb20fe9d672d0fdc,
+                0xd7e772cee186176b,
+                0x7e68b68aec7ba23b
+            ],
+            rng.next_array()
+        );
 
-    //     rng.key = [0xffffffffffffffff, 0xffffffffffffffff];
-    //     rng.ctr = [
-    //         0xffffffffffffffff,
-    //         0xffffffffffffffff,
-    //         0xffffffffffffffff,
-    //         0xffffffffffffffff,
-    //     ];
-    //     assert_eq!(
-    //         [
-    //             0x87b092c3013fe90b,
-    //             0x438c3c67be8d0224,
-    //             0x9cc7d7c69cd777b6,
-    //             0xa09caebf594f0ba0
-    //         ],
-    //         rng.next_array()
-    //     );
+        rng.key = [0xffffffffffffffff, 0xffffffffffffffff];
+        rng.ctr = [
+            0xffffffffffffffff,
+            0xffffffffffffffff,
+            0xffffffffffffffff,
+            0xffffffffffffffff,
+        ];
+        assert_eq!(
+            [
+                0x87b092c3013fe90b,
+                0x438c3c67be8d0224,
+                0x9cc7d7c69cd777b6,
+                0xa09caebf594f0ba0
+            ],
+            rng.next_array()
+        );
 
-    //     rng.key = [0x243f6a8885a308d3, 0x13198a2e03707344];
-    //     rng.ctr = [
-    //         0xa4093822299f31d0,
-    //         0x082efa98ec4e6c89,
-    //         0x452821e638d01377,
-    //         0xbe5466cf34e90c6c,
-    //     ];
-    //     assert_eq!(
-    //         [
-    //             0xa528f45403e61d95,
-    //             0x38c72dbd566e9788,
-    //             0xa5a1610e72fd18b5,
-    //             0x57bd43b5e52b7fe6
-    //         ],
-    //         rng.next_array()
-    //     );
-    // }
+        rng.key = [0x452821e638d01377, 0xbe5466cf34e90c6c];
+        rng.ctr = [
+            0x243f6a8885a308d3,
+            0x13198a2e03707344,
+            0xa4093822299f31d0,
+            0x082efa98ec4e6c89,
+        ];
+        assert_eq!(
+            [
+                0xa528f45403e61d95,
+                0x38c72dbd566e9788,
+                0xa5a1610e72fd18b5,
+                0x57bd43b5e52b7fe6
+            ],
+            rng.next_array()
+        );
+    }
 }
