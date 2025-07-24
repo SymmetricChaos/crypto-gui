5166a21aef6d9063541ec9f3c3addf275549f1cc
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2025-07-24 09:04:39 -0400

    threefry4_64_20

--- rngs/src/threefry/mod.rs
+++ rngs/src/threefry/mod.rs
@@ -1,24 +1,7 @@
 pub mod threefry;
 
-use std::ops::{Index, IndexMut};
-
 // The number 240 encrypted with AES with an all zero key
 const C240: u64 = 0x1BD11BDAA9FC1A22;
-pub const SCHEMA_VERSION: u64 = 0x0000000133414853; // schema string "SHA3" and version number 1
-pub const TREE_INFO: u64 = 0x0000000000000000; // only sequential hashing is supported so this is all zero
-
-/// Processing the first block
-const FIRST: u64 = 1 << 62;
-/// Processing the last block
-const FINAL: u64 = 1 << 63;
-/// Processing the configuration block
-const CFG: u64 = 4 << 56;
-/// Processing a message block
-const MSG: u64 = 48 << 56;
-/// Processing an output block
-const OUT: u64 = 63 << 56;
-/// Length of the Config string
-const CFG_LEN: u64 = 4 * 8;
 
 macro_rules! subkey_add {
     ($state: expr, $key: expr) => {
@@ -36,8 +19,42 @@ macro_rules! skein_mix {
 }
 
 #[inline]
-pub fn threefry64_4_round(w: &mut [u64; 4], subkey: &[[u64; 4]]) {
-    subkey_add!(w, &subkey[0]);
+pub fn threefry_64_4_12(w: &mut [u64; 4], key: &[u64; 5]) {
+    skein_mix!(w[0], w[1], 14);
+    skein_mix!(w[2], w[3], 16);
+
+    skein_mix!(w[0], w[3], 52);
+    skein_mix!(w[2], w[1], 57);
+
+    skein_mix!(w[0], w[1], 23);
+    skein_mix!(w[2], w[3], 40);
+
+    skein_mix!(w[0], w[3], 5);
+    skein_mix!(w[2], w[1], 37);
+
+    w[0] = w[0].wrapping_add(key[0]);
+    w[1] = w[1].wrapping_add(key[1]);
+    w[2] = w[2].wrapping_add(key[2]);
+    w[3] = w[3].wrapping_add(key[3]);
+    w[3] = w[3].wrapping_add(1);
+
+    skein_mix!(w[0], w[1], 25);
+    skein_mix!(w[2], w[3], 33);
+
+    skein_mix!(w[0], w[3], 46);
+    skein_mix!(w[2], w[1], 12);
+
+    skein_mix!(w[0], w[1], 58);
+    skein_mix!(w[2], w[3], 22);
+
+    skein_mix!(w[0], w[3], 32);
+    skein_mix!(w[2], w[1], 32);
+
+    w[0] = w[0].wrapping_add(key[2]);
+    w[1] = w[1].wrapping_add(key[3]);
+    w[2] = w[2].wrapping_add(key[4]);
+    w[3] = w[3].wrapping_add(key[0]);
+    w[3] = w[3].wrapping_add(2);
 
     skein_mix!(w[0], w[1], 14);
     skein_mix!(w[2], w[3], 16);
@@ -51,7 +68,37 @@ pub fn threefry64_4_round(w: &mut [u64; 4], subkey: &[[u64; 4]]) {
     skein_mix!(w[0], w[3], 5);
     skein_mix!(w[2], w[1], 37);
 
-    subkey_add!(w, &subkey[1]);
+    w[0] = w[0].wrapping_add(key[3]);
+    w[1] = w[1].wrapping_add(key[4]);
+    w[2] = w[2].wrapping_add(key[0]);
+    w[3] = w[3].wrapping_add(key[1]);
+    w[3] = w[3].wrapping_add(3);
+}
+
+#[inline]
+pub fn threefry_64_4_20(w: &mut [u64; 4], key: &[u64; 5]) {
+    w[0] = w[0].wrapping_add(key[0]);
+    w[1] = w[1].wrapping_add(key[1]);
+    w[2] = w[2].wrapping_add(key[2]);
+    w[3] = w[3].wrapping_add(key[3]);
+
+    skein_mix!(w[0], w[1], 14);
+    skein_mix!(w[2], w[3], 16);
+
+    skein_mix!(w[0], w[3], 52);
+    skein_mix!(w[2], w[1], 57);
+
+    skein_mix!(w[0], w[1], 23);
+    skein_mix!(w[2], w[3], 40);
+
+    skein_mix!(w[0], w[3], 5);
+    skein_mix!(w[2], w[1], 37);
+
+    w[0] = w[0].wrapping_add(key[1]);
+    w[1] = w[1].wrapping_add(key[2]);
+    w[2] = w[2].wrapping_add(key[3]);
+    w[3] = w[3].wrapping_add(key[4]);
+    w[3] = w[3].wrapping_add(1);
 
     skein_mix!(w[0], w[1], 25);
     skein_mix!(w[2], w[3], 33);
@@ -64,6 +111,66 @@ pub fn threefry64_4_round(w: &mut [u64; 4], subkey: &[[u64; 4]]) {
 
     skein_mix!(w[0], w[3], 32);
     skein_mix!(w[2], w[1], 32);
+
+    w[0] = w[0].wrapping_add(key[2]);
+    w[1] = w[1].wrapping_add(key[3]);
+    w[2] = w[2].wrapping_add(key[4]);
+    w[3] = w[3].wrapping_add(key[0]);
+    w[3] = w[3].wrapping_add(2);
+
+    skein_mix!(w[0], w[1], 14);
+    skein_mix!(w[2], w[3], 16);
+
+    skein_mix!(w[0], w[3], 52);
+    skein_mix!(w[2], w[1], 57);
+
+    skein_mix!(w[0], w[1], 23);
+    skein_mix!(w[2], w[3], 40);
+
+    skein_mix!(w[0], w[3], 5);
+    skein_mix!(w[2], w[1], 37);
+
+    w[0] = w[0].wrapping_add(key[3]);
+    w[1] = w[1].wrapping_add(key[4]);
+    w[2] = w[2].wrapping_add(key[0]);
+    w[3] = w[3].wrapping_add(key[1]);
+    w[3] = w[3].wrapping_add(3);
+
+    skein_mix!(w[0], w[1], 25);
+    skein_mix!(w[2], w[3], 33);
+
+    skein_mix!(w[0], w[3], 46);
+    skein_mix!(w[2], w[1], 12);
+
+    skein_mix!(w[0], w[1], 58);
+    skein_mix!(w[2], w[3], 22);
+
+    skein_mix!(w[0], w[3], 32);
+    skein_mix!(w[2], w[1], 32);
+
+    w[0] = w[0].wrapping_add(key[4]);
+    w[1] = w[1].wrapping_add(key[0]);
+    w[2] = w[2].wrapping_add(key[1]);
+    w[3] = w[3].wrapping_add(key[2]);
+    w[3] = w[3].wrapping_add(4);
+
+    skein_mix!(w[0], w[1], 14);
+    skein_mix!(w[2], w[3], 16);
+
+    skein_mix!(w[0], w[3], 52);
+    skein_mix!(w[2], w[1], 57);
+
+    skein_mix!(w[0], w[1], 23);
+    skein_mix!(w[2], w[3], 40);
+
+    skein_mix!(w[0], w[3], 5);
+    skein_mix!(w[2], w[1], 37);
+
+    w[0] = w[0].wrapping_add(key[0]);
+    w[1] = w[1].wrapping_add(key[1]);
+    w[2] = w[2].wrapping_add(key[2]);
+    w[3] = w[3].wrapping_add(key[3]);
+    w[3] = w[3].wrapping_add(5);
 }
 
 #[inline]
--- rngs/src/threefry/threefry.rs
+++ rngs/src/threefry/threefry.rs
@@ -1,4 +1,7 @@
-use crate::ClassicRng;
+use crate::{
+    threefry::{threefry_64_4_12, threefry_64_4_20},
+    ClassicRng,
+};
 
 // pub struct Threefry32_2 {}
 
@@ -12,23 +15,21 @@ use crate::ClassicRng;
 
 // impl Threefry64_2 {}
 
-pub struct Threefry64_4 {
-    rounds: usize,
+pub struct Threefry4_64_12 {
     ctr: [u64; 4],
     key: [u64; 4],
 }
 
-impl Default for Threefry64_4 {
+impl Default for Threefry4_64_12 {
     fn default() -> Self {
         Self {
-            rounds: 20,
             ctr: [0; 4],
             key: [0; 4],
         }
     }
 }
 
-impl Threefry64_4 {
+impl Threefry4_64_12 {
     pub fn array(&self) -> [u64; 4] {
         let mut arr = self.ctr.clone();
         let mut ex_key = [0; 4 + 1];
@@ -37,13 +38,114 @@ impl Threefry64_4 {
             ex_key[i] = self.key[i];
             ex_key[4] ^= self.key[i];
         }
-        for i in 0..self.rounds {}
+        threefry_64_4_12(&mut arr, &ex_key);
+        arr
+    }
+}
+
+impl ClassicRng for Threefry4_64_12 {
+    fn next_u32(&mut self) -> u32 {
         todo!()
     }
 }
 
-impl ClassicRng for Threefry64_4 {
+pub struct Threefry4_64_20 {
+    ctr: [u64; 4],
+    key: [u64; 4],
+}
+
+impl Default for Threefry4_64_20 {
+    fn default() -> Self {
+        Self {
+            ctr: [0; 4],
+            key: [0; 4],
+        }
+    }
+}
+
+impl Threefry4_64_20 {
+    pub fn array(&self) -> [u64; 4] {
+        let mut arr = self.ctr.clone();
+        let mut ex_key = [0; 4 + 1];
+        ex_key[4] = super::C240;
+        for i in 0..4 {
+            ex_key[i] = self.key[i];
+            ex_key[4] ^= self.key[i];
+        }
+        threefry_64_4_20(&mut arr, &ex_key);
+        arr
+    }
+}
+
+impl ClassicRng for Threefry4_64_20 {
     fn next_u32(&mut self) -> u32 {
         todo!()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn sequence4_64_20() {
+        let mut rng = Threefry4_64_20::default();
+
+        rng.ctr = [0, 0, 0, 0];
+        rng.key = [0, 0, 0, 0];
+        assert_eq!(
+            [
+                0x09218ebde6c85537,
+                0x55941f5266d86105,
+                0x4bd25e16282434dc,
+                0xee29ec846bd2e40b
+            ],
+            rng.array()
+        );
+
+        rng.ctr = [
+            0xffffffffffffffff,
+            0xffffffffffffffff,
+            0xffffffffffffffff,
+            0xffffffffffffffff,
+        ];
+        rng.key = [
+            0xffffffffffffffff,
+            0xffffffffffffffff,
+            0xffffffffffffffff,
+            0xffffffffffffffff,
+        ];
+        assert_eq!(
+            [
+                0x29c24097942bba1b,
+                0x0371bbfb0f6f4e11,
+                0x3c231ffa33f83a1c,
+                0xcd29113fde32d168
+            ],
+            rng.array()
+        );
+
+        rng.ctr = [
+            0x243f6a8885a308d3,
+            0x13198a2e03707344,
+            0xa4093822299f31d0,
+            0x082efa98ec4e6c89,
+        ];
+        rng.key = [
+            0x452821e638d01377,
+            0xbe5466cf34e90c6c,
+            0xbe5466cf34e90c6c,
+            0xc0ac29b7c97c50dd,
+        ];
+        assert_eq!(
+            [
+                0xa7e8fde591651bd9,
+                0xbaafd0c30138319b,
+                0x84a5c1a729e685b9,
+                0x901d406ccebc1ba4
+            ],
+            rng.array()
+        );
     }
 }
