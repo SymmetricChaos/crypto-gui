5e3c42b5f067601c0a2378fd02365242a07fedb7
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2025-07-25 14:51:51 -0400

    threefry3_32

--- rngs/src/threefry/mod.rs
+++ rngs/src/threefry/mod.rs
@@ -1,7 +1,8 @@
 pub mod threefry;
 
 // The number 240 encrypted with AES with an all zero key
-const C240: u64 = 0x1BD11BDAA9FC1A22;
+const C240_64: u64 = 0x1BD11BDAA9FC1A22;
+const C240_32: u32 = 0x1BD11BDA;
 
 macro_rules! skein_mix {
     ($a: expr, $b: expr, $r: literal) => {
@@ -150,4 +151,146 @@ pub fn threefry_64_4_r(w: &mut [u64; 4], key: &[u64; 5], rounds: usize) {
     }
 
     //this goes up to 72 rounds but I'm lazy and 20 rounds is the recommendation
+}
+
+// Yes, this is incredibly awkard
+// A real implementation should just pick a specific round count like 12 or 20
+#[inline]
+pub fn threefry_32_4_r(w: &mut [u32; 4], key: &[u32; 5], rounds: usize) {
+    w[0] = w[0].wrapping_add(key[0]);
+    w[1] = w[1].wrapping_add(key[1]);
+    w[2] = w[2].wrapping_add(key[2]);
+    w[3] = w[3].wrapping_add(key[3]);
+
+    if rounds >= 1 {
+        skein_mix!(w[0], w[1], 10);
+        skein_mix!(w[2], w[3], 26);
+    }
+
+    if rounds >= 2 {
+        skein_mix!(w[0], w[3], 11);
+        skein_mix!(w[2], w[1], 21);
+    }
+
+    if rounds >= 3 {
+        skein_mix!(w[0], w[1], 13);
+        skein_mix!(w[2], w[3], 27);
+    }
+
+    if rounds >= 4 {
+        skein_mix!(w[0], w[3], 23);
+        skein_mix!(w[2], w[1], 5);
+
+        w[0] = w[0].wrapping_add(key[1]);
+        w[1] = w[1].wrapping_add(key[2]);
+        w[2] = w[2].wrapping_add(key[3]);
+        w[3] = w[3].wrapping_add(key[4]);
+        w[3] = w[3].wrapping_add(1);
+    }
+
+    if rounds >= 5 {
+        skein_mix!(w[0], w[1], 6);
+        skein_mix!(w[2], w[3], 20);
+    }
+
+    if rounds >= 6 {
+        skein_mix!(w[0], w[3], 17);
+        skein_mix!(w[2], w[1], 11);
+    }
+
+    if rounds >= 7 {
+        skein_mix!(w[0], w[1], 25);
+        skein_mix!(w[2], w[3], 10);
+    }
+
+    if rounds >= 8 {
+        skein_mix!(w[0], w[3], 18);
+        skein_mix!(w[2], w[1], 20);
+
+        w[0] = w[0].wrapping_add(key[2]);
+        w[1] = w[1].wrapping_add(key[3]);
+        w[2] = w[2].wrapping_add(key[4]);
+        w[3] = w[3].wrapping_add(key[0]);
+        w[3] = w[3].wrapping_add(2);
+    }
+
+    if rounds >= 9 {
+        skein_mix!(w[0], w[1], 10);
+        skein_mix!(w[2], w[3], 26);
+    }
+
+    if rounds >= 10 {
+        skein_mix!(w[0], w[3], 11);
+        skein_mix!(w[2], w[1], 21);
+    }
+
+    if rounds >= 11 {
+        skein_mix!(w[0], w[1], 13);
+        skein_mix!(w[2], w[3], 27);
+    }
+
+    if rounds >= 12 {
+        skein_mix!(w[0], w[3], 23);
+        skein_mix!(w[2], w[1], 5);
+
+        w[0] = w[0].wrapping_add(key[3]);
+        w[1] = w[1].wrapping_add(key[4]);
+        w[2] = w[2].wrapping_add(key[0]);
+        w[3] = w[3].wrapping_add(key[1]);
+        w[3] = w[3].wrapping_add(3);
+    }
+
+    if rounds >= 13 {
+        skein_mix!(w[0], w[1], 6);
+        skein_mix!(w[2], w[3], 20);
+    }
+
+    if rounds >= 14 {
+        skein_mix!(w[0], w[3], 17);
+        skein_mix!(w[2], w[1], 11);
+    }
+
+    if rounds >= 15 {
+        skein_mix!(w[0], w[1], 25);
+        skein_mix!(w[2], w[3], 10);
+    }
+
+    if rounds >= 16 {
+        skein_mix!(w[0], w[3], 18);
+        skein_mix!(w[2], w[1], 20);
+
+        w[0] = w[0].wrapping_add(key[4]);
+        w[1] = w[1].wrapping_add(key[0]);
+        w[2] = w[2].wrapping_add(key[1]);
+        w[3] = w[3].wrapping_add(key[2]);
+        w[3] = w[3].wrapping_add(4);
+    }
+
+    if rounds >= 17 {
+        skein_mix!(w[0], w[1], 10);
+        skein_mix!(w[2], w[3], 26);
+    }
+
+    if rounds >= 18 {
+        skein_mix!(w[0], w[3], 11);
+        skein_mix!(w[2], w[1], 21);
+    }
+
+    if rounds >= 19 {
+        skein_mix!(w[0], w[1], 13);
+        skein_mix!(w[2], w[3], 27);
+    }
+
+    if rounds >= 20 {
+        skein_mix!(w[0], w[3], 23);
+        skein_mix!(w[2], w[1], 5);
+
+        w[0] = w[0].wrapping_add(key[0]);
+        w[1] = w[1].wrapping_add(key[1]);
+        w[2] = w[2].wrapping_add(key[2]);
+        w[3] = w[3].wrapping_add(key[3]);
+        w[3] = w[3].wrapping_add(5);
+    }
+
+    //this goes up to 72 rounds (the full Skein round function) but I'm lazy and 20 rounds is the recommendation
 }
--- rngs/src/threefry/threefry.rs
+++ rngs/src/threefry/threefry.rs
@@ -1,16 +1,100 @@
-use crate::{threefry::threefry_64_4_r, ClassicRng};
+use crate::{
+    threefry::{threefry_32_4_r, threefry_64_4_r},
+    ClassicRng,
+};
 
 // pub struct Threefry32_2 {}
 
 // impl Threefry32_2 {}
 
-// pub struct Threefry32_4 {}
+pub struct Threefry4_32 {
+    pub ctr: [u32; 4],
+    pub key: [u32; 4],
+    pub rounds: usize,
+    saved: [u32; 4],
+    idx: usize,
+}
 
-// impl Threefry32_4 {}
+impl Default for Threefry4_32 {
+    fn default() -> Self {
+        Self {
+            ctr: [0; 4],
+            key: [0; 4],
+            rounds: 20,
+            saved: [0; 4],
+            idx: 0,
+        }
+    }
+}
 
-// pub struct Threefry64_2 {}
+impl Threefry4_32 {
+    pub fn array(&self) -> [u32; 4] {
+        let mut arr = self.ctr.clone();
+        let mut ex_key: [u32; 5] = [0; 4 + 1];
+        ex_key[4] = super::C240_32;
+        for i in 0..4 {
+            ex_key[i] = self.key[i];
+            ex_key[4] ^= self.key[i];
+        }
+        threefry_32_4_r(&mut arr, &ex_key, self.rounds);
+        arr
+    }
+}
 
-// impl Threefry64_2 {}
+impl ClassicRng for Threefry4_32 {
+    fn next_u32(&mut self) -> u32 {
+        if self.idx == 0 {
+            self.saved = self.array();
+            self.ctr[0] = self.ctr[0].wrapping_add(1);
+            if self.ctr[0] == 0 {
+                self.ctr[1] = self.ctr[1].wrapping_add(1);
+                if self.ctr[1] == 0 {
+                    self.ctr[2] = self.ctr[2].wrapping_add(1);
+                    if self.ctr[2] == 0 {
+                        self.ctr[3] = self.ctr[3].wrapping_add(1);
+                    }
+                }
+            }
+        }
+        let out = self.saved[self.idx];
+        self.idx = (self.idx + 1) % 4;
+        out
+    }
+}
+
+// pub struct Threefry2_64 {
+//     pub ctr: [u64; 2],
+//     pub key: [u64; 2],
+//     pub rounds: usize,
+//     saved: [u64; 2],
+//     idx: usize,
+// }
+
+// impl Default for Threefry2_64 {
+//     fn default() -> Self {
+//         Self {
+//             ctr: [0; 2],
+//             key: [0; 2],
+//             rounds: 20,
+//             saved: [0; 2],
+//             idx: 0,
+//         }
+//     }
+// }
+
+// impl Threefry2_64 {
+//     pub fn array(&self) -> [u64; 2] {
+//         let mut arr = self.ctr.clone();
+//         let mut ex_key = [0; 4 + 1];
+//         ex_key[4] = super::C240;
+//         for i in 0..4 {
+//             ex_key[i] = self.key[i];
+//             ex_key[4] ^= self.key[i];
+//         }
+//         threefry_64_4_r(&mut arr, &ex_key, self.rounds);
+//         arr
+//     }
+// }
 
 pub struct Threefry4_64 {
     pub ctr: [u64; 4],
@@ -36,7 +120,7 @@ impl Threefry4_64 {
     pub fn array(&self) -> [u64; 4] {
         let mut arr = self.ctr.clone();
         let mut ex_key = [0; 4 + 1];
-        ex_key[4] = super::C240;
+        ex_key[4] = super::C240_64;
         for i in 0..4 {
             ex_key[i] = self.key[i];
             ex_key[4] ^= self.key[i];
@@ -79,7 +163,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn sequence4_64_20() {
+    fn sequence4_64() {
         let mut rng = Threefry4_64::default();
 
         rng.ctr = [0, 0, 0, 0];
@@ -137,11 +221,7 @@ mod tests {
             ],
             rng.array()
         );
-    }
 
-    #[test]
-    fn sequence4_64_13() {
-        let mut rng = Threefry4_64::default();
         rng.rounds = 13;
 
         rng.ctr = [0, 0, 0, 0];
@@ -199,5 +279,54 @@ mod tests {
             ],
             rng.array()
         );
+    }
+
+    #[test]
+    fn sequence4_32() {
+        let mut rng = Threefry4_32::default();
+
+        rng.ctr = [0, 0, 0, 0];
+        rng.key = [0, 0, 0, 0];
+        assert_eq!(
+            [0x9c6ca96a, 0xe17eae66, 0xfc10ecd4, 0x5256a7d8],
+            rng.array()
+        );
+
+        rng.ctr = [0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff];
+        rng.key = [0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff];
+        assert_eq!(
+            [0x2a881696, 0x57012287, 0xf6c7446e, 0xa16a6732],
+            rng.array()
+        );
+
+        rng.ctr = [0x243f6a88, 0x85a308d3, 0x13198a2e, 0x03707344];
+        rng.key = [0xa4093822, 0x299f31d0, 0x082efa98, 0xec4e6c89];
+        assert_eq!(
+            [0x59cd1dbb, 0xb8879579, 0x86b5d00c, 0xac8b6d84],
+            rng.array()
+        );
+
+        rng.rounds = 13;
+
+        // rng.ctr = [0, 0, 0, 0];
+        // rng.key = [0, 0, 0, 0];
+        // assert_eq!(
+        //     [0x9c6ca96a, 0xe17eae66, 0xfc10ecd4, 0x5256a7d8],
+        //     rng.array()
+        // );
+
+        // rng.ctr = [0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff];
+        // rng.key = [0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff];
+        // assert_eq!(
+        //     [0x2a881696, 0x57012287, 0xf6c7446e, 0xa16a6732],
+        //     rng.array()
+        // );
+
+        // rng.ctr = [0x243f6a88, 0x85a308d3, 0x13198a2e, 0x03707344];
+        // rng.key = [0xa4093822, 0x299f31d0, 0x082efa98, 0xec4e6c89];
+        // assert_eq!(
+        //     [0x59cd1dbb, 0xb8879579, 0x86b5d00c, 0xac8b6d84],
+        //     rng.array()
+        // );
     }
 }
