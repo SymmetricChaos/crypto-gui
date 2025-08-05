4bb2a549446cb3b4ec39e833aa6f152f8b7fb35a
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2025-08-05 15:40:20 -0400

    Update pcg.rs

--- rngs/src/pcg.rs
+++ rngs/src/pcg.rs
@@ -16,28 +16,20 @@ pub enum PcgTransform {
 impl PcgTransform {
     pub fn description(&self) -> &'static str {
         match self {
-            Self::Rs => {
-                "Take the top three bits as a binary number 'n'. Shift the bits of the state to the right 29 - n places."
-            }
-            Self::Rr => {
-                "Take the top three bits as a binary number 'n'. Rotate the bits of the state to the right 29 - n places."
-            }
-            Self::XshRr => "u64::rotate_right((n ^ (n >> 18)) >> 27, (n >> 59) as u32)",
-            Self::XshRs => " (the bits XORed with (the bits shifted right 22 places)) shifted right (22 places plus (the bits shifted right 61 places))",
+            Self::Rs => "n >> (29 - (n >> 61))",
+            Self::Rr => "n >>> (29 - (n >> 61))",
+            Self::XshRr => "((n ^ (n >> 18)) >> 27) >>> (n >> 59)",
+            Self::XshRs => " (n ^ (n >> 22)) >> (22 + (n >> 61))",
         }
     }
 
-    pub fn transform64(&self, n: u64) -> u64 {
+    pub fn transform(&self, n: u64) -> u64 {
         match self {
             Self::Rs => n >> (29 - (n >> 61)),
             Self::Rr => n.rotate_right(29 - (n >> 61) as u32),
             Self::XshRr => u64::rotate_right((n ^ (n >> 18)) >> 27, (n >> 59) as u32),
             Self::XshRs => (n ^ (n >> 22)) >> (22 + (n >> 61)),
         }
-    }
-
-    pub fn transform32(&self, n: u64) -> u32 {
-        self.transform64(n) as u32 // truncate the transformed u64 to a u32
     }
 }
 
@@ -61,17 +53,14 @@ impl Default for Pcg {
 
 impl ClassicRng for Pcg {
     fn next_u32(&mut self) -> u32 {
-        self.state = (self.state)
-            .wrapping_mul(self.multiplier)
-            .wrapping_add(self.increment);
-        self.transform.transform32(self.state)
+        self.next_u64() as u32
     }
 
     fn next_u64(&mut self) -> u64 {
         self.state = (self.state)
             .wrapping_mul(self.multiplier)
             .wrapping_add(self.increment);
-        self.transform.transform64(self.state)
+        self.transform.transform(self.state)
     }
 }
 
