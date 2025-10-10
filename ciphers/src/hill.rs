3312beb2475187f253519e5fbf960c4eeca6ec5f
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2025-10-10 12:38:13 -0400

    partial hill

--- ciphers/src/hill.rs
+++ ciphers/src/hill.rs
@@ -1,13 +1,16 @@
 use itertools::Itertools;
-use utils::{errors::GeneralError, preset_alphabet::Alphabet, vecstring::VecString};
+use utils::{
+    errors::GeneralError, preset_alphabet::Alphabet, text_functions::string_chunks,
+    vecstring::VecString,
+};
 
 use crate::Cipher;
 
 // https://patents.google.com/patent/US1845947
 
 // Defeaults because these are hard to create
-const HILL_MATRIX: [[usize; 3]; 3] = [[6, 21, 1], [13, 16, 10], [20, 17, 15]];
-const HILL_MATRIX_INV: [[usize; 3]; 3] = [[8, 5, 10], [21, 8, 21], [21, 12, 8]];
+const HM: [[usize; 3]; 3] = [[6, 24, 1], [13, 16, 10], [20, 17, 15]];
+const HMI: [[usize; 3]; 3] = [[8, 5, 10], [21, 8, 21], [21, 12, 8]];
 
 pub struct Hill {
     pub alphabet: VecString,
@@ -90,56 +93,58 @@ impl Hill {
     }
 
     fn encrypt_matrix(&self, text: &str) -> Result<String, GeneralError> {
-        let column_vector = text
-            .chars()
-            .map(|x| self.alphabet.get_pos(x).unwrap())
-            .collect_vec();
-        let x = HILL_MATRIX[0][0] * column_vector[0]
-            + HILL_MATRIX[1][0] * column_vector[1]
-            + HILL_MATRIX[2][0] * column_vector[2];
-        let y = HILL_MATRIX[0][1] * column_vector[0]
-            + HILL_MATRIX[1][1] * column_vector[1]
-            + HILL_MATRIX[2][1] * column_vector[2];
-        let z = HILL_MATRIX[0][2] * column_vector[0]
-            + HILL_MATRIX[1][2] * column_vector[1]
-            + HILL_MATRIX[2][2] * column_vector[2];
         let mut out = String::new();
-        out.push(*self.alphabet.get_char(x % 26).unwrap());
-        out.push(*self.alphabet.get_char(y % 26).unwrap());
-        out.push(*self.alphabet.get_char(z % 26).unwrap());
+        for chunk in string_chunks(text, 3) {
+            let column = chunk
+                .chars()
+                .map(|x| self.alphabet.get_pos(x).unwrap())
+                .collect_vec();
+            let x = HM[0][0] * column[0] + HM[0][1] * column[1] + HM[0][2] * column[2];
+            let y = HM[1][0] * column[0] + HM[1][1] * column[1] + HM[1][2] * column[2];
+            let z = HM[2][0] * column[0] + HM[2][1] * column[1] + HM[2][2] * column[2];
+            out.push(*self.alphabet.get_char(x % 26).unwrap());
+            out.push(*self.alphabet.get_char(y % 26).unwrap());
+            out.push(*self.alphabet.get_char(z % 26).unwrap());
+        }
         Ok(out)
     }
 
     fn decrypt_matrix(&self, text: &str) -> Result<String, GeneralError> {
-        let column_vector = text
-            .chars()
-            .map(|x| self.alphabet.get_pos(x).unwrap())
-            .collect_vec();
-        let x = HILL_MATRIX_INV[0][0] * column_vector[0]
-            + HILL_MATRIX_INV[1][0] * column_vector[1]
-            + HILL_MATRIX_INV[2][0] * column_vector[2];
-        let y = HILL_MATRIX_INV[0][1] * column_vector[0]
-            + HILL_MATRIX_INV[1][1] * column_vector[1]
-            + HILL_MATRIX_INV[2][1] * column_vector[2];
-        let z = HILL_MATRIX_INV[0][2] * column_vector[0]
-            + HILL_MATRIX_INV[1][2] * column_vector[1]
-            + HILL_MATRIX_INV[2][2] * column_vector[2];
         let mut out = String::new();
-        out.push(*self.alphabet.get_char(x % 26).unwrap());
-        out.push(*self.alphabet.get_char(y % 26).unwrap());
-        out.push(*self.alphabet.get_char(z % 26).unwrap());
+        for chunk in string_chunks(text, 3) {
+            let column = chunk
+                .chars()
+                .map(|x| self.alphabet.get_pos(x).unwrap())
+                .collect_vec();
+            let x = HMI[0][0] * column[0] + HMI[0][1] * column[1] + HMI[0][2] * column[2];
+            let y = HMI[1][0] * column[0] + HMI[1][1] * column[1] + HMI[1][2] * column[2];
+            let z = HMI[2][0] * column[0] + HMI[2][1] * column[1] + HMI[2][2] * column[2];
+            out.push(*self.alphabet.get_char(x % 26).unwrap());
+            out.push(*self.alphabet.get_char(y % 26).unwrap());
+            out.push(*self.alphabet.get_char(z % 26).unwrap());
+        }
         Ok(out)
     }
 }
 
 impl Cipher for Hill {
     fn encrypt(&self, text: &str) -> Result<String, GeneralError> {
+        if text.chars().count() % 3 != 0 {
+            return Err(GeneralError::input(
+                "plaintext length must be a multiple of three",
+            ));
+        }
         let t = self.encrypt_cyclic_1(text)?;
         let t = self.encrypt_matrix(&t)?;
         self.encrypt_cyclic_2(&t)
     }
 
     fn decrypt(&self, text: &str) -> Result<String, GeneralError> {
+        if text.chars().count() % 3 != 0 {
+            return Err(GeneralError::input(
+                "ciphertext length must be a multiple of three",
+            ));
+        }
         let t = self.decrypt_cyclic_2(text)?;
         let t = self.decrypt_matrix(&t)?;
         self.decrypt_cyclic_1(&t)
@@ -154,6 +159,16 @@ mod test {
     #[test]
     fn matrix_encr() {
         let cipher = Hill::default();
-        println!("{}", cipher.encrypt_matrix("ACT").unwrap());
+        assert_eq!("POH", cipher.encrypt_matrix("ACT").unwrap());
+        assert_eq!("ACT", cipher.decrypt_matrix("POH").unwrap());
+        assert_eq!("FIN", cipher.encrypt_matrix("CAT").unwrap());
+        assert_eq!("CAT", cipher.decrypt_matrix("FIN").unwrap());
+    }
+
+    #[test]
+    fn encrypt_decrypt() {
+        let cipher = Hill::default();
+        let ct = cipher.encrypt("THEQUICKBROWNFOXJUMPS").unwrap();
+        assert_eq!("THEQUICKBROWNFOXJUMPS", cipher.encrypt(&ct).unwrap());
     }
 }
