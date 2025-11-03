32dc40fe8ed9601f00a3c764d2efef9c55dad497
SymmetricChaos <42520289+SymmetricChaos@users.noreply.github.com>
2025-11-03 14:11:49 -0500

    Update achterbahn.rs

--- ciphers/src/digital/stream_ciphers/achterbahn.rs
+++ ciphers/src/digital/stream_ciphers/achterbahn.rs
@@ -17,38 +17,72 @@ const NLFSR_MASK: [u64; 13] = [
 
 macro_rules! and3 {
     ($a: expr, $b: expr, $c: expr) => {
-        a & b & c
+        $a & $b & $c
     };
 }
 
 macro_rules! and2 {
     ($a: expr, $b: expr) => {
-        a & b
+        $a & $b
     };
 }
 
 macro_rules! xor3 {
     ($a: expr, $b: expr, $c: expr) => {
-        a ^ b ^ c
+        $a ^ $b ^ $c
     };
 }
 
 macro_rules! xor2 {
     ($a: expr, $b: expr) => {
-        a ^ b
+        $a ^ $b
     };
 }
 
 macro_rules! mux3 {
     ($a: expr, $b: expr, $c: expr) => {
-        (c & (a ^ b)) ^ a
+        ($c & ($a ^ $b)) ^ $a
     };
 }
 
 macro_rules! maj3 {
     ($a: expr, $b: expr, $c: expr) => {
-        (a & (b ^ c)) ^ (b & c)
+        ($a & ($b ^ $c)) ^ ($b & $c)
     };
+}
+
+// #define A0_cycle(x,feedin)\
+//    (x = (x >> ONE) | ((ONE & (XOR3(XOR3(feedin,\
+//                                          x >>15,\
+//                                         XOR3( x >>3, x >>2, x )),\
+//                                    XOR3(AND2( x >>4, x >>7),\
+//                                         XOR3( x >>5, x >>6, x >>8),\
+//                                         MUX3( x >>4, x >>5, x >>6)),\
+//                                    MUX3(MUX3( x >>11, x >>12, x >>2),\
+//                                         AND3( x >>6, x >>2, x >>13),\
+//                                         MUX3( x >>1, x >>10, x >>9)))\
+//                      )) << 20))
+
+fn step_a0(x: u64, feedin: u64) -> u64 {
+
+    (x >> 1) | 1 & 
+    xor3!(
+        xor3!(
+            feedin, 
+            x >> 15,
+            xor3!(x >> 3, x >> 2, x)
+            ),
+        xor3!(
+            and2!(x >> 4, x >> 7),
+            xor3!(x >> 5, x >> 6, x >> 8),
+            mux3!(x >> 4, x >> 5, x >> 6)
+            ),
+        mux3!(
+            mux3!(x >>11, x >>12, x >>2),
+            and3!(x >>6, x >>2, x >>13),
+            mux3!( x >>1, x >>10, x >>9)
+            )
+        )
 }
 
 pub struct Achterbahn128 {}
